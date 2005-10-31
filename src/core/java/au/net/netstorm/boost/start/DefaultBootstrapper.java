5fcee168b4140577f91e757dc1cf23e138413c75
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2005-10-31 10:39:40 +0000

    SC502
    git-svn-id: https://geekscape.org/svn/boost/trunk@88 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/start/DefaultBootstrapper.java
+++ src/core/java/au/net/netstorm/boost/start/DefaultBootstrapper.java
@@ -1,13 +1,6 @@
 package au.net.netstorm.boost.start;
 
 public final class DefaultBootstrapper implements Bootstrapper {
-//    private WiringFactory wiringFactory = (WiringFactory) XxxIocNeedsToDoThis.create(DefaultWiringFactory.class);
-//
-//
-//    public void bootstrap(VmStyle style) {
-//        wiringFactory.get(style).wire();
-//        exporterFactory.get(style).export();
-//    }
 
     public void bootstrap(VmStyle style) {
         throw new UnsupportedOperationException();
