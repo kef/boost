c0e76ebd2ce3b9df36639162e87de746cb0f961f
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2005-10-31 01:32:13 +0000

    SC502
    git-svn-id: https://geekscape.org/svn/boost/trunk@42 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/testing/suite/AtomicTestSuite.java
+++ src/test/java/au/net/netstorm/boost/testing/suite/AtomicTestSuite.java
@@ -4,6 +4,7 @@ import au.net.netstorm.boost.testing.aggregator.TestSuiteAggregator;
 import au.net.netstorm.boost.primordial.PrimordialTestCase;
 import junit.framework.Test;
 
+// FIXME: SC502 Move all testing.suite to "suite".
 public class AtomicTestSuite extends PrimordialTestCase {
     public static Test suite() {
         return TestSuiteAggregator.aggregate("Atomic", ".*AtomicTest");
