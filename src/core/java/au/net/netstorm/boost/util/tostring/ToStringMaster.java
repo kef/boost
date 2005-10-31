fadc3097f8d9b11d4dfd522056582831aeb83e52
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2005-10-31 22:30:17 +0000

    SC502
    git-svn-id: https://geekscape.org/svn/boost/trunk@105 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/util/tostring/ToStringMaster.java
+++ src/core/java/au/net/netstorm/boost/util/tostring/ToStringMaster.java
@@ -1,25 +1,17 @@
 package au.net.netstorm.boost.util.tostring;
 
-// FIXME: SC501 Move everything to util except primordial, start, time.
-// FIXME: SC501 Consistent approach to static fields.
-// FIXME: SC501 ? reflect.core reflect.type.
-// FIXME: SC501 Move all test code into test hierarchy.
-// FIXME: SC501 Quantum vs Class as name for tests.
-// FIXME: SC501 Create primordial subpackage.
-// FIXME: SC501 Rename all UnitTests to ClassTest.
-// FIXME: SC501 Leave no fixmes.
-// FIXME: SC501 Interject or provide PrimordialTestCase.
-// FIXME: SC501 Sort out reflect/introspect schism.
-// FIXME: SC501 Check all fail<TestName> methods and convert.
-// FIXME: SC501 Ensure all tests sit in the test hierarchy.
-// FIXME: SC501 Comb and replace string literals.
-// FIXME: SC501 Test drive all exceptions.
-// FIXME: SC501 Create NotImplementedException.
-// FIXME: SC501 Have a good hard look at and remove the "fixtures" package.  This stuff is smelly cheese.
-// FIXME: SC501 Remove all SC501 references.
-// FIXME: SC501 Primordial extenders need to be final.  Put this check into primordial.
-// FIXME: SC501 Throw exception from clone().
-// FIXME: SC501 Minimal braces everywhere.
+// FIXME: SC507 Move everything to util except primordial, start, time.
+// FIXME: SC506 Consistent approach to static fields.
+// FIXME: SC506 ? reflect.core reflect.type.
+// FIXME: SC509 Interject or provide PrimordialTestCase.
+// FIXME: SC509 Sort out reflect/introspect schism.
+// FIXME: SC506 Check all fail<TestName> methods and convert.
+// FIXME: SC506 Comb and replace string literals.
+// FIXME: SC510 Create NotImplementedException.
+// FIXME: SC506 Have a good hard look at and remove the "fixtures" package.  This stuff is smelly cheese.
+// FIXME: SC509 Primordial extenders need to be final.  Put this check into primordial.
+// FIXME: SC509 Throw exception from clone().
+// FIXME: SC506 Minimal braces everywhere.
 public interface ToStringMaster {
     String getString(Object ref);
 }
