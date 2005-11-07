b1409524d7d39ac1078565aa5ce0a7f631e914b0
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2005-11-07 05:33:20 +0000

    SC502
    git-svn-id: https://geekscape.org/svn/boost/trunk@110 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/time/type/MonthSpecOutOfBoundsException.java
+++
@@ -1,8 +0,0 @@
-package au.net.netstorm.boost.time.type;
-
-public final class MonthSpecOutOfBoundsException extends RuntimeException {
-
-    public MonthSpecOutOfBoundsException(MonthSpec spec, Throwable t) {
-//        super("Month spec out of range (spec="+spec+"),", t);
-    }
-}
