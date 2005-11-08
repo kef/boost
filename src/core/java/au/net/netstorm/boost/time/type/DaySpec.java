5eb66dc66d185a22c844a19f480c939a5de6fd08
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2005-11-08 00:27:16 +0000

    SC502
    git-svn-id: https://geekscape.org/svn/boost/trunk@142 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/time/type/DaySpec.java
+++ src/core/java/au/net/netstorm/boost/time/type/DaySpec.java
@@ -1,9 +1,17 @@
 package au.net.netstorm.boost.time.type;
 
-public final class DaySpec implements Comparable {
+import au.net.netstorm.boost.primordial.Primordial;
+
+public final class DaySpec extends Primordial implements Comparable {
 
     public final MonthSpec month;
     public final int day;
+
+    public DaySpec(MonthSpec month, int day) {
+        this.month = month;
+        this.day = day;
+        validate();
+    }
 
     public int compareTo(Object o) {
         if (! getClass().isAssignableFrom(o.getClass())) throw new IllegalArgumentException("Cannot perform a comparison with a "+o.getClass()+".");
@@ -13,29 +21,6 @@ public final class DaySpec implements Comparable {
         if (us < them) return -1;
         if (us > them) return 1;
         return 0;
-    }
-
-    public DaySpec(MonthSpec month, int day) {
-        this.month = month;
-        this.day = day;
-        validate();
-    }
-
-    public int hashCode() {
-        return day;
-    }
-
-    public boolean equals(Object o) {
-        if (!(o instanceof DaySpec)) return false;
-        return equals((DaySpec)o);
-    }
-
-    public boolean equals(DaySpec day) {
-        return compareTo(day) == 0;
-    }
-
-    public String toString() {
-        return "DaySpec[month="+month+",day="+day+"]";
     }
 
     private void validate() {
