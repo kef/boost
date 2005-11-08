da4687e76286d40250226c2918b010bca76e9b19
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2005-11-08 01:10:10 +0000

    SC502
    git-svn-id: https://geekscape.org/svn/boost/trunk@147 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/time/type/DaySpecOutOfBoundsException.java
+++
@@ -1,9 +0,0 @@
-package au.net.netstorm.boost.time.type;
-
-// FIXME: SC510 Test drive exceptions.
-public final class DaySpecOutOfBoundsException extends RuntimeException {
-
-    public DaySpecOutOfBoundsException(DaySpec spec, Throwable t) {
-        super("Day spec out of range (spec="+spec+"),", t);
-    }
-}
