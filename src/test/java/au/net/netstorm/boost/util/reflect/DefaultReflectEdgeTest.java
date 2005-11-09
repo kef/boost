8b58b2546ceab5193466aeb9579f75b01a3589d4
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2005-11-09 00:29:27 +0000

    SC502
    git-svn-id: https://geekscape.org/svn/boost/trunk@170 05db6a09-9903-0410-bedf-ba9bc4063e48

---
+++ src/test/java/au/net/netstorm/boost/util/reflect/DefaultReflectEdgeTest.java
@@ -0,0 +1,9 @@
+package au.net.netstorm.boost.util.reflect;
+
+import junit.framework.TestCase;
+
+public final class DefaultReflectEdgeTest extends TestCase {
+    public void testTickle() {
+        fail("WRITE A TEST");
+    }
+}
