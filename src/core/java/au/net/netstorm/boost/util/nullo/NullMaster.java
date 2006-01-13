e17f040cc4bf6d3037347d3850373a0831bb742b
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2006-01-13 04:06:49 +0000

    SC502
    git-svn-id: https://geekscape.org/svn/boost/trunk@210 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/util/nullo/NullMaster.java
+++ src/core/java/au/net/netstorm/boost/util/nullo/NullMaster.java
@@ -5,7 +5,6 @@ import java.io.Serializable;
 // FIXME: SC502 Test serializable in NullAT.
 public class NullMaster implements Serializable {
     // FIXME: SC502 INSTANCE pattern.
-    // FIXME: SC502 Make instance.
 
     public void check(Object parameter) {
         if (parameter == null) throw new IllegalArgumentException();
