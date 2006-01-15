7a8a2da2d3cface8a02d1583e35427281cb305e6
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2006-01-15 23:07:20 +0000

    SC502
    git-svn-id: https://geekscape.org/svn/boost/trunk@213 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/util/fixture/InstanceProviderTestUtilSuppressed.java
+++ src/test/java/au/net/netstorm/boost/util/fixture/InstanceProviderTestUtilSuppressed.java
@@ -7,16 +7,21 @@ import au.net.netstorm.boost.util.type.Interface;
 
 public class InstanceProviderTestUtilSuppressed implements InstanceProvider {
     public Object getInstance(Class type) {
-        if (type == Object.class || type == String.class) return "We cower in the corner our hands up over our ears.";
-        if (type == Class.class) return CharSequence.class;
-        if (type == Remote.class) return new Remote() { };
-        if (type == Interface.class) return new Interface(CharSequence.class);
-        throw new UnsupportedOperationException("Lovely fella, please write the code for type " + type);
+        return xxx(type);
     }
 
     // FIXME: SC502.  Modify in calls to use this.  And method above.  This should throw the exception.
     public boolean canProvide(Class type) {
         // FIXME: SC502.  Replace with NIY exception.
         throw new RuntimeException("NOT IMPLEMENTED YET");
+    }
+
+    // FIXME: RDSC050 Rename.
+    private Object xxx(Class type) {
+        if (type == Object.class || type == String.class) return "We cower in the corner our hands up over our ears.";
+        if (type == Class.class) return CharSequence.class;
+        if (type == Remote.class) return new Remote() { };
+        if (type == Interface.class) return new Interface(CharSequence.class);
+        throw new UnsupportedOperationException("Lovely fella, please write the code for type " + type);
     }
 }
