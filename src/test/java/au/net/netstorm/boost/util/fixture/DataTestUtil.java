c06718cc022e0bb8ac2db901c8e7b74461ca21f0
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2006-01-15 23:37:55 +0000

    SC502
    git-svn-id: https://geekscape.org/svn/boost/trunk@221 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/util/fixture/DataTestUtil.java
+++ src/test/java/au/net/netstorm/boost/util/fixture/DataTestUtil.java
@@ -1,15 +1,13 @@
 package au.net.netstorm.boost.util.fixture;
 
-
-import au.net.netstorm.boost.util.type.Data;
 import au.net.netstorm.boost.util.introspect.FieldSpec;
 import au.net.netstorm.boost.util.serialize.SerializationTestUtil;
+import au.net.netstorm.boost.util.type.Data;
 import junit.framework.Assert;
 
 // FIXME: SC506 Make instance.
-public class DataTestUtil extends Assert {
 
-    private static final InstanceProviderTestUtil INSTANCE_PROVIDER_TEST_UTIL = new InstanceProviderTestUtil();
+public class DataTestUtil extends Assert {
     public static void checkIsDataObject(Class cls, FieldSpec[] newArgTypes) {
         ClassTestFixture.checkClass(cls, newArgTypes, Data.class);
         Object[] parameters = InstanceTestUtil.getParameters(newArgTypes);
