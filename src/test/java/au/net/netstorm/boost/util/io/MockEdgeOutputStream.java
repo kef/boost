05dac32570d738664d635d720a7da90bfc2f7dc7
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2006-01-16 04:32:59 +0000

    SC502
    git-svn-id: https://geekscape.org/svn/boost/trunk@263 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/util/io/MockEdgeOutputStream.java
+++ src/test/java/au/net/netstorm/boost/util/io/MockEdgeOutputStream.java
@@ -3,26 +3,28 @@ package au.net.netstorm.boost.util.io;
 import java.io.Serializable;
 
 import junit.framework.Assert;
+import au.net.netstorm.boost.primordial.Primordial;
 
-public final class MockEdgeOutputStream extends Assert implements EdgeOutputStream, Serializable {
+// FIXME: SC050 ? PrimordialTest.
+public final class MockEdgeOutputStream extends Primordial implements EdgeOutputStream, Serializable {
     private transient final TestAsserter asserter = new TestAsserter();
     private byte[] actual;
     private boolean flushed = false;
     private boolean writeCalled = false;
 
     public void flush() {
-        if (!writeCalled) fail("write(...) must be called prior to flush().");
+        if (!writeCalled) Assert.fail("write(...) must be called prior to flush().");
         flushed = true;
     }
 
     public void write(byte[] bytes) {
-        if (writeCalled) fail("Expecting a single call only to write.");
+        if (writeCalled) Assert.fail("Expecting a single call only to write.");
         writeCalled = true;
         actual = bytes;
     }
 
     public void verify(byte[] expected) {
         asserter.assertEquals(expected, actual);
-        assertTrue(flushed);
+        Assert.assertTrue(flushed);
     }
 }
