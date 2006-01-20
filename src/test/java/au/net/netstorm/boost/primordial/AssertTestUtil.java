ab8dac2f84f7706cf7b4129bce50e5eaa84f1f67
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2006-01-20 05:14:14 +0000

    SC502
    git-svn-id: https://geekscape.org/svn/boost/trunk@279 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/primordial/AssertTestUtil.java
+++ src/test/java/au/net/netstorm/boost/primordial/AssertTestUtil.java
@@ -3,7 +3,7 @@ package au.net.netstorm.boost.primordial;
 import junit.framework.Assert;
 
 // FIXME: ? SC506 Instancise.
-class AssertTestUtil extends Assert {
+public class AssertTestUtil extends Assert {
     public static final void assertEquals(Object[] expected, Object[] actual) {
         assertEquals(expected.length, actual.length);
         for (int i = 0; i < actual.length; i++) assertEquals(expected[i], actual[i]);
