96d54a7ba13f2cd44730ce4dacc5565c1ea757ea
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2006-01-20 05:22:51 +0000

    SC502
    git-svn-id: https://geekscape.org/svn/boost/trunk@281 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/primordial/AssertTestUtil.java
+++ src/test/java/au/net/netstorm/boost/primordial/AssertTestUtil.java
@@ -4,7 +4,8 @@ import junit.framework.Assert;
 
 // FIXME: ? SC506 Instancise.
 public class AssertTestUtil extends Assert {
-    public static final void assertEquals(Object[] expected, Object[] actual) {
+    
+    public static final void checkEquals(Object[] expected, Object[] actual) {
         assertEquals(expected.length, actual.length);
         for (int i = 0; i < actual.length; i++) assertEquals(""+i, expected[i], actual[i]);
     }
--- src/test/java/au/net/netstorm/boost/primordial/PrimordialTestCase.java
+++ src/test/java/au/net/netstorm/boost/primordial/PrimordialTestCase.java
@@ -8,7 +8,7 @@ import junit.framework.TestCase;
 // Would prefer not to declare this "abstract", but contract with JUnit/TestCase precludes this.
 public abstract class PrimordialTestCase extends TestCase {
     public final void assertEquals(Object[] expected, Object[] fields) {
-        AssertTestUtil.assertEquals(expected, fields);
+        AssertTestUtil.checkEquals(expected, fields);
     }
 
     public final void testSoThePoxyScabJUnitDoesNotWhinge() {
