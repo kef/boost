22c8ff6029cdbcac7a503715922b29bc427b30f5
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2006-01-20 05:26:37 +0000

    SC502
    git-svn-id: https://geekscape.org/svn/boost/trunk@282 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/primordial/AssertTestUtil.java
+++ src/test/java/au/net/netstorm/boost/primordial/AssertTestUtil.java
@@ -3,10 +3,11 @@ package au.net.netstorm.boost.primordial;
 import junit.framework.Assert;
 
 // FIXME: ? SC506 Instancise.
-public class AssertTestUtil extends Assert {
-    
+// Do not extend Assert.  Users of this class may inadvertently pick up Assert's methods.  We don't want this :)
+public class AssertTestUtil  {
+
     public static final void checkEquals(Object[] expected, Object[] actual) {
-        assertEquals(expected.length, actual.length);
-        for (int i = 0; i < actual.length; i++) assertEquals(""+i, expected[i], actual[i]);
+        Assert.assertEquals(expected.length, actual.length);
+        for (int i = 0; i < actual.length; i++) Assert.assertEquals(""+i, expected[i], actual[i]);
     }
 }
