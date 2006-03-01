17bac1f332063406bdcc952ca2c1f7fcf6096aa3
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2006-03-01 04:30:56 +0000

    SC042 - A tidy up pass on the reflection utilities.
    git-svn-id: https://geekscape.org/svn/boost/trunk@417 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/util/reflect/FieldTestUtil.java
+++ src/test/java/au/net/netstorm/boost/util/reflect/FieldTestUtil.java
@@ -2,11 +2,7 @@ package au.net.netstorm.boost.util.reflect;
 
 import java.lang.reflect.Field;
 
-// FIXME: SC042 In fact this mostly smells like a FieldTestUtil.
-
 // FIXME: SC042 Change field names for all references to this. (DO SAME FOR METHOD, CLASS ...)
-
-// FIXME: SC042 Split out check methods.
 
 public interface FieldTestUtil {
     // FIXME: SC042 Remove field from all methods names.
