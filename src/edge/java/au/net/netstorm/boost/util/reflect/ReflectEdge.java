61aa38c8ccb5c3d5179c8991ae2fa2e3198bc16d
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2006-03-01 06:58:06 +0000

    SC042 - A tidy up pass on the reflection utilities.
    git-svn-id: https://geekscape.org/svn/boost/trunk@452 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/edge/java/au/net/netstorm/boost/util/reflect/ReflectEdge.java
+++ src/edge/java/au/net/netstorm/boost/util/reflect/ReflectEdge.java
@@ -4,12 +4,11 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
-// FUTURE: This stuff will probably follow the same sort of pattern as the MemberTestUtil, ClassTestUtil...
+// SUGGEST: This stuff will probably follow the same sort of pattern as the MemberTestUtil, ClassTestUtil...
 
 public interface ReflectEdge {
     ReflectEdge INSTANCE = new DefaultReflectEdge();
 
-    // FIXME: SC042 Reverse field ordering.  Same below.
     Object getFieldValue(Field field, Object ref);
 
     void setFieldValue(Field field, Object ref, Object value);
