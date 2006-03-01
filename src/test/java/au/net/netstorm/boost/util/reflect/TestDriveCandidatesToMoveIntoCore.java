8d159d7b20a5e9134e1a2d03ee4c6264fb802e46
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2006-03-01 01:12:23 +0000

    SC042 - A tidy up pass on the reflection utilities.
    git-svn-id: https://geekscape.org/svn/boost/trunk@407 05db6a09-9903-0410-bedf-ba9bc4063e48

---
+++ src/test/java/au/net/netstorm/boost/util/reflect/TestDriveCandidatesToMoveIntoCore.java
@@ -0,0 +1,4 @@
+package au.net.netstorm.boost.util.reflect;
+
+public interface TestDriveCandidatesToMoveIntoCore extends ClassTestUtil, ExceptionTestUtil, FieldTestUtil, MethodTestUtil, ModifierTestUtil {
+}
