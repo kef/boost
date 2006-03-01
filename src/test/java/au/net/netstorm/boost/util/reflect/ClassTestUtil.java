c9f07fcaf11c0bb89a5dee5db65662002fce3cab
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2006-03-01 01:35:21 +0000

    SC042 - A tidy up pass on the reflection utilities.
    git-svn-id: https://geekscape.org/svn/boost/trunk@410 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/util/reflect/ClassTestUtil.java
+++ src/test/java/au/net/netstorm/boost/util/reflect/ClassTestUtil.java
@@ -2,9 +2,6 @@ package au.net.netstorm.boost.util.reflect;
 
 import au.net.netstorm.boost.util.type.Interface;
 
-// FIXME: SC042 There seems to be a distinct split into check* and is* methods.  Split.
-// FIXME: SC042 Split into checker.
-
 public interface ClassTestUtil {
     boolean isImplementationOf(Interface targetInterface, Class cls);
 
--- src/test/java/au/net/netstorm/boost/util/reflect/DefaultClassTestUtil.java
+++ src/test/java/au/net/netstorm/boost/util/reflect/DefaultClassTestUtil.java
@@ -2,12 +2,7 @@ package au.net.netstorm.boost.util.reflect;
 
 import au.net.netstorm.boost.util.type.Interface;
 
-// FIXME: SC042 Rename.
-
 public class DefaultClassTestUtil implements ClassTestUtil {
-    private final ModifierTestChecker modifier = new DefaultModifierTestChecker();
-    private final ClassMaster clsMaster = new DefaultClassMaster();
-
     // FIXME: SC042 Investigate ... This did not appear to work for targetInterface == java.io.Serializable ?
     public boolean isImplementationOf(Interface targetInterface, Class cls) {
         Class type = targetInterface.getType();
