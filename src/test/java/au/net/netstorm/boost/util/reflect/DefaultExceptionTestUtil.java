cd3653b6a6b91e566727231f4c54a982c4bab280
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2006-03-01 23:31:44 +0000

    SC042 - A tidy up pass on the reflection utilities.
    git-svn-id: https://geekscape.org/svn/boost/trunk@462 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/util/reflect/DefaultExceptionTestUtil.java
+++ src/test/java/au/net/netstorm/boost/util/reflect/DefaultExceptionTestUtil.java
@@ -2,14 +2,15 @@ package au.net.netstorm.boost.util.reflect;
 
 import java.lang.reflect.InvocationTargetException;
 
+// FIXME: SC050 What a pile of smelly rot.
+
 public final class DefaultExceptionTestUtil implements ExceptionTestUtil {
     public Class getRealExceptionClass(Throwable t) {
         // FIXME: SC050 This certainly does not really work.  Sort this out!!!
-        // FIXME: SC042 Early returns fellas.
         Throwable realException = t;
         if (realException.getClass() == RuntimeException.class) realException = (Throwable) realException.getCause();
         if (realException.getClass() == InvocationTargetException.class)
-            realException = (Throwable) realException.getCause();
+            realException = (Throwable) realException.getCause(); // FIXME: SC050 Bloody amateurs.  This should be getTargetInvocation ...
         return realException.getClass();
     }
 }
