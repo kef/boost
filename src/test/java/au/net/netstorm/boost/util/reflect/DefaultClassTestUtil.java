7c2a49c67bcc35284d3d5dd3ed3fc2cf7cd02bd9
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2006-03-01 06:26:47 +0000

    SC042 - A tidy up pass on the reflection utilities.
    git-svn-id: https://geekscape.org/svn/boost/trunk@446 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/util/reflect/DefaultClassTestUtil.java
+++ src/test/java/au/net/netstorm/boost/util/reflect/DefaultClassTestUtil.java
@@ -3,7 +3,6 @@ package au.net.netstorm.boost.util.reflect;
 import au.net.netstorm.boost.util.type.Interface;
 
 public class DefaultClassTestUtil implements ClassTestUtil {
-    // FIXME: SC042 Investigate ... This did not appear to work for targetInterface == java.io.Serializable ?
     public boolean isImplementationOf(Interface targetInterface, Class cls) {
         Class type = targetInterface.getType();
         return type.isAssignableFrom(cls);
