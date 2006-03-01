41b61a952fd14ab57a4a74bf8b50c6f3498cd86b
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2006-03-01 04:49:07 +0000

    SC042 - A tidy up pass on the reflection utilities.
    git-svn-id: https://geekscape.org/svn/boost/trunk@427 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/util/introspect/CallSpecAtomicTest.java
+++ src/test/java/au/net/netstorm/boost/util/introspect/CallSpecAtomicTest.java
@@ -4,10 +4,10 @@ import au.net.netstorm.boost.util.fixture.DefaultDataTestUtil;
 import junit.framework.TestCase;
 
 public class CallSpecAtomicTest extends TestCase {
-    private static final DefaultFieldSpec FIELD_SPEC_METHOD_NAME = new DefaultFieldSpec("methodName", String.class);
-    private static final DefaultFieldSpec FIELD_SPEC_ARG_TYPES = new DefaultFieldSpec("argTypes", Class[].class);
-    private static final DefaultFieldSpec FIELD_SPEC_ARGS = new DefaultFieldSpec("args", Object[].class);
-    private static final DefaultFieldSpec[] TYPES = {FIELD_SPEC_METHOD_NAME, FIELD_SPEC_ARG_TYPES, FIELD_SPEC_ARGS};
+    private static final FieldSpec FIELD_SPEC_METHOD_NAME = new DefaultFieldSpec("methodName", String.class);
+    private static final FieldSpec FIELD_SPEC_ARG_TYPES = new DefaultFieldSpec("argTypes", Class[].class);
+    private static final FieldSpec FIELD_SPEC_ARGS = new DefaultFieldSpec("args", Object[].class);
+    private static final FieldSpec[] TYPES = {FIELD_SPEC_METHOD_NAME, FIELD_SPEC_ARG_TYPES, FIELD_SPEC_ARGS};
 
     public void testCallSpec() {
         // FIXME: SC042 To field ... all of these.
--- src/test/java/au/net/netstorm/boost/util/reflect/MethodTestUtil.java
+++ src/test/java/au/net/netstorm/boost/util/reflect/MethodTestUtil.java
@@ -4,6 +4,7 @@ import java.lang.reflect.Method;
 
 public interface MethodTestUtil {
     Object invoke(Object invokee, String methodName, Object[] parameters);
+    // FIXME: SC042 Invoke with a CallSpec.
 
     Class getThrowsType(Method method);
 }
