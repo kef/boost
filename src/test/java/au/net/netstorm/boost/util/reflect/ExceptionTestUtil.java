c5a36b9007a445a2cf1fc5b7e7c1a1be93ff8b95
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2006-03-01 00:34:35 +0000

    SC042 - A tidy up pass on the reflection utilities.
    git-svn-id: https://geekscape.org/svn/boost/trunk@397 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/util/reflect/ExceptionTestUtil.java
+++ src/test/java/au/net/netstorm/boost/util/reflect/ExceptionTestUtil.java
@@ -1,5 +1,5 @@
 package au.net.netstorm.boost.util.reflect;
 
 public interface ExceptionTestUtil {
-    Class getRealExceptionClass(Throwable t); // FIXME: SC042 This belongs elsewhere (ExceptionTestUtil?).
+    Class getRealExceptionClass(Throwable t);
 }
