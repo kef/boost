23e3a9cd40c7b642bb38bd756ba7cdd3c2c296b5
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2006-03-02 04:23:21 +0000

    SC042 - A tidy up pass on the reflection utilities.
    git-svn-id: https://geekscape.org/svn/boost/trunk@503 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/util/reflect/DefaultFieldTestChecker.java
+++ src/test/java/au/net/netstorm/boost/util/reflect/DefaultFieldTestChecker.java
@@ -6,20 +6,22 @@ import java.lang.reflect.Modifier;
 import junit.framework.Assert;
 
 public final class DefaultFieldTestChecker implements FieldTestChecker {
-    private final FieldTestUtil fielder = new DefaultFieldTestUtil();
+    private final FieldTestUtil fielderUtil = new DefaultFieldTestUtil();
+    private final FieldTestChecker fieldChecker = new DefaultFieldTestChecker();
 
     // FIXME: SC042 Remove duplication and tidy.
     // FIXME: SC042 Add the modifier checks to FieldTestUtil.
     public void checkPrivateFinalField(Class type, String fieldName) {
-        Field field = fielder.getDeclared(type, fieldName);
+        Field field = fielderUtil.getDeclared(type, fieldName);
         int modifiers = field.getModifiers();
         if (!Modifier.isFinal(modifiers)) Assert.fail("Field '" + fieldName + "' should be declared final.");
         if (Modifier.isPublic(modifiers)) Assert.fail("Field '" + fieldName + "' should be declared private.");
         if (Modifier.isStatic(modifiers)) Assert.fail("Field '" + fieldName + "' cannot be static.");
     }
 
+    // FIXME: SC042 Is this ok?
     public void checkType(Class expectedType, Object ref, String fieldName) {
-        Object value = fielder.getInstance(ref, fieldName);
+        Object value = fielderUtil.getInstance(ref, fieldName);
         Class type = value.getClass();
         Assert.assertEquals(expectedType, type);
     }
