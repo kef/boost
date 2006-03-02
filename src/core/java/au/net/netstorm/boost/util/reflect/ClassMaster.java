839c1206c5b0d0b67a5c80e7d2de51b25bfdd423
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2006-03-02 00:29:01 +0000

    SC042 - A tidy up pass on the reflection utilities.
    git-svn-id: https://geekscape.org/svn/boost/trunk@472 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/util/reflect/ClassMaster.java
+++ src/core/java/au/net/netstorm/boost/util/reflect/ClassMaster.java
@@ -3,7 +3,7 @@ package au.net.netstorm.boost.util.reflect;
 import au.net.netstorm.boost.util.type.Interface;
 
 public interface ClassMaster {
-    String getShortName(Class cls);
-
     String getShortName(Interface iface);
+
+    String getShortName(Class cls);
 }
--- src/core/java/au/net/netstorm/boost/util/reflect/DefaultClassMaster.java
+++ src/core/java/au/net/netstorm/boost/util/reflect/DefaultClassMaster.java
@@ -3,14 +3,14 @@ package au.net.netstorm.boost.util.reflect;
 import au.net.netstorm.boost.util.type.Interface;
 
 public class DefaultClassMaster implements ClassMaster {
+    public String getShortName(Interface iface) {
+        Class type = iface.getType();
+        return getShortName(type);
+    }
+
     public String getShortName(Class cls) {
         String clsName = cls.getName();
         int lastPeriod = clsName.lastIndexOf('.');
         return clsName.substring(lastPeriod + 1);
-    }
-
-    public String getShortName(Interface iface) {
-        Class type = iface.getType();
-        return getShortName(type);
     }
 }
