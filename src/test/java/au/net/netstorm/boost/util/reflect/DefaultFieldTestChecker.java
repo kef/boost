5239fa28d6bc15b65e610128383be2c3b43eca28
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2006-03-02 06:12:22 +0000

    SC042 - A tidy up pass on the reflection utilities.
    git-svn-id: https://geekscape.org/svn/boost/trunk@510 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/util/reflect/DefaultFieldTestChecker.java
+++ src/test/java/au/net/netstorm/boost/util/reflect/DefaultFieldTestChecker.java
@@ -1,7 +1,6 @@
 package au.net.netstorm.boost.util.reflect;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
 
 import junit.framework.Assert;
 
@@ -9,23 +8,11 @@ public final class DefaultFieldTestChecker implements FieldTestChecker {
     private final FieldTestUtil fielderUtil = new DefaultFieldTestUtil();
     private final ModifierTestChecker modifiers = new DefaultModifierTestChecker();
 
-    // FIXME: SC042 Remove duplication and tidy.
-    // FIXME: SC042 Add the modifier checks to FieldTestUtil.
-    // FIXME: SC042 Rename.
     public void checkPrivateFinalInstanceField(Class type, String fieldName) {
-        Field field = fielderUtil.getDeclared(type, fieldName);
-        int modifiers = field.getModifiers();
-        if (!Modifier.isFinal(modifiers)) Assert.fail("Field '" + fieldName + "' should be declared final.");
-        if (Modifier.isPublic(modifiers)) Assert.fail("Field '" + fieldName + "' should be declared private.");
-        if (Modifier.isStatic(modifiers)) Assert.fail("Field '" + fieldName + "' cannot be static.");
-    }
-
-    // FIXME: SC042 Complete this.
-    private void xxx(Class type, String fieldName) {
         Field declared = fielderUtil.getDeclared(type, fieldName);
-//        modifiers.checkPrivate(declared);
+        modifiers.checkPrivate(declared);
         modifiers.checkFinal(declared);
-//        modifiers.checkInstance(declared);
+        modifiers.checkInstance(declared);
     }
 
     // FIXME: SC042 Is this ok?
