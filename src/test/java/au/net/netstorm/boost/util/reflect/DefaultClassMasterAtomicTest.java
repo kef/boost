ba9b501898391eeed20a414cfe000b6a3d45e446
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2006-03-02 00:20:45 +0000

    SC042 - A tidy up pass on the reflection utilities.
    git-svn-id: https://geekscape.org/svn/boost/trunk@469 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/util/reflect/DefaultClassMasterAtomicTest.java
+++ src/test/java/au/net/netstorm/boost/util/reflect/DefaultClassMasterAtomicTest.java
@@ -10,21 +10,28 @@ public class DefaultClassMasterAtomicTest extends TestCase {
     private final ClassMaster master = new DefaultClassMaster();
 
     public void testClass() {
-        checkEquals("Object", Object.class);
-        checkEquals("Proxy", Proxy.class);
-        checkEquals("String", String.class);
+        checkClass("Object", Object.class);
+        checkClass("Proxy", Proxy.class);
+        checkClass("String", String.class);
     }
 
     public void testInterface() {
         Class type = Remote.class;
-        Interface iface = new Interface(type);
-        String actual = master.getShortName(iface);
-        assertEquals("Remote", actual);
+        String expected = "Remote";
+        checkInterface(type, expected);
 //        checkEquals("Remote", Remote.class);
 //        checkEquals("Remote", Remote.class);
     }
 
-    private void checkEquals(String expected, Class cls) {
-        assertEquals(expected, master.getShortName(cls));
+    // FIXME: SC042 Dupe.
+    private void checkClass(String expected, Class cls) {
+        String shortName = master.getShortName(cls);
+        assertEquals(expected, shortName);
+    }
+
+    private void checkInterface(Class type, String expected) {
+        Interface iface = new Interface(type);
+        String shortName = master.getShortName(iface);
+        assertEquals(expected, shortName);
     }
 }
