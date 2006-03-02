c3b04b5a1dfd96263f14cda462ee47ba763d37b7
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2006-03-02 04:08:45 +0000

    SC042 - A tidy up pass on the reflection utilities.
    git-svn-id: https://geekscape.org/svn/boost/trunk@500 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/util/reflect/ModifierTestChecker.java
+++ src/test/java/au/net/netstorm/boost/util/reflect/ModifierTestChecker.java
@@ -2,10 +2,15 @@ package au.net.netstorm.boost.util.reflect;
 
 import java.lang.reflect.Member;
 
+// FIXME: SC042 Type up a story card for building a delegator (composer).
+
 public interface ModifierTestChecker {
     void checkFinal(Member member);
 
     void checkSynchronized(Member member);
+
+    // FIXME: SC042 Add checkStatic(member)
+    // FIXME: SC042 Add checkPublic(member)
 
     void checkPublic(Class cls);
 
