8e590db32c7212d7ba7f055df311131b8fb7cdef
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2006-03-03 06:16:59 +0000

    SC521 Create "Composer".
    git-svn-id: https://geekscape.org/svn/boost/trunk@523 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/compose/DefaultComposerAtomicTest.java
+++ src/test/java/au/net/netstorm/boost/compose/DefaultComposerAtomicTest.java
@@ -3,10 +3,12 @@ package au.net.netstorm.boost.compose;
 import junit.framework.TestCase;
 
 public final class DefaultComposerAtomicTest extends TestCase {
+    private final MockTestInterfaceOne mock1 = new MockTestInterfaceOne();
     // FIXME: SC521 Complete.
 
     // FIXME: SC521 Rename
     public void testSingle() {
+        mock1.init();
     }
 
     // FIXME: SC521 Complete.
