eb3bbdb99e822ef46863f5d38ba90ab0f543960f
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2006-03-03 06:27:55 +0000

    SC521 Create "Composer".
    git-svn-id: https://geekscape.org/svn/boost/trunk@526 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/compose/DefaultComposerAtomicTest.java
+++ src/test/java/au/net/netstorm/boost/compose/DefaultComposerAtomicTest.java
@@ -6,15 +6,16 @@ import junit.framework.TestCase;
 public final class DefaultComposerAtomicTest extends TestCase {
     private final MockTestInterfaceOne mock1 = new MockTestInterfaceOne();
     private final Composer composer = new DefaultComposer();
-    // FIXME: SC521 Complete.
 
+    // FIXME: SC521 Check exceptions are thrown across the boundary.
     // FIXME: SC521 Rename.
     // FIXME: SC521 Check implementations cannot change under our feet.  Maybe.  Yes we will need to.
     public void testSingle() {
         mock1.init();
         Interface iface = new Interface(TestInterfaceOne.class);
         Class[] implementations = {};
-        composer.compose(iface, implementations);
+        Object ref = composer.compose(iface, implementations);
+        Object composite = ref;
     }
 
     // FIXME: SC521 Complete.
