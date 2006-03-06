7b79794746f617e6457771765b914064e010c814
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2006-03-06 04:06:22 +0000

    SC521 Create "Composer".
    git-svn-id: https://geekscape.org/svn/boost/trunk@564 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/compose/DefaultComposer.java
+++ src/test/java/au/net/netstorm/boost/compose/DefaultComposer.java
@@ -4,6 +4,6 @@ import au.net.netstorm.boost.util.type.Interface;
 
 public final class DefaultComposer implements Composer {
     public Object compose(Interface iface, Object delegateA, Object delegateB) {
-        return null; // FIXME: RDSC124
+        return null; // FIXME: SC521
     }
 }
