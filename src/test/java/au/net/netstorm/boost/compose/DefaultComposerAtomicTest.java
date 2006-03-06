8719c5bd1da9c25b4cb6aedb0e6f0ff6bca6175c
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2006-03-06 01:46:35 +0000

    SC521 Create "Composer".
    git-svn-id: https://geekscape.org/svn/boost/trunk@540 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/compose/DefaultComposerAtomicTest.java
+++ src/test/java/au/net/netstorm/boost/compose/DefaultComposerAtomicTest.java
@@ -11,20 +11,24 @@ import junit.framework.TestCase;
 // FIXME: SC521 Check exceptions are thrown across the boundary.
 // FIXME: SC521 getProxy can be replaced with ProxyFactory.
 
+/**
+ * The composer currently only supports composition of two classes.
+ */
 public final class DefaultComposerAtomicTest extends TestCase {
     private final ProxyFactory proxyFactory = new EdgeProxyFactory();
     private final MockInvocationHandler mockHandlerA = new MockInvocationHandler();
     private final Composer composer = new DefaultComposer();
 
-    public void testUnsupported() {
-        // FIXME: SC521 Test failure modes for 0,1, 3 or more composed.
-    }
-
+    // FIXME: SC521 Check interface implements 2 subinterfaces only.
     // FIXME: SC521 Rename.
     // FIXME: SC521 Check implementations cannot change under our feet.  Maybe.  Yes we will need to.
     public void testDouble() {
-        mockHandlerA.init();
-        Object ref = getProxy(TestInterfaceA.class, mockHandlerA);
+        Object delegateA = createMockHandler(mockHandlerA);
+    }
+
+    private Object createMockHandler(MockInvocationHandler mockHandler) {
+        mockHandler.init();
+        return getProxy(TestInterfaceA.class, mockHandler);
     }
 
     private Object getProxy(Class iface, InvocationHandler handler) {
