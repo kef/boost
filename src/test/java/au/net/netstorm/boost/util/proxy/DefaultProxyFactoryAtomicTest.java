2cbf65e537d24bc9f34ec3d71e3a69d458755ba5
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2006-03-06 03:57:37 +0000

    SC521 Create "Composer".
    git-svn-id: https://geekscape.org/svn/boost/trunk@559 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/util/proxy/DefaultProxyFactoryAtomicTest.java
+++ src/test/java/au/net/netstorm/boost/util/proxy/DefaultProxyFactoryAtomicTest.java
@@ -9,17 +9,16 @@ import junit.framework.TestCase;
 // FIXME: SC521 Complete.
 
 public final class DefaultProxyFactoryAtomicTest extends TestCase {
-    private MockEdgeProxyFactory mockEdgeProxyFactory = new MockEdgeProxyFactory();
-    private final ProxyFactory factory = new DefaultProxyFactory(mockEdgeProxyFactory);
     private static final Interface TYPE_1 = new Interface(CharSequence.class);
+    private final MockEdgeProxyFactory mockEdgeProxyFactory = new MockEdgeProxyFactory();
+    private final ProxyFactory factory = new DefaultProxyFactory(mockEdgeProxyFactory);
+    private final InvocationHandler handler = new MockInvocationHandler();
 
     // FIXME: SC521 Triangulate on interface.
     public void testFactory() {
         Object result = new Object();
         mockEdgeProxyFactory.init(result);
-        Interface type = TYPE_1;
-        InvocationHandler handler = new MockInvocationHandler();
-        Object proxy = factory.newProxy(type, handler);
+        Object proxy = factory.newProxy(TYPE_1, handler);
         assertSame(result, proxy);
         // FIXME: SC521 Check return value.
         Class cls = factory.getClass();
