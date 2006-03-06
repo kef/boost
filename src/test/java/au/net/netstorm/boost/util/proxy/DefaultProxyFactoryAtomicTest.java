6c836b9f2bf45cd679ac8098e24a654949222375
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2006-03-06 04:02:01 +0000

    SC521 Create "Composer".
    git-svn-id: https://geekscape.org/svn/boost/trunk@561 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/util/proxy/DefaultProxyFactoryAtomicTest.java
+++ src/test/java/au/net/netstorm/boost/util/proxy/DefaultProxyFactoryAtomicTest.java
@@ -6,9 +6,6 @@ import java.util.Map;
 import au.net.netstorm.boost.util.type.Interface;
 import junit.framework.TestCase;
 
-// FIXME: SC521 Move into proxy package.
-// FIXME: SC521 Complete.
-
 public final class DefaultProxyFactoryAtomicTest extends TestCase {
     private static final Interface TYPE_1 = new Interface(CharSequence.class);
     private static final Interface TYPE_2 = new Interface(Map.class);
@@ -16,17 +13,16 @@ public final class DefaultProxyFactoryAtomicTest extends TestCase {
     private final ProxyFactory factory = new DefaultProxyFactory(mockEdgeProxyFactory);
     private final InvocationHandler handler = new MockInvocationHandler();
 
-    // FIXME: SC521 Triangulate on interface.
     public void testFactory() {
         checkFactory(TYPE_1);
         checkFactory(TYPE_2);
     }
 
     private void checkFactory(Interface type) {
-        Object result = new Object();
-        mockEdgeProxyFactory.init(result);
-        Object proxy = factory.newProxy(type, handler);
-        assertSame(result, proxy);
+        Object expected = new Object();
+        mockEdgeProxyFactory.init(expected);
+        Object result = factory.newProxy(type, handler);
+        assertSame(expected, result);
         checkCall(type);
     }
 
