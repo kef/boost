60540cf45a864de1d1348170b82a3d4051abd5a3
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2006-03-08 09:11:40 +0000

    SC999 Testing cruise build
    git-svn-id: https://geekscape.org/svn/boost/trunk@578 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/reflect/MethodTestUtil.java
+++ src/test/java/au/net/netstorm/boost/reflect/MethodTestUtil.java
@@ -2,7 +2,6 @@ package au.net.netstorm.boost.reflect;
 
 import java.lang.reflect.Method;
 
-// FIXME: SC999 Remove.  Just testing cruise build email messages.
 public interface MethodTestUtil {
     Object invoke(Object invokee, String methodName, Object[] parameters);
     Class getThrowsType(Method method);
