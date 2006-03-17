f2d89b322ec67073d0221bed5f99b45b484048b7
paul.okeeffe <paul.okeeffe@05db6a09-9903-0410-bedf-ba9bc4063e48>
2006-03-17 12:41:59 +0000

    SC524
    git-svn-id: https://geekscape.org/svn/boost/trunk@592 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/kickstart/practice/Coordinate.java
+++
@@ -1,6 +0,0 @@
-package au.net.netstorm.boost.kickstart.practice;
-
-class Coordinate {
-    public Coordinate(int x, int y) {
-    }
-}
--- src/test/java/au/net/netstorm/boost/kickstart/practice/CoordinateAtomicTest.java
+++
@@ -1,9 +0,0 @@
-package au.net.netstorm.boost.kickstart.practice;
-
-import junit.framework.TestCase;
-
-public final class CoordinateAtomicTest extends TestCase {
-    public void testConstructor() {
-        new Coordinate(0, 0);
-    }
-}
