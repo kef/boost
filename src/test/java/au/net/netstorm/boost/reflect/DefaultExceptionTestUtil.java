570ec3aaa0fa9b53b381394e2c44c9717a597d77
paul.okeeffe <paul.okeeffe@05db6a09-9903-0410-bedf-ba9bc4063e48>
2006-03-17 12:50:41 +0000

    SC524
    git-svn-id: https://geekscape.org/svn/boost/trunk@594 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/reflect/DefaultExceptionTestUtil.java
+++ src/test/java/au/net/netstorm/boost/reflect/DefaultExceptionTestUtil.java
@@ -13,4 +13,42 @@ public final class DefaultExceptionTestUtil implements ExceptionTestUtil {
             realException = realException.getCause(); // FIXME: SC050 Bloody amateurs.  This should be getTargetInvocation ...
         return realException.getClass();
     }
+
+    // FIXME: SC524 Build up Boost version of this.
+//    private static final String MESSAGE_1 = "message 1";
+//    private static final String MESSAGE_2 = "message 2";
+//
+//    public static void checkException(Class cls) {
+//        ClassPropertiesTestUtil.checkSubclassOf(RuntimeException.class, cls);
+//        ClassPropertiesTestUtil.checkClassFinal(cls);
+//        checkException(cls, MESSAGE_1);
+//        checkException(cls, MESSAGE_2);
+//    }
+//
+//    public static void checkExceptionWrapping(Throwable expectedWrapped, String expectedMessage, Throwable actualWrapper) {
+//        checkExceptionWrapping(expectedWrapped, actualWrapper);
+//        checkMessage(expectedMessage, actualWrapper);
+//    }
+//
+//    public static void checkExceptionWrapping(Throwable expectedWrapped, Throwable actualWrapper) {
+//        Assert.assertEquals(expectedWrapped, actualWrapper.getCause());
+//    }
+//
+//    private static void checkMessage(String expectedMessage, Throwable actualWrapper) {
+//        Assert.assertEquals("Incorrect message", expectedMessage, actualWrapper.getMessage());
+//    }
+//
+//    private static void checkException(Class cls, String message) {
+//        RuntimeException e = createException(cls, message);
+//        Assert.assertEquals(message, e.getMessage());
+//    }
+//
+//    private static RuntimeException createException(Class cls, String message) {
+//        try {
+//            Constructor constructor = cls.getConstructor(new Class[] {String.class});
+//            return (RuntimeException) constructor.newInstance(new Object[] {message});
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
 }
--- src/test/java/au/net/netstorm/boost/test/checker/NullParameterTestChecker.java
+++ src/test/java/au/net/netstorm/boost/test/checker/NullParameterTestChecker.java
@@ -2,56 +2,131 @@ package au.net.netstorm.boost.test.checker;
 
 import java.lang.reflect.Constructor;
 
-import au.net.netstorm.boost.reflect.DefaultExceptionTestUtil;
-import au.net.netstorm.boost.reflect.ExceptionTestUtil;
 import au.net.netstorm.boost.util.fixture.InstanceProvider;
-import au.net.netstorm.boost.util.fixture.InstanceProviderTestUtil;
-import junit.framework.Assert;
 
+// FIXME: SC524 Build up Boost version of this.
 // FIXME: SC523 Pull in latest external changes.
 public final class NullParameterTestChecker {
-    private static final InstanceProviderTestUtil INSTANCE_PROVIDER_TEST_UTIL = new InstanceProviderTestUtil();
-    private static final ExceptionTestUtil EXCEPTION_TEST_UTIL = new DefaultExceptionTestUtil();
+//    private static final InstanceProviderTestUtil INSTANCE_PROVIDER_TEST_UTIL = new InstanceProviderTestUtil();
+//    private static final ExceptionTestUtil EXCEPTION_TEST_UTIL = new DefaultExceptionTestUtil();
 
     // FIXME: SC050 Given we're seeing a lot of the constructor/parameters together, isn't it about time to build an aggregate?
     // FIXME: SC523 Rename additional.
     public void checkNullConstructorParameters(Constructor constructor, Class[] parameterTypes, InstanceProvider additional) {
-        for (int i = 0; i < parameterTypes.length; i++) {
-            if (!parameterTypes[i].isPrimitive()) {
-                checkNullParameter(constructor, parameterTypes, i, additional);
-            }
-        }
+//        for (int i = 0; i < parameterTypes.length; i++) {
+//            if (!parameterTypes[i].isPrimitive()) {
+//                checkNullParameter(constructor, parameterTypes, i, additional);
+//            }
+//        }
     }
 
-    private void checkNullParameter(Constructor constructor, Class[] parameterTypes, int currentParameter, InstanceProvider additional) {
-        Object[] parameters = getParamsWithNull(parameterTypes, currentParameter, additional);
-        try {
-            INSTANCE_PROVIDER_TEST_UTIL.getInstance(constructor, parameters);
-            Class parameterType = parameterTypes[currentParameter];
-            String message = "NULLs are evil!!! This object allows null for argument number " + (currentParameter + 1) +
-                    " of type " + parameterType;
-            Assert.fail(message);
-        } catch (RuntimeException e) {
-            checkExpected(e);
-        }
-    }
-
-    private void checkExpected(RuntimeException e) {
-        if (!isExpected(e)) throw e;
-    }
-
-    private Object[] getParamsWithNull(Class[] parameterTypes, int currentParameter, InstanceProvider additional) {
-        Object[] parameters = INSTANCE_PROVIDER_TEST_UTIL.getInstances(parameterTypes, additional);
-        parameters[currentParameter] = null;
-        return parameters;
-    }
-
-    private boolean isExpected(RuntimeException e) {
-        Class cls = EXCEPTION_TEST_UTIL.getRealExceptionClass(e); // FIXME: SC050 Is this needed?
-        return isIllegalArgumentException(cls);
-    }
-
-    private boolean isIllegalArgumentException(Class cls) {
-        return cls.equals(IllegalArgumentException.class);
-    }
+//    private void checkNullParameter(Constructor constructor, Class[] parameterTypes, int currentParameter, InstanceProvider additional) {
+//        Object[] parameters = getParamsWithNull(parameterTypes, currentParameter, additional);
+//        try {
+//            INSTANCE_PROVIDER_TEST_UTIL.getInstance(constructor, parameters);
+//            Class parameterType = parameterTypes[currentParameter];
+//            String message = "NULLs are evil!!! This object allows null for argument number " + (currentParameter + 1) +
+//                    " of type " + parameterType;
+//            Assert.fail(message);
+//        } catch (RuntimeException e) {
+//            checkExpected(e);
+//        }
+//        ThrowAssert.assertThrows(IllegalArgumentException.class, cls, parameters, message);
+//    }
+//
+//    private void checkExpected(RuntimeException e) {
+//        if (!isExpected(e)) throw e;
+//    }
+//
+//    private Object[] getParamsWithNull(Class[] parameterTypes, int currentParameter, InstanceProvider additional) {
+//        Object[] parameters = INSTANCE_PROVIDER_TEST_UTIL.getInstances(parameterTypes, additional);
+//        parameters[currentParameter] = null;
+//        return parameters;
+//    }
+//
+//    private boolean isExpected(RuntimeException e) {
+//        Class cls = EXCEPTION_TEST_UTIL.getRealExceptionClass(e); // FIXME: SC050 Is this needed?
+//        return isIllegalArgumentException(cls);
+//    }
+//
+//    private boolean isIllegalArgumentException(Class cls) {
+//        return cls.equals(IllegalArgumentException.class);
+//    }
+//}
+//
+//final class ThrowAssert {
+//    public static void assertThrows(Class expectedException, Class implementation, Object[] parameterValues,
+//                                      String assertionMessage) {
+//        ExceptionResult result = tryCall(new ConstructionBlock(implementation, parameterValues), expectedException);
+//        Assert.assertTrue(assertionMessage, result.getExceptionThrown());
+//    }
+//
+//    private static ExceptionResult tryCall(Block block, Class throwable) {
+//        try {
+//            block.call();
+//            return new ExceptionResult(false);
+//        } catch (Throwable t) {
+//            return correctExceptionThrown(throwable, t);
+//        }
+//    }
+//
+//    private static ExceptionResult correctExceptionThrown(Class throwable, Throwable t) {
+//        checkException(throwable, t);
+//        return new ExceptionResult(true);
+//    }
+//
+//    private static void checkException(Class expectedType, Throwable t) {
+//        if (!isExpectedException(expectedType, t)) {
+//            fail(expectedType, t);
+//        }
+//    }
+//
+//    private static void fail(Class expectedType, Throwable t) {
+//        AssertionFailedError error =
+//                new AssertionFailedError("Expected exception of type '" + expectedType.getName() + "' but exception was " + t);
+//        error.initCause(t);
+//        throw error;
+//    }
+//
+//    private static boolean isExpectedException(Class expectedType, Throwable t) {
+//        return expectedType.isAssignableFrom(t.getClass());
+//    }
+//
+//    private static final class ExceptionResult {
+//        private final boolean exceptionThrown;
+//
+//        private ExceptionResult(boolean expectedThrowableThrown) {
+//            this.exceptionThrown = expectedThrowableThrown;
+//        }
+//
+//        private boolean getExceptionThrown() {
+//            return exceptionThrown;
+//        }
+//    }
+//}
+//
+//interface Block {
+//    void call() throws Throwable;
+//}
+//
+//class ConstructionBlock implements Block {
+//    private final Class implementation;
+//    private final Object[] instances;
+//
+//    public ConstructionBlock(Class implementation, Object[] instances) {
+//        this.implementation = implementation;
+//        this.instances = instances;
+//    }
+//
+//    public void call() throws Throwable {
+//        Constructor constructor = new ReflectConstructorUtil().getConstructor(implementation);
+//        constructor.setAccessible(true);
+//        try {
+//            constructor.newInstance(instances);
+//        } catch (InstantiationException e) {
+//            throw e.getCause();
+//        } catch (InvocationTargetException e) {
+//            throw e.getCause();
+//        }
+//    }
 }
