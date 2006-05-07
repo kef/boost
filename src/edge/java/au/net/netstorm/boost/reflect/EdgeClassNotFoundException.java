2359cbd2be1b4d84b895b2549a517ce10b68784f
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2006-05-07 10:25:25 +0000

    git-svn-id: https://geekscape.org/svn/boost/trunk@654 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/edge/java/au/net/netstorm/boost/reflect/EdgeClassNotFoundException.java
+++ src/edge/java/au/net/netstorm/boost/reflect/EdgeClassNotFoundException.java
@@ -1,6 +1,6 @@
 package au.net.netstorm.boost.reflect;
 
-// FIXME: SC502 Atomic test.
+// FIXME: SC043 Extend EdgeException which extends RuntimeException
 
 public final class EdgeClassNotFoundException extends RuntimeException {
     public EdgeClassNotFoundException(ClassNotFoundException e) {
