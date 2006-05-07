d59c9232dd5ebb0b4ebc2a903f8d8ab7de39821c
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2006-05-07 08:00:23 +0000

    git-svn-id: https://geekscape.org/svn/boost/trunk@647 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/test/suite/AllTestSuite.java
+++ src/test/java/au/net/netstorm/boost/test/suite/AllTestSuite.java
@@ -4,6 +4,8 @@ import au.net.netstorm.boost.test.primordial.PrimordialTestCase;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
+// FIXME: SC043 Sort out hang when we run this inside the IDE.
+
 public class AllTestSuite extends PrimordialTestCase {
     public static Test suite() {
         TestSuite suite = new TestSuite("The Whole Box and Dice");
