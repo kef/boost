9dba738841ed2da6570f61f6a629e1901d51d8b2
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2006-05-07 11:48:28 +0000

    SC043
    git-svn-id: https://geekscape.org/svn/boost/trunk@677 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/test/suite/AllTestSuite.java
+++ src/test/java/au/net/netstorm/boost/test/suite/AllTestSuite.java
@@ -5,6 +5,7 @@ import junit.framework.Test;
 import junit.framework.TestSuite;
 
 // FIXME: SC043 Sort out hang when we run this inside the IDE.
+// FIXME: SC043 Boost does not need all of these, but most project do/might.  Rationalise this.
 
 public class AllTestSuite extends PrimordialTestCase {
     public static Test suite() {
