a72b6034cb5adc2d6ad1e407a3c34d4922c93dfa
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2006-05-07 10:38:01 +0000

    git-svn-id: https://geekscape.org/svn/boost/trunk@657 05db6a09-9903-0410-bedf-ba9bc4063e48

---
+++ src/edge/java/au/net/netstorm/boost/lang/EdgeSystem.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) 2005 Transtoll Pty Limited.
+ */
+package au.net.netstorm.boost.lang;
+
+public final class EdgeSystem implements System {
+    public String getProperty(String key) {
+        return java.lang
+                .System
+                .getProperty(key);
+    }
+}
--- src/edge/java/au/net/netstorm/boost/reflect/EdgeClassNotFoundException.java
+++ src/edge/java/au/net/netstorm/boost/reflect/EdgeClassNotFoundException.java
@@ -2,8 +2,6 @@ package au.net.netstorm.boost.reflect;
 
 import au.net.netstorm.boost.misc.EdgeException;
 
-// FIXME: SC043 Extend EdgeException which extends RuntimeException
-
 public final class EdgeClassNotFoundException extends EdgeException {
     public EdgeClassNotFoundException(ClassNotFoundException e) {
         super(e);
