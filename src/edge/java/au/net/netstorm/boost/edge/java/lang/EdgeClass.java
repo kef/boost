8d1c0f8be650c64404a465060d4133baa9368999
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2006-07-31 12:34:07 +0000

    SC600
    git-svn-id: https://geekscape.org/svn/boost/trunk@782 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/edge/java/au/net/netstorm/boost/edge/java/lang/EdgeClass.java
+++ src/edge/java/au/net/netstorm/boost/edge/java/lang/EdgeClass.java
@@ -1,5 +1,14 @@
 package au.net.netstorm.boost.edge.java.lang;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 public interface EdgeClass {
     Object newInstance();
+
+    Method getMethod(String methodName, Class[] parameterTypes);
+
+    Field getDeclaredField(String fieldName);
 }
+// FIXME: SC600 Evaluate SUGGEST.  The more I see the more I think we do this.
+// SUGGEST: Return EdgeMethod, EdgeField...  Requires across the board change to utilities.
