f7bf0c3a07f120b4d167312ca84685286093fcda
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2006-07-31 13:43:09 +0000

    SC600
    git-svn-id: https://geekscape.org/svn/boost/trunk@792 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/edge/java/au/net/netstorm/boost/edge/java/lang/reflect/EdgeReflect.java
+++ src/edge/java/au/net/netstorm/boost/edge/java/lang/reflect/EdgeReflect.java
@@ -9,12 +9,15 @@ import java.lang.reflect.Method;
 // FIX SC600 Move all these methods out.
 
 public interface EdgeReflect {
+    // FIX SC600 EdgeField
     Object getFieldValue(Field field, Object ref);
 
     void setFieldValue(Field field, Object ref, Object value);
 
+    // FIX SC600 EdgeConstructor.
     Object newInstance(Constructor constructor, Object[] parameters);
 
+    // FIX SC600 EdgeMethod.
     Object invoke(Method method, Object instance);
 
     Object invoke(Method method, Object instance, Object[] args);
