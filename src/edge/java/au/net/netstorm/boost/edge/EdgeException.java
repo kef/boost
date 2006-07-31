9ffcffbe55eacad32740157603642f931910a250
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2006-07-31 11:25:21 +0000

    SC600
    git-svn-id: https://geekscape.org/svn/boost/trunk@769 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/edge/java/au/net/netstorm/boost/edge/EdgeException.java
+++ src/edge/java/au/net/netstorm/boost/edge/EdgeException.java
@@ -1,9 +1,5 @@
 package au.net.netstorm.boost.edge;
 
-// FIXME: SC043 Checkstyle pattern to exclude RuntimeException.
-// FIXME: SC043 Remove the INSTANCE (or EDGE_CONSTRUCTOR) stuff.
-// FIXME: SC043 Turn off simian checks.
-
 public final class EdgeException extends RuntimeException {
     public EdgeException(Throwable throwable) {
         super(throwable);
