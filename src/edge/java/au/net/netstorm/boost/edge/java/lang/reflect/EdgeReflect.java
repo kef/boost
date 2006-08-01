55ae81c0ad44939d7c66cf2bbe3b322f9cac601f
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2006-08-01 11:59:44 +0000

    SC600
    git-svn-id: https://geekscape.org/svn/boost/trunk@796 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/edge/java/au/net/netstorm/boost/edge/java/lang/reflect/EdgeReflect.java
+++ src/edge/java/au/net/netstorm/boost/edge/java/lang/reflect/EdgeReflect.java
@@ -4,8 +4,6 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
-// SUGGEST: This stuff will probably follow the same sort of pattern as the MemberTestUtil, ClassTestUtil...
-// SUGGEST: Maybe follow the java.lang. package structure.
 // FIX SC600 Move all these methods out.
 
 public interface EdgeReflect {
