a2803477d6c5f941e53ac7a96bb8e89613e7ff56
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2006-08-02 13:04:35 +0000

    SC600
    git-svn-id: https://geekscape.org/svn/boost/trunk@812 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/util/tostring/ToStringMaster.java
+++ src/core/java/au/net/netstorm/boost/util/tostring/ToStringMaster.java
@@ -6,8 +6,9 @@ package au.net.netstorm.boost.util.tostring;
 // FIX SC506 Comb and replace string literals.
 // FIX SC506 Have a good hard look at and remove the "fixtures" package.  This stuff is smelly cheese.
 // FIX SC509 Primordial extenders need to be final.  Put this check into primordial.
-// FIX SC509 Throw exception from clone().
+// FIX SC509 Throw exception from clone() in primordial.
 // FIX SC506 Minimal braces everywhere.
+
 public interface ToStringMaster {
     String getString(Object ref);
 }
