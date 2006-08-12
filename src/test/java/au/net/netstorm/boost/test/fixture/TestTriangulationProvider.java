f33cb6022a67a95d820f0e8b37e9de4be8be4d0b
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2006-08-12 11:46:24 +0000

    SC600
    git-svn-id: https://geekscape.org/svn/boost/trunk@822 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/test/fixture/TestTriangulationProvider.java
+++ src/test/java/au/net/netstorm/boost/test/fixture/TestTriangulationProvider.java
@@ -1,8 +1,20 @@
 package au.net.netstorm.boost.test.fixture;
 
-import au.net.netstorm.boost.util.exception.NotImplementedException;
+import java.lang.reflect.InvocationHandler;
 
+import au.net.netstorm.boost.util.exception.NotImplementedException;
+import au.net.netstorm.boost.util.proxy.ProxyFactory;
+import au.net.netstorm.boost.util.proxy.DefaultProxyFactory;
+import au.net.netstorm.boost.util.type.Interface;
+import au.net.netstorm.boost.edge.java.lang.reflect.EdgeProxy;
+import au.net.netstorm.boost.edge.java.lang.reflect.DefaultEdgeProxy;
+
+// FIXME: SC600 Create demo.
 public final class TestTriangulationProvider implements TriangulationProvider {
+    private EdgeProxy edgeProxy = new DefaultEdgeProxy();
+    private ProxyFactory proxyFactory = new DefaultProxyFactory(edgeProxy);
+    private static final InvocationHandler EMTPY_HANDLER = null;
+
     public Object getInstance(Class type) {
         Object ref = doGetInstance(type);
         if (ref != null) return ref;
@@ -15,6 +27,13 @@ public final class TestTriangulationProvider implements TriangulationProvider {
     }
 
     private Object doGetInstance(Class type) {
+        if (type.isInterface()) return getInterface(type);
+        // FIXME: SC600 return Primitive types.
         throw new NotImplementedException();
+    }
+
+    private Object getInterface(Class type) {
+        Interface iface = new Interface(type);
+        return proxyFactory.newProxy(iface, EMTPY_HANDLER);
     }
 }
