6a151de0ee9a11a767cda6ca0c3000cbdabaa08f
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2006-08-12 21:33:36 +0000

    SC600
    git-svn-id: https://geekscape.org/svn/boost/trunk@832 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/test/fixture/TriangulationProviderTestUtil.java
+++ src/test/java/au/net/netstorm/boost/test/fixture/TriangulationProviderTestUtil.java
@@ -1,12 +1,6 @@
 package au.net.netstorm.boost.test.fixture;
 
-import java.lang.reflect.Array;
 import java.lang.reflect.Constructor;
-
-import au.net.netstorm.boost.edge.java.lang.reflect.DefaultEdgeConstructor;
-import au.net.netstorm.boost.edge.java.lang.reflect.EdgeConstructor;
-import au.net.netstorm.boost.reflect.DefaultReflectMaster;
-import au.net.netstorm.boost.reflect.ReflectMaster;
 
 // FIX SC600 This tidies up as TestTriangulationProvider is stitched in.
 // FIX SC502 Move instances to their respective unit tests (as per POK theory).
@@ -14,15 +8,14 @@ import au.net.netstorm.boost.reflect.ReflectMaster;
 // FIX SC502 Rename.
 
 public final class TriangulationProviderTestUtil {
-    private final ReflectMaster reflectMaster = new DefaultReflectMaster();
-    private final EdgeConstructor edgeConstructor = new DefaultEdgeConstructor();
     private final TriangulationProvider knownTypes = new TestTriangulationProvider();
 
+    // FIX SC600 Parameter "additional" is not used.
     // FIX SC050 ? Rename occurrences of "additional" to "extra".
     // FIX SC050 BREADCRUMB - Incorporate "additional"
     public Object[] getInstances(Class[] types, TriangulationProvider additional) {
         Object[] params = new Object[types.length];
-        for (int i = 0; i < types.length; i++) params[i] = getInstance(types[i], additional);
+        for (int i = 0; i < types.length; i++) params[i] = getInstance(types[i]);
         return params;
     }
 
@@ -39,37 +32,7 @@ public final class TriangulationProviderTestUtil {
         }
     }
 
-    private Object getInstance(Class type, TriangulationProvider additional) {
+    private Object getInstance(Class type) {
         return knownTypes.getInstance(type);
-    }
-
-    // FIX BREADCRUMB A whole bunch of code goes.  Follow the yellow.
-
-    private Object getArrayInstance(Class type) {
-        return Array.newInstance(type.getComponentType(), 0);
-    }
-
-    private Object getPrimitiveInstance(Class type) {
-        if (type == int.class) return new Integer(34177239);
-        if (type == long.class) return new Long(44762654617L);
-        if (type == float.class) return new Float(31.123475);
-//        if (type == boolean.class) return randomBoolean();
-        throw new UnsupportedOperationException("Please honey pie write the code for primitive type " + type);
-    }
-
-    // FIX SC050.  Reinstate when FIXMEs are complete.
-//    private Boolean randomBoolean() {
-//        // FIX SC050 This ties in with the target approach to triangulation.  Talk to group.
-//        // FIX SC043 R Use random.  Not TIME.
-//        boolean result = (System.currentTimeMillis() % 2) == 0;
-//        return Boolean.valueOf(result);
-//    }
-
-    private Object getDataInstance(Class type, TriangulationProvider additional) {
-        Constructor constructor = reflectMaster.getConstructor(type);
-        Class[] parameterTypes = constructor.getParameterTypes();
-        Object[] params = getInstances(parameterTypes, additional);
-        // FIX SC050 This barfs if the constructor is not "public".  Should be able to lower scope.
-        return edgeConstructor.newInstance(constructor, params);
     }
 }
