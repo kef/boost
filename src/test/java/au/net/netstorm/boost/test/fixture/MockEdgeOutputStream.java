fd1ac6071afe2ffc80206f46757b643ffc469e09
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2006-08-12 12:27:48 +0000

    SC600
    git-svn-id: https://geekscape.org/svn/boost/trunk@827 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/test/fixture/MockEdgeOutputStream.java
+++ src/test/java/au/net/netstorm/boost/test/fixture/MockEdgeOutputStream.java
@@ -3,6 +3,7 @@ package au.net.netstorm.boost.test.fixture;
 import au.net.netstorm.boost.edge.java.io.EdgeOutputStream;
 import au.net.netstorm.boost.util.exception.NotImplementedException;
 
+// FIXME: SC600 Not used.  Delete.
 public final class MockEdgeOutputStream implements EdgeOutputStream {
     public void write(byte[] bytes) {
         throw new NotImplementedException();
