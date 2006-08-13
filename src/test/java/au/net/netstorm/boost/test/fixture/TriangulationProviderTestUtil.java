efcde5d38c2f00c24c976f597e8ab77c23eddcc6
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2006-08-13 10:06:15 +0000

    SC600
    git-svn-id: https://geekscape.org/svn/boost/trunk@847 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/test/fixture/TriangulationProviderTestUtil.java
+++ src/test/java/au/net/netstorm/boost/test/fixture/TriangulationProviderTestUtil.java
@@ -2,6 +2,9 @@ package au.net.netstorm.boost.test.fixture;
 
 import java.lang.reflect.Constructor;
 
+import au.net.netstorm.boost.edge.java.lang.reflect.DefaultEdgeConstructor;
+import au.net.netstorm.boost.edge.java.lang.reflect.EdgeConstructor;
+
 // FIX SC600 This tidies up as TestTriangulationProvider is stitched in.
 // FIX SC502 Move instances to their respective unit tests (as per POK theory).
 // FIX SC502 Interface it.
@@ -9,6 +12,7 @@ import java.lang.reflect.Constructor;
 
 public final class TriangulationProviderTestUtil {
     private final TriangulationProvider knownTypes = new TestTriangulationProvider();
+    private final EdgeConstructor edgeConstructor = new DefaultEdgeConstructor();
 
     // FIX SC600 Parameter "additional" is not used.
     // FIX SC050 ? Rename occurrences of "additional" to "extra".
@@ -19,17 +23,10 @@ public final class TriangulationProviderTestUtil {
         return params;
     }
 
+    // FIX SC600 This goes now.
     public Object getInstance(Constructor constructor, Object[] parameters) {
-        try {
-            // FIX SC523 Edge class exists now, use it. Is the exception message below meaningful?
-            // FIX SC050 ? use an edge class.
-            constructor.setAccessible(true);
-            return constructor.newInstance(parameters);
-        } catch (Exception e) {
-            throw new RuntimeException("Constructor does not match expected.", e);
-            // FIX SC050 This is NOT always the case.  If the object throws ANY exception in the constructor, this will be reported.  FIX!!!
-            // FIX SC050 Ensure ALL Mock* are serializable.  Required if they are to be used in testing DATA objects which in turn may need to be serializable.
-        }
+        constructor.setAccessible(true);
+        return edgeConstructor.newInstance(constructor, parameters);
     }
 
     private Object getInstance(Class type) {
