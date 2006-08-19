ebfee7019a608ab1922a0fd6859a35f46c6e7a07
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2006-08-19 07:02:47 +0000

    SC600
    git-svn-id: https://geekscape.org/svn/boost/trunk@906 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/demo/java/au/net/netstorm/boost/demo/data/DataDemoTest.java
+++ src/demo/java/au/net/netstorm/boost/demo/data/DataDemoTest.java
@@ -9,7 +9,6 @@ import junit.framework.TestCase;
 
 public final class DataDemoTest extends TestCase {
     private DataTestChecker dataChecker = new DefaultDataTestChecker();
-    private static final FieldSpec[] NO_FIELDS = {};
     private static final FieldSpec STRING_PROPERTY = new DefaultFieldSpec("guitar", String.class);
     private static final FieldSpec[] SINGLE_STRING_PROPERTY = {STRING_PROPERTY};
     private static final String MESSAGE_METHODS_MUST_BE_PUBLIC_INSTANCE_OR_PRIVATE = "All methods must be public non-static or private.  Method getGuitar() violates this constraint.";
@@ -22,7 +21,7 @@ public final class DataDemoTest extends TestCase {
 
     // FIX SC600 Test only a single constructor.
     public void testBadDataAtoms() {
-        checkData(NotPrimordialData.class, NO_FIELDS, "NotPrimordialData is not a subclass of Primordial");
+        checkData(NotPrimordialData.class, "NotPrimordialData is not a subclass of Primordial");
         checkData(ConstructorParameterCountMismatchData.class, "Constructor must have 1 argument(s).  Instead it appears to have 0 arguments(s).");
         checkData(ConstructorParameterMismatchData.class, "For constructor parameter 0 we expected:<class java.lang.String> but was:<class java.lang.Integer>");
         checkData(ProtectedMethodsIllegalData.class, MESSAGE_METHODS_MUST_BE_PUBLIC_INSTANCE_OR_PRIVATE);
@@ -35,12 +34,8 @@ public final class DataDemoTest extends TestCase {
     }
 
     private void checkData(Class cls, String expectedMsg) {
-        checkData(cls, SINGLE_STRING_PROPERTY, expectedMsg);
-    }
-
-    private void checkData(Class cls, FieldSpec[] fields, String expectedMsg) {
         try {
-            dataChecker.checkIsData(cls, fields);
+            dataChecker.checkIsData(cls, SINGLE_STRING_PROPERTY);
             barf();
         } catch (AssertionFailedError e) {
             checkMessage(e, expectedMsg);
