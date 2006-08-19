8ad73ad20b713d39ff1ba33889327f43ed91eb8b
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2006-08-19 06:24:49 +0000

    SC600
    git-svn-id: https://geekscape.org/svn/boost/trunk@900 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/test/atom/DefaultClassMethodUtil.java
+++ src/test/java/au/net/netstorm/boost/test/atom/DefaultClassMethodUtil.java
@@ -2,38 +2,75 @@ package au.net.netstorm.boost.test.atom;
 
 import au.net.netstorm.boost.test.reflect.util.DefaultModifierTestUtil;
 import au.net.netstorm.boost.test.reflect.util.ModifierTestUtil;
-import au.net.netstorm.boost.util.exception.NotImplementedException;
 
 import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 // FIX SC600 Candidate for reflect.util.
 
 final class DefaultClassMethodUtil implements ClassMethodUtil {
+    // FUTURE: This only works for classes which inherit java.lang.Object (or Primordial).  Should work for all classes.
+    private final Set inherited = new HashSet();
     private ModifierTestUtil modifierUtil = new DefaultModifierTestUtil();
+
+    {
+        inherited.add("toString");
+        inherited.add("hashCode");
+        inherited.add("equals");
+    }
 
     public Method[] getAll(Class cls) {
         return cls.getDeclaredMethods();
     }
 
     public Method[] getAllPublicInstance(Class cls) {
-        Method[] all = getAll(cls);
-        List list = Arrays.asList(all);
-        HashSet result = new HashSet(list);
-        for (int i = 0; i < all.length; i++) {
-            Method method = all[i];
-            if (!modifierUtil.isPublicInstance(method)) result.remove(method);
-        }
-        return (Method[]) result.toArray(new Method[]{});
+        Set result = getAllAsSet(cls);
+        keepPublicInstance(result);
+        return methods(result);
     }
 
     public Method[] getAllNonInherited(Class cls) {
-        throw new NotImplementedException();
+        Set result = getAllAsSet(cls);
+        keepNonInherited(result);
+        return methods(result);
     }
 
     public Method[] getAllNotInheritedPublicInstance(Class cls) {
-        throw new NotImplementedException();
+        HashSet result = getAllAsSet(cls);
+        keepNonInherited(result);
+        keepPublicInstance(result);
+        return methods(result);
+    }
+
+    private HashSet getAllAsSet(Class cls) {
+        Method[] all = getAll(cls);
+        List list = Arrays.asList(all);
+        return new HashSet(list);
+    }
+
+    private void keepPublicInstance(Set set) {
+        Method[] methods = methods(set);
+        for (int i = 0; i < methods.length; i++) keepPublicInstance(set, methods[i]);
+    }
+
+    private void keepNonInherited(Set set) {
+        Method[] methods = methods(set);
+        for (int i = 0; i < methods.length; i++) keepNonInherited(set, methods[i]);
+    }
+
+    private void keepPublicInstance(Set set, Method method) {
+        if (!modifierUtil.isPublicInstance(method)) set.remove(method);
+    }
+
+    private void keepNonInherited(Set set, Method method) {
+        String name = method.getName();
+        if (inherited.contains(name)) set.remove(method);
+    }
+
+    private Method[] methods(Set set) {
+        return (Method[]) set.toArray(new Method[]{});
     }
 }
