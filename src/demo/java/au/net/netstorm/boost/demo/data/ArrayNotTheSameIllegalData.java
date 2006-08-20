c4717724683e30388ed05e615bc6fd7f6d02448d
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2006-08-20 06:57:46 +0000

    SC600
    git-svn-id: https://geekscape.org/svn/boost/trunk@994 05db6a09-9903-0410-bedf-ba9bc4063e48

---
+++ src/demo/java/au/net/netstorm/boost/demo/data/ArrayNotTheSameIllegalData.java
@@ -0,0 +1,23 @@
+package au.net.netstorm.boost.demo.data;
+
+import au.net.netstorm.boost.primordial.Primordial;
+import au.net.netstorm.boost.util.type.Data;
+
+final class ArrayNotTheSameIllegalData extends Primordial implements Data {
+    private final Integer[] integers;
+
+    public ArrayNotTheSameIllegalData(Integer[] integers) {
+        validate(integers);
+        this.integers = (Integer[]) integers.clone();
+    }
+
+    public Integer[] getIntegers() {
+        Integer[] result = (Integer[]) integers.clone();
+        result[0] = Integer.valueOf(42);
+        return result;
+    }
+
+    private void validate(Object ref) {
+        if (ref == null) throw new IllegalArgumentException();
+    }
+}
