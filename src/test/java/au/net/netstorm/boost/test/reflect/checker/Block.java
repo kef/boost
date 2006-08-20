2b11275b2240b47bd3bbe4faa6c2d5e6e300367c
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2006-08-20 08:35:00 +0000

    SC600
    git-svn-id: https://geekscape.org/svn/boost/trunk@1008 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/test/reflect/checker/Block.java
+++ src/test/java/au/net/netstorm/boost/test/reflect/checker/Block.java
@@ -2,7 +2,7 @@ package au.net.netstorm.boost.test.reflect.checker;
 
 // FIX SC043 R Remove.  Use Call instead.
 // FIX SC600 Move items which are not checkers out of this package.
-// FIX SC600 Shouldn't this be a TestBlock?
+// FIX SC600 Shouldn't this be a TestBlock.
 public interface Block {
     void execute();
 }
