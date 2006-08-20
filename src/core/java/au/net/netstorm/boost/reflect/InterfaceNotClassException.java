e161b385c9b1378ca96e1929920261fa12ee78c6
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2006-08-20 09:58:54 +0000

    SC600
    git-svn-id: https://geekscape.org/svn/boost/trunk@1021 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/reflect/InterfaceNotClassException.java
+++ src/core/java/au/net/netstorm/boost/reflect/InterfaceNotClassException.java
@@ -1,8 +1,5 @@
 package au.net.netstorm.boost.reflect;
 
-// FIX SC502 Need some code which ensures there is a corresponding Atomic test for each class.
-// FIX SC502 Test drive this.
-
 public class InterfaceNotClassException extends RuntimeException {
     public InterfaceNotClassException(Class cls) {
         super(cls.toString());
