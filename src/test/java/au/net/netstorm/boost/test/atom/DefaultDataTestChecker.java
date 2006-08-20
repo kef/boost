f81e6734e30d3ea3238780dbf40f2d1f7decb9db
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2006-08-20 02:59:36 +0000

    SC600
    git-svn-id: https://geekscape.org/svn/boost/trunk@963 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/test/atom/DefaultDataTestChecker.java
+++ src/test/java/au/net/netstorm/boost/test/atom/DefaultDataTestChecker.java
@@ -2,10 +2,10 @@ package au.net.netstorm.boost.test.atom;
 
 import au.net.netstorm.boost.util.introspect.FieldSpec;
 
-// FIX SC600 Think about how to incorporate a test with a marker interface which uses field
-// FIX SC600 introspection to determine which properties.  Declaration of a single array
-// FIX SC600 is probably enough to specify field/property order.  Might use volatile or
-// FIX SC600 transient to mark fields as optional.
+// SUGGEST Think about how to incorporate a test with a marker interface which uses field
+// SUGGEST introspection to determine which properties.  Declaration of a single array
+// SUGGEST is probably enough to specify field/property order.  Might use volatile or
+// SUGGEST transient to mark fields as optional.
 // FIX SC600 Rename to DataAtomTestChecker.
 // FIX SC600 Remove OldDTC.
 // FIX SC600 Narrow scope in this package.  Private classes for all but DDTC.
@@ -38,10 +38,6 @@ public final class DefaultDataTestChecker implements DataTestChecker {
     private void checkBehaviour(Class cls, FieldSpec[] fields) {
         checkTriangulationOnProperties(cls, fields);
         checkConstructorRefusesNulls(cls, fields);
-
-        // FIX SC600 BREADCRUMB Complete.
-        // Arrays with nulls.
-        // Arrays must be copied going in and copied coming out.
     }
 
     private void checkClassDeclaration(Class cls, FieldSpec[] fields) {
