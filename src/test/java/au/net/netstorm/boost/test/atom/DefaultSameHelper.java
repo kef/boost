fc2f8573a816292c33c3a681b451a8866954c753
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2006-08-20 06:09:48 +0000

    SC600
    git-svn-id: https://geekscape.org/svn/boost/trunk@983 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/test/atom/DefaultSameHelper.java
+++ src/test/java/au/net/netstorm/boost/test/atom/DefaultSameHelper.java
@@ -5,16 +5,15 @@ final class DefaultSameHelper implements SameHelper {
 
     public boolean same(Object o1, Object o2) {
         if (isArray(o1)) return isArraySame(o1, o2);
-        return isSame(o1, o2);
+        return isNonArraySame(o1, o2);
     }
 
     private boolean isArraySame(Object o1, Object o2) {
-        Object[] array1 = (Object[]) o1;
-        Object[] array2 = (Object[]) o2;
-        int length = array1.length;
-        if (length != array2.length) return false;
-        for (int i = 0; i < length; i++) {
-            if (!isSame(array1[i], array2[i])) return false;
+        Object[] a1 = (Object[]) o1;
+        Object[] a2 = (Object[]) o2;
+        if (sameLength(a1, a2)) return false;
+        for (int i = 0; i < a1.length; i++) {
+            if (!same(a1[i], a2[i])) return false;
         }
         return true;
     }
@@ -24,7 +23,7 @@ final class DefaultSameHelper implements SameHelper {
         return type.isArray();
     }
 
-    private boolean isSame(Object o1, Object o2) {
+    private boolean isNonArraySame(Object o1, Object o2) {
         boolean boxed = isBoxed(o1);
         if (boxed) return o1.equals(o2);
         return o1 == o2;
@@ -33,5 +32,9 @@ final class DefaultSameHelper implements SameHelper {
     private boolean isBoxed(Object value) {
         Class cls = value.getClass();
         return primitiveBoxer.isBoxed(cls);
+    }
+
+    private boolean sameLength(Object[] array1, Object[] array2) {
+        return array1.length != array2.length;
     }
 }
