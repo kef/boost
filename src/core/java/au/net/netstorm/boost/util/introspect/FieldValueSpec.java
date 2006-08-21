06a169f49b04a5d8cebdccae5b56e7f63b40c12a
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2006-08-21 20:52:16 +0000

    SC600
    git-svn-id: https://geekscape.org/svn/boost/trunk@1038 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/util/introspect/FieldValueSpec.java
+++ src/core/java/au/net/netstorm/boost/util/introspect/FieldValueSpec.java
@@ -1,5 +1,6 @@
 package au.net.netstorm.boost.util.introspect;
 
+// FIX SC600 Extend NameValueSpec.  Then have FieldValueSpec/PropertyValueSpec extend NameValueSpec.
 public interface FieldValueSpec {
     String getName();
 
