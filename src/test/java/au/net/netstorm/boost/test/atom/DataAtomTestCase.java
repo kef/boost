f537d00f1f0911c3f4960fe7df4f47e507e2d4f0
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2006-08-21 21:23:10 +0000

    SC600
    git-svn-id: https://geekscape.org/svn/boost/trunk@1042 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/test/atom/DataAtomTestCase.java
+++ src/test/java/au/net/netstorm/boost/test/atom/DataAtomTestCase.java
@@ -1,12 +1,11 @@
 package au.net.netstorm.boost.test.atom;
 
-import au.net.netstorm.boost.util.exception.NotImplementedException;
-
 // FIX SC600 BREADCRUMB Rename DataDemoTest to DataDemoTestCase.  Rename to DataAtomScenarioTest.
 // FIX SC600 BREADCRUMB Complete this.
 public class DataAtomTestCase extends PrimordialTestCase implements DataAtomTester {
     public void checkAtom(Object field1) {
-        throw new NotImplementedException();
+        Object[] fields = {field1};
+        checkAtom(fields);
     }
 
     public void checkAtom(Object field1, Object field2) {
@@ -15,19 +14,23 @@ public class DataAtomTestCase extends PrimordialTestCase implements DataAtomTest
     }
 
     public void checkAtom(Object field1, Object field2, Object field3) {
-        throw new NotImplementedException();
+        Object[] fields = {field1, field2, field3};
+        checkAtom(fields);
     }
 
     public void checkAtom(Object field1, Object field2, Object field3, Object field4) {
-        throw new NotImplementedException();
+        Object[] fields = {field1, field2, field3, field4};
+        checkAtom(fields);
     }
 
     public void checkAtom(Object[] fields) {
+        // FIX SC600 BREADCRUMB DELEGATE To SUPPORTING DUDE.
         // FIX SC600 Use field name to obtain bean property name.
         // FIX SC600 BREADCRUMB Complete.
     }
 
     protected final void setUp() throws Exception {
+        // FIX SC600 BREADCRUMB Delegate to populate fields using getDeclaredFields() which excludes inherited ones :-)
         // FIX SC600 Populate each of the subclasses fields using TriangulationProvider.
         // FIX SC600 Use the populated fields values to correlate with checkAtom(...) parameters.
         // FIX SC600 Perform and fail on fields not being Data/Immutable.
