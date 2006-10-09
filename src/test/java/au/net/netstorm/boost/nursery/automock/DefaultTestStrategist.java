f4bfc6e1966469c6c6b7171cb54fac9408b0709f
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2006-10-09 22:03:51 +0000

    SC525
    git-svn-id: https://geekscape.org/svn/boost/trunk@1070 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/nursery/automock/DefaultTestStrategist.java
+++ src/test/java/au/net/netstorm/boost/nursery/automock/DefaultTestStrategist.java
@@ -2,6 +2,7 @@ package au.net.netstorm.boost.nursery.automock;
 
 public final class DefaultTestStrategist implements TestStrategist {
     public TestStrategy determineStrategy() {
+        // FIX SC525 Create instance, passing in the test case.
         throw new UnsupportedOperationException();
     }
 }
