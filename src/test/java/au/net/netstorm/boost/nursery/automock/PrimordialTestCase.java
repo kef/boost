150113186fbd89ed3a833715c8ad081ccb540ba4
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2006-10-09 21:48:44 +0000

    SC525
    git-svn-id: https://geekscape.org/svn/boost/trunk@1067 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/nursery/automock/PrimordialTestCase.java
+++ src/test/java/au/net/netstorm/boost/nursery/automock/PrimordialTestCase.java
@@ -5,13 +5,21 @@ import junit.framework.TestCase;
 public class PrimordialTestCase extends TestCase {
 
     // FIX SC525 Wire in hook to delegates.
-    // FIX SC525 Hook into the strategist.
     public void runBare() throws Throwable {
-        // FIX SC525 init().
+        init();
         try {
             super.runBare();
-        } catch (Throwable throwable) {
+        } finally {
+            destroy();
             // FIX SC525 destroy.
         }
+    }
+
+    private void init() {
+        // FIX SC525 Hook into the strategist.
+    }
+
+    private void destroy() {
+        // FIX SC525 cleanup.
     }
 }
