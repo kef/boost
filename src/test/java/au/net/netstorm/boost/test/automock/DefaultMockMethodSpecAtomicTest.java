6fe4914df3355e03829111c76d623340930eac5b
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2006-11-01 06:44:30 +0000

    git-svn-id: https://geekscape.org/svn/boost/trunk@1154 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/test/automock/DefaultMockMethodSpecAtomicTest.java
+++ src/test/java/au/net/netstorm/boost/test/automock/DefaultMockMethodSpecAtomicTest.java
@@ -2,11 +2,10 @@ package au.net.netstorm.boost.test.automock;
 
 import au.net.netstorm.boost.test.atom.AtomTestChecker;
 import au.net.netstorm.boost.test.atom.LooseDataAtomTestChecker;
-import au.net.netstorm.boost.util.introspect.FieldSpec;
 import au.net.netstorm.boost.util.introspect.DefaultFieldSpec;
+import au.net.netstorm.boost.util.introspect.FieldSpec;
 import junit.framework.TestCase;
 
-// FIX 525 Since it is a method expectation move it into the automocking area.
 public final class DefaultMockMethodSpecAtomicTest extends TestCase {
     private AtomTestChecker checker = new LooseDataAtomTestChecker();
     private FieldSpec returnValue = new DefaultFieldSpec("returnValue", Object.class);
