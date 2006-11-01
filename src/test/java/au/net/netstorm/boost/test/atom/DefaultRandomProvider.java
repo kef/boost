d8481647eec0cd2b01d4f9ac39467323655d8001
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2006-11-01 23:48:29 +0000

    git-svn-id: https://geekscape.org/svn/boost/trunk@1164 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/test/atom/DefaultRandomProvider.java
+++ src/test/java/au/net/netstorm/boost/test/atom/DefaultRandomProvider.java
@@ -9,7 +9,6 @@ public final class DefaultRandomProvider implements RandomProvider {
     private Random random = new Random();
 
     // FIX 525 Can we pull these apart?
-    // FIX 525 No nulls.
     public Object getRandom(Class type) {
         if (type == Boolean.class) return randomBoolean();
         if (type == Integer.class) return randomInteger();
