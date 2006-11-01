cdd176b9eb857685463fb755fc1f46c313f6833e
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2006-11-01 23:23:49 +0000

    git-svn-id: https://geekscape.org/svn/boost/trunk@1160 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/test/atom/DefaultSameHelper.java
+++ src/test/java/au/net/netstorm/boost/test/atom/DefaultSameHelper.java
@@ -11,20 +11,27 @@ final class DefaultSameHelper implements SameHelper {
     }
 
     private boolean isArraySame(Object a1, Object a2) {
-        // FIX 525 Tidy this up now!!!
-        // FIX 525 Reinstate this.
-//        if (sameLength(a1, a2)) return false;
+        if (!isArray(a2)) return false;
+        if (!sameLength(a1, a2)) return false;
         return areElementsSame(a1, a2);
+    }
+
+    private boolean sameLength(Object a1, Object a2) {
+        return length(a1) == length(a2);
     }
 
     private boolean areElementsSame(Object a1, Object a2) {
         int length = length(a1);
         for (int i = 0; i < length; i++) {
-            Object x1 = get(a1, i);
-            Object x2 = get(a2, i);
-            if (!same(x1, x2)) return false;
+            if (!elementSame(i, a1, a2)) return false;
         }
         return true;
+    }
+
+    private boolean elementSame(int i, Object a1, Object a2) {
+        Object x1 = get(a1, i);
+        Object x2 = get(a2, i);
+        return same(x1, x2);
     }
 
     private boolean isArray(Object ref) {
