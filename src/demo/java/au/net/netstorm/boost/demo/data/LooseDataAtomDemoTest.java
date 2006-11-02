edb692a0fd1700f00d9f1dc8eeaca7cfdf4ff2fc
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2006-11-02 03:21:13 +0000

    git-svn-id: https://geekscape.org/svn/boost/trunk@1180 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/demo/java/au/net/netstorm/boost/demo/data/LooseDataAtomDemoTest.java
+++ src/demo/java/au/net/netstorm/boost/demo/data/LooseDataAtomDemoTest.java
@@ -8,12 +8,15 @@ import au.net.netstorm.boost.util.introspect.DefaultFieldSpec;
 
 public final class LooseDataAtomDemoTest extends TestCase {
     private static final FieldSpec STRING_PROPERTY = new DefaultFieldSpec("guitar", String.class);
+    private static final FieldSpec NON_DATA_PROPERTY = new DefaultFieldSpec("nonImmutable", NonImmutableInterface.class);
     private static final FieldSpec[] SINGLE_STRING_PROPERTY = { STRING_PROPERTY };
+    private static final FieldSpec[] COMPLEX_NON_DATA_PROPERTIES = { STRING_PROPERTY, NON_DATA_PROPERTY };
 
     private AtomTestChecker checker = new LooseDataAtomTestChecker();
 
     public void testGoodAtoms() {
         checkGoodAtom(NullsAreIllegalData.class, SINGLE_STRING_PROPERTY);
+        checkGoodAtom(NestedWithNonImmutablePartsIllegalData.class,  COMPLEX_NON_DATA_PROPERTIES);
     }
 
     private void checkGoodAtom(Class cls, FieldSpec[] fields) {
--- src/demo/java/au/net/netstorm/boost/demo/data/NestedNonImmutableInterface.java
+++ src/demo/java/au/net/netstorm/boost/demo/data/NestedNonImmutableInterface.java
@@ -1,8 +1,9 @@
 package au.net.netstorm.boost.demo.data;
 
 import au.net.netstorm.boost.util.type.Data;
+import au.net.netstorm.boost.util.type.LooseData;
 
-interface NestedNonImmutableInterface extends Data {
+interface NestedNonImmutableInterface extends Data, LooseData {
     String getGuitar();
 
     NonImmutableInterface getNonImmutable();
