a8f754f70245db3223e5417cb772cbb0c21db20f
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2006-12-06 21:47:33 +0000

    Story:  Basic auto "edge" implementation. [id=1624]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1188 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/edger/DefaultEdgerAtomicTest.java
+++ src/test/java/au/net/netstorm/boost/edger/DefaultEdgerAtomicTest.java
@@ -5,12 +5,14 @@ import au.net.netstorm.boost.edger.edge.java.lang.ClassFactory;
 import au.net.netstorm.boost.edger.edge.java.lang.Class;
 
 // FIX 1624 This is actually an edge test not atomic.
+// FIX 1624 Consider what to do with bloody generics.
 // FIX 1624 Maybe what is going on here with Class should be in demo?
 public final class DefaultEdgerAtomicTest extends TestCase {
 
     public void testFactoryEdgification() {
         Edgifier edgifier = new DefaultEdifier();
         ClassFactory classFactory = (ClassFactory) edgifier.edgifyFactory(ClassFactory.class, Class.class);
+        classFactory.newInstance();
 
 
         // FIX 1624 Complete factory.forName(String class).
