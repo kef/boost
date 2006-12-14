eddf4680bc17c230f2ac071fac040d1026de70ff
tom.adams <tom.adams@05db6a09-9903-0410-bedf-ba9bc4063e48>
2006-12-14 23:11:18 +0000

    Removed redundant code (was checking for anon inner class behaviour). Fixed svn:ignore tags on root
    git-svn-id: https://geekscape.org/svn/boost/trunk@1200 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/nursery/reflect/checker/Block.java
+++ src/test/java/au/net/netstorm/boost/nursery/reflect/checker/Block.java
@@ -1,6 +1,5 @@
 package au.net.netstorm.boost.nursery.reflect.checker;
 
-// SUGGEST R Remove.  Use Call instead.
 // SUGGEST Move items which are not checkers out of this package.
 // SUGGEST Shouldn't this be a TestBlock.
 public interface Block {
--- src/test/java/au/net/netstorm/boost/nursery/reflect/checker/BlockAtomicTest.java
+++ src/test/java/au/net/netstorm/boost/nursery/reflect/checker/BlockAtomicTest.java
@@ -4,44 +4,10 @@ import au.net.netstorm.boost.test.atom.PrimordialTestCase;
 import au.net.netstorm.boost.test.reflect.checker.DefaultModifierTestChecker;
 import au.net.netstorm.boost.test.reflect.checker.ModifierTestChecker;
 
-// SUGGEST Is this too much for an interface? Maybe just a test that it's an interface & public.
 public final class BlockAtomicTest extends PrimordialTestCase {
-    private static final ModifierTestChecker MODIFIER_CHECKER = new DefaultModifierTestChecker();
-    private static final AssertException ASSERT_EXCEPTION = new DefaultAssertException();
+    private final ModifierTestChecker modifierChecker = new DefaultModifierTestChecker();
 
     public void testProperties() {
-        MODIFIER_CHECKER.checkPublic(Block.class);
-    }
-
-    public void testExecuteThrowsException() {
-        checkExceptionOne();
-        checkExceptionTwo();
-    }
-
-    private void checkExceptionTwo() {
-        Block block = new Block() {
-            public void execute() {
-                throw new UnsupportedOperationException();
-            }
-        };
-        expectFailure(UnsupportedOperationException.class, block);
-    }
-
-    private void checkExceptionOne() {
-        Block block = new Block() {
-            public void execute() {
-                throw new IllegalArgumentException();
-            }
-        };
-        expectFailure(IllegalArgumentException.class, block);
-    }
-
-    private void expectFailure(Class expectedException, Block toExecute) {
-        try {
-            toExecute.execute();
-            fail();
-        } catch (Throwable expected) {
-            ASSERT_EXCEPTION.checkExceptionClass(expectedException, expected);
-        }
+        modifierChecker.checkPublic(Block.class);
     }
 }
