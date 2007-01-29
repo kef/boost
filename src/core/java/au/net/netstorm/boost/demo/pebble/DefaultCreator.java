8c8bb7a9baed39e5fcfc3d9826ae735141825ee5
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-01-29 01:47:43 +0000

    Story:  New interceptor ("pebble"). [id=1665]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1214 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/demo/pebble/DefaultCreator.java
+++ src/core/java/au/net/netstorm/boost/demo/pebble/DefaultCreator.java
@@ -1,4 +1,6 @@
 package au.net.netstorm.boost.demo.pebble;
 
 public final class DefaultCreator implements Creator {
+    public DefaultCreator(Class type) {
+    }
 }
--- src/test/java/au/net/netstorm/boost/demo/pebble/DefaultCreatorAtomicTest.java
+++ src/test/java/au/net/netstorm/boost/demo/pebble/DefaultCreatorAtomicTest.java
@@ -1,10 +1,12 @@
 package au.net.netstorm.boost.demo.pebble;
 
+import java.util.HashSet;
 import junit.framework.TestCase;
 
 public final class DefaultCreatorAtomicTest extends TestCase {
 
     public void testCreator() {
-        Creator creator = new DefaultCreator();
+        Class type = HashSet.class;
+        Creator creator = new DefaultCreator(type);
     }
 }
