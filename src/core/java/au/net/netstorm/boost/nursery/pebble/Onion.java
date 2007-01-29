d6830f8b09f71a06063f059b3a9347aab2318bce
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-01-29 23:53:05 +0000

    Story:  New interceptor ("pebble"). [id=1665]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1242 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/nursery/pebble/Onion.java
+++ src/core/java/au/net/netstorm/boost/nursery/pebble/Onion.java
@@ -2,7 +2,6 @@ package au.net.netstorm.boost.nursery.pebble;
 
 import au.net.netstorm.boost.util.type.Interface;
 
-// FIX 1665 Implement me
 public interface Onion {
     Object onionize(Object ref, Interface type);
 }
