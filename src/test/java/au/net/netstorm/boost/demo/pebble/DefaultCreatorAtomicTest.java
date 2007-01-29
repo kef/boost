4b8b8acda581eb88f753364f9bf65bf40b0c4aa5
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-01-29 01:45:11 +0000

    Story:  New interceptor ("pebble"). [id=1665]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1213 05db6a09-9903-0410-bedf-ba9bc4063e48

---
+++ src/test/java/au/net/netstorm/boost/demo/pebble/DefaultCreatorAtomicTest.java
@@ -0,0 +1,10 @@
+package au.net.netstorm.boost.demo.pebble;
+
+import junit.framework.TestCase;
+
+public final class DefaultCreatorAtomicTest extends TestCase {
+
+    public void testCreator() {
+        Creator creator = new DefaultCreator();
+    }
+}
