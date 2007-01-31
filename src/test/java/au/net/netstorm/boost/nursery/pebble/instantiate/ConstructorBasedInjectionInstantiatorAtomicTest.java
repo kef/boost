c6b62da8f5da94596d3d4123dc37168d0e5f5834
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-01-31 00:29:30 +0000

    Story:  New interceptor ("pebble"). [id=1665]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1266 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/nursery/pebble/instantiate/ConstructorBasedInjectionInstantiatorAtomicTest.java
+++ src/test/java/au/net/netstorm/boost/nursery/pebble/instantiate/ConstructorBasedInjectionInstantiatorAtomicTest.java
@@ -3,13 +3,14 @@ package au.net.netstorm.boost.nursery.pebble.instantiate;
 import junit.framework.TestCase;
 
 public final class ConstructorBasedInjectionInstantiatorAtomicTest extends TestCase {
+    private Instantiator instantiator = new ConstructorBasedInjectionInstantiator();
 
     public void testInstantiate() {
-        Instantiator instantiator = new ConstructorBasedInjectionInstantiator();
         Class type = String.class;
-        Object[] parameters = { "Hi" };
+        String parameter = "Hi";
+        Object[] parameters = {parameter};
         Object ref = instantiator.instantiate(type, parameters);
-        assertEquals("Hi", ref);
+        assertEquals(parameter, ref);
         // FIX BREADCRUMB 1665 Complete.
     }
 }
