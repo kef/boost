574033b53aab2f2057668bf1b3b88a6b4bf3074b
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-02-06 02:55:13 +0000

    Story:  New interceptor ("pebble"). [id=1665]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1321 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/nursery/pebble/instantiate/Instantiator.java
+++ src/core/java/au/net/netstorm/boost/nursery/pebble/instantiate/Instantiator.java
@@ -1,5 +1,6 @@
 package au.net.netstorm.boost.nursery.pebble.instantiate;
 
+// FIX 1665 Merge with GenericCreator.
 public interface Instantiator {
     Object instantiate(Class type, Object[] parameters);
 }
