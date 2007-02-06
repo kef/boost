1e5002a13f21e3328818d343e9e37d170054d3d9
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-02-06 02:50:13 +0000

    Story:  New interceptor ("pebble"). [id=1665]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1317 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/nursery/pebble/create/Fred.java
+++ src/test/java/au/net/netstorm/boost/nursery/pebble/create/Fred.java
@@ -1,8 +1,5 @@
 package au.net.netstorm.boost.nursery.pebble.create;
 
-// FIX 1665 Ensure naming works.  If a field is call fooCreator chuck a wobbly.
-
-// FIX 1665 Close down scope.  Make package-private.
 class Fred {
     private NewNedCreator newNedCreator;
     private NewTedCreator newTedCreator;
