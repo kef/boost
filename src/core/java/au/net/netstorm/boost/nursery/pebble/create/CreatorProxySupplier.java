e6fc9b82e1c38bfd8cef3856fab35069196250c3
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-02-06 02:56:53 +0000

    Story:  New interceptor ("pebble"). [id=1665]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1322 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/nursery/pebble/create/CreatorProxySupplier.java
+++ src/core/java/au/net/netstorm/boost/nursery/pebble/create/CreatorProxySupplier.java
@@ -2,6 +2,7 @@ package au.net.netstorm.boost.nursery.pebble.create;
 
 import au.net.netstorm.boost.util.type.Interface;
 
+// FIX 1665 Narrow scope in this package.  Probably CreatorInjector is the only one to expose.
 public interface CreatorProxySupplier {
     Object create(Interface type);
 }
