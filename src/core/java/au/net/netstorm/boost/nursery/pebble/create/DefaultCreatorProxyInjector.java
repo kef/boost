1c09344220ee2cc76232bf922c68b308382a0e22
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-02-06 04:00:36 +0000

    Story:  New interceptor ("pebble"). [id=1665]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1324 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/nursery/pebble/create/DefaultCreatorProxyInjector.java
+++ src/core/java/au/net/netstorm/boost/nursery/pebble/create/DefaultCreatorProxyInjector.java
@@ -1,11 +1,14 @@
 package au.net.netstorm.boost.nursery.pebble.create;
 
 import au.net.netstorm.boost.edge.java.lang.EdgeClass;
+import au.net.netstorm.boost.edge.java.lang.reflect.DefaultEdgeField;
+import au.net.netstorm.boost.edge.java.lang.reflect.EdgeField;
 
 public final class DefaultCreatorProxyInjector implements CreatorProxyInjector {
     private CreatorProxySupplier proxySupplier;
     private EdgeClass edgeClass;
     private CreatorFieldFinder fieldFinder;
+    private EdgeField edgeField = new DefaultEdgeField();
 
     public DefaultCreatorProxyInjector(CreatorProxySupplier creatorProxySupplier, EdgeClass edgeClass) {
         this.proxySupplier = creatorProxySupplier;
@@ -13,12 +16,16 @@ public final class DefaultCreatorProxyInjector implements CreatorProxyInjector {
     }
 
     public void inject(Object ref) {
-//        spike(ref);
+        // FIX 1665 Rename CreatorField.getCreatorType to getCreatorInterface.
+        // FIX 1665 Call CreatorFieldsFinder.
+        // FIX 1665 Use ProxySupplier to get the Object proxy.
+        // FIX 1665 Set Object proxy in fields.
         // FIX 1665 Reinstate
         // edgeClass.getDeclaredFields(object.getClass());
     }
-/*
 
+    // FIX 1665 Test drive up and hook in.
+/*
     private void spike(Object ref) {
         CreatorField[] creatorFields = fieldFinder.find(ref);
         for (int i = 0; i < creatorFields.length; i++) {
@@ -27,7 +34,17 @@ public final class DefaultCreatorProxyInjector implements CreatorProxyInjector {
     }
 
     private void inject(Object ref, CreatorField field) {
-        proxySupplier.create(type);
+        Interface type = field.getCreatorType();
+        Object proxy = proxySupplier.create(type);
+        inject(ref, proxy, field);
+    }
+
+    private void inject(Object ref, Object proxy, CreatorField creatorField) {
+        String fieldName = creatorField.getFieldName();
+        Class cls = ref.getClass();
+        Field field = edgeClass.getDeclaredField(cls, fieldName);
+        edgeField.set(field, ref, proxy);
+
     }
 */
 }
