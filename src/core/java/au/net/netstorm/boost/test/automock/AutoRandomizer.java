261ed94e64611e9e206ce5aa16549cb284d95eb2
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-02-06 23:11:25 +0000

    Story:  New interceptor ("pebble"). [id=1665]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1350 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/test/automock/AutoRandomizer.java
+++ src/core/java/au/net/netstorm/boost/test/automock/AutoRandomizer.java
@@ -2,9 +2,6 @@ package au.net.netstorm.boost.test.automock;
 
 import java.lang.reflect.Field;
 
-// FIX 1665 Is this where we want this class?
-
-// FIX 1665 Is this what we want to call this class? PrimitiveRandomizer?
 public interface AutoRandomizer {
     void randomizePrimitivesAndStrings(Field[] fields);
 }
