3e6eaccf8a6ef09a3971d55b4049b0b8be72772c
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-02-07 23:57:07 +0000

    Story: New interceptor ("pebble"). [id=1665]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1378 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/DefaultClassNameMorpher.java
+++ src/core/java/au/net/netstorm/boost/DefaultClassNameMorpher.java
@@ -1,9 +1,24 @@
 package au.net.netstorm.boost;
 
-import java.util.Map;
+import au.net.netstorm.boost.edge.java.lang.DefaultEdgeClass;
+import au.net.netstorm.boost.edge.java.lang.EdgeClass;
+import au.net.netstorm.boost.reflect.ClassMaster;
+import au.net.netstorm.boost.reflect.DefaultClassMaster;
 
 public final class DefaultClassNameMorpher implements ClassNameMorpher {
+    private final ClassMaster classMaster = new DefaultClassMaster();
+    private final EdgeClass edgeClass = new DefaultEdgeClass();
+
     public Class stripPrefix(String prefix, Class cls) {
-        return Map.class;
+        String packageName = classMaster.getPackageName(cls);
+        String shortName = classMaster.getShortName(cls);
+        String morphedName = stripPrefix(prefix, shortName);
+        String morphedClassName = packageName + "." + morphedName;
+        return edgeClass.forName(morphedClassName);
+    }
+
+    private String stripPrefix(String prefix, String shortName) {
+        int length = prefix.length();
+        return shortName.substring(length);
     }
 }
--- src/test/java/au/net/netstorm/boost/DefaultClassNameMorpherAtomicTest.java
+++ src/test/java/au/net/netstorm/boost/DefaultClassNameMorpherAtomicTest.java
@@ -1,7 +1,9 @@
 package au.net.netstorm.boost;
 
-import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
 import junit.framework.TestCase;
 
 public final class DefaultClassNameMorpherAtomicTest extends TestCase {
@@ -11,7 +13,8 @@ public final class DefaultClassNameMorpherAtomicTest extends TestCase {
     // FIX 1665 Handle prefix not in class.
     // FIX 1665 Handle result class not existing.
     public void testStripPrefix() {
-        checkStripPrefix(Map.class, "Hash", HashMap.class);
+        checkStripPrefix(Map.class, "Tree", TreeMap.class);
+        checkStripPrefix(Set.class, "Hash", HashSet.class);
     }
 
     private void checkStripPrefix(Class expected, String prefix, Class cls) {
