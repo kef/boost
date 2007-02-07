4d40d1a59e3c7fe6464499d905c7494f3bd56290
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-02-07 23:45:17 +0000

    Story: New interceptor ("pebble"). [id=1665]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1377 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/DefaultClassNameMorpherAtomicTest.java
+++ src/test/java/au/net/netstorm/boost/DefaultClassNameMorpherAtomicTest.java
@@ -5,10 +5,17 @@ import java.util.Map;
 import junit.framework.TestCase;
 
 public final class DefaultClassNameMorpherAtomicTest extends TestCase {
+    private ClassNameMorpher morpher = new DefaultClassNameMorpher();
 
+    // FIX 1665 Triangulate.
+    // FIX 1665 Handle prefix not in class.
+    // FIX 1665 Handle result class not existing.
     public void testStripPrefix() {
-        ClassNameMorpher morpher = new DefaultClassNameMorpher();
-        Class result = morpher.stripPrefix("Hash", HashMap.class);
-        assertEquals(Map.class, result);
+        checkStripPrefix(Map.class, "Hash", HashMap.class);
+    }
+
+    private void checkStripPrefix(Class expected, String prefix, Class cls) {
+        Class result = morpher.stripPrefix(prefix, cls);
+        assertEquals(expected, result);
     }
 }
