663569fd13ffbb765a4afc50152e7f7a106e91cd
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-02-07 23:34:08 +0000

    Story: New interceptor ("pebble"). [id=1665]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1375 05db6a09-9903-0410-bedf-ba9bc4063e48

---
+++ src/test/java/au/net/netstorm/boost/DefaultClassNameMorpherAtomicTest.java
@@ -0,0 +1,10 @@
+package au.net.netstorm.boost;
+
+import junit.framework.TestCase;
+
+public final class DefaultClassNameMorpherAtomicTest extends TestCase {
+
+    public void testStripPrefix() {
+        ClassNameMorpher morpher = new DefaultClassNameMorpher();
+    }
+}
