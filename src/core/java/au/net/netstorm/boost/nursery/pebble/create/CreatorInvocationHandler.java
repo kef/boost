60b4341b0d6e68930ddf21939460ac1ef14299f6
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-02-07 03:50:20 +0000

    Story:  New interceptor ("pebble"). [id=1665]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1363 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/nursery/pebble/create/CreatorInvocationHandler.java
+++ src/core/java/au/net/netstorm/boost/nursery/pebble/create/CreatorInvocationHandler.java
@@ -12,6 +12,8 @@ public final class CreatorInvocationHandler implements InvocationHandler {
 
     // FIX 1665 If we added a line like "if (!method.getName().equals("create")) barf();"
     public Object invoke(Object object, Method method, Object[] params) throws Throwable {
+        // FIX BREADCRUMB 1665 -10000 Use the Creator name to determine the create type.
+        // FIX BREADCRUMB 1665 -10000 Do not use the return type.
         Class returnType = method.getReturnType();
         return genericCreator.create(returnType, params);
     }
