85f58519d8cc87f141f9b96f87e861db431c9db2
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-02-08 23:33:09 +0000

    Story: New interceptor ("pebble"). [id=1665]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1404 05db6a09-9903-0410-bedf-ba9bc4063e48

---
+++ src/test/java/au/net/netstorm/boost/DefaultPassThroughInvovationHandlerAtomicTest.java
@@ -0,0 +1,13 @@
+package au.net.netstorm.boost;
+
+import junit.framework.TestCase;
+
+// FIX 1665 Move into appropriate package.
+
+// FIX 1665 Fix FIX #.
+public final class DefaultPassThroughInvovationHandlerAtomicTest extends TestCase {
+
+    public void testHandler() {
+        // FIX 1665 Complete this.
+    }
+}
