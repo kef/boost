1533513c1a4a086cb8416e36ca7a5f1dcfddfd93
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-02-09 01:34:04 +0000

    Story: New interceptor ("pebble"). [id=1665]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1419 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/pebble/create/DefaultCreator.java
+++ src/core/java/au/net/netstorm/boost/pebble/create/DefaultCreator.java
@@ -5,7 +5,9 @@ import au.net.netstorm.boost.edge.java.lang.reflect.ProxySupplier;
 import au.net.netstorm.boost.pebble.instantiate.Instantiator;
 import au.net.netstorm.boost.pebble.instantiate.SingleConstructorBasedInjectionInstantiator;
 import au.net.netstorm.boost.pebble.onion.BermudaOnion;
+import au.net.netstorm.boost.pebble.onion.DefaultPassThroughInvocationHandler;
 import au.net.netstorm.boost.pebble.onion.Onion;
+import au.net.netstorm.boost.pebble.onion.PassThroughInvocationHandler;
 import au.net.netstorm.boost.util.proxy.DefaultProxyFactory;
 import au.net.netstorm.boost.util.proxy.ProxyFactory;
 
@@ -15,21 +17,38 @@ public final class DefaultCreator implements Creator {
     // FIX 1665 Introduce the proxy pass through.
     // FIX 1665 Pass in "true" depencencies.
     // FIX 1665 Move the wiring into CreatorWirer.
-    private final Instantiator instantiator = new SingleConstructorBasedInjectionInstantiator();
-    private final Onion onion = new BermudaOnion();
+    // FIX 1665 Change atomic test use constructor injection.
+    private Onion onion = new BermudaOnion();
     private final ProxySupplier proxySupplier = new DefaultProxySupplier();
     private final ProxyFactory proxyFactory = new DefaultProxyFactory(proxySupplier);
-    //    private final PassThroughInvocationHandler passThroughInvocationHandler = new DefaultPassThroughInvocationHandler();
+    private Instantiator instantiator = new SingleConstructorBasedInjectionInstantiator();
     private final CreatorProxySupplier creatorProxySupplier =
             new DefaultCreatorProxySupplier(proxyFactory, this, instantiator);
     private final CreatorFieldFinder creatorFieldFinder = new DefaultCreatorFieldFinder();
-    private final CreatorProxyInjector creatorProxyInjector =
+    private CreatorProxyInjector creatorProxyInjector =
             new DefaultCreatorProxyInjector(creatorProxySupplier, creatorFieldFinder);
+
+    public DefaultCreator(Onion onion, CreatorProxyInjector creatorProxyInjector, Instantiator instantiator) {
+        this.onion = onion;
+        this.creatorProxyInjector = creatorProxyInjector;
+        this.instantiator = instantiator;
+    }
+
+    // FIX 1665 Remove this no-arg constructor.
+    public DefaultCreator() {
+    }
 
     public Object create(Class type, Object[] parameters) {
         Object ref = instantiator.instantiate(type, parameters);
         creatorProxyInjector.inject(ref);
         return onion.onionize(ref);
+    }
+
+    private Creator buildCreator() {
+        PassThroughInvocationHandler passThroughInvocationHandler = new DefaultPassThroughInvocationHandler();
+        // FIX 1665 Reintroduce
+//        passThroughInvocationHandler.setDelegate();
+        return null;
     }
 }
 // } DEBT LineLength|ClassDataAbstractionCoupling
