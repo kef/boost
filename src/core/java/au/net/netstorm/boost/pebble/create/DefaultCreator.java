4bcf8c709304cc904e2273d00471f954d7c0e72d
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-02-09 02:00:46 +0000

    Story: New interceptor ("pebble"). [id=1665]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1422 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/pebble/create/DefaultCreator.java
+++ src/core/java/au/net/netstorm/boost/pebble/create/DefaultCreator.java
@@ -1,30 +1,12 @@
 package au.net.netstorm.boost.pebble.create;
 
-import au.net.netstorm.boost.edge.java.lang.reflect.DefaultProxySupplier;
-import au.net.netstorm.boost.edge.java.lang.reflect.ProxySupplier;
 import au.net.netstorm.boost.pebble.instantiate.Instantiator;
-import au.net.netstorm.boost.pebble.instantiate.SingleConstructorBasedInjectionInstantiator;
-import au.net.netstorm.boost.pebble.onion.BermudaOnion;
 import au.net.netstorm.boost.pebble.onion.Onion;
-import au.net.netstorm.boost.util.proxy.DefaultProxyFactory;
-import au.net.netstorm.boost.util.proxy.ProxyFactory;
 
-// DEBT LineLength|ClassDataAbstractionCoupling {
 public final class DefaultCreator implements Creator {
-    // FIX 1665 Move assembly out into demo area.
-    // FIX 1665 Introduce the proxy pass through.
-    // FIX 1665 Pass in "true" depencencies.
-    // FIX 1665 Move the wiring into CreatorWirer.
-    // FIX 1665 Change atomic test use constructor injection.
-    private Onion onion = new BermudaOnion();
-    private final ProxySupplier proxySupplier = new DefaultProxySupplier();
-    private final ProxyFactory proxyFactory = new DefaultProxyFactory(proxySupplier);
-    private Instantiator instantiator = new SingleConstructorBasedInjectionInstantiator();
-    private final CreatorProxySupplier creatorProxySupplier =
-            new DefaultCreatorProxySupplier(proxyFactory, this, instantiator);
-    private final CreatorFieldFinder creatorFieldFinder = new DefaultCreatorFieldFinder();
-    private CreatorProxyInjector creatorProxyInjector =
-            new DefaultCreatorProxyInjector(creatorProxySupplier, creatorFieldFinder);
+    private Onion onion;
+    private Instantiator instantiator;
+    private CreatorProxyInjector creatorProxyInjector;
 
     public DefaultCreator(Onion onion, CreatorProxyInjector creatorProxyInjector, Instantiator instantiator) {
         this.onion = onion;
@@ -38,4 +20,3 @@ public final class DefaultCreator implements Creator {
         return onion.onionize(ref);
     }
 }
-// } DEBT LineLength|ClassDataAbstractionCoupling
