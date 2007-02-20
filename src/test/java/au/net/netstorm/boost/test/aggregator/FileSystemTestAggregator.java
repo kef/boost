a2260b3a1040fe8e6e705194cab916cfef5ab4d9
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-02-20 22:55:29 +0000

    Story: New interceptor ("pebble"). [id=1665]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1444 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/test/aggregator/FileSystemTestAggregator.java
+++ src/test/java/au/net/netstorm/boost/test/aggregator/FileSystemTestAggregator.java
@@ -1,13 +1,10 @@
 package au.net.netstorm.boost.test.aggregator;
 
-import au.net.netstorm.boost.edge.java.lang.DefaultEdgeClass;
-import au.net.netstorm.boost.edge.java.lang.EdgeClass;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
 public final class FileSystemTestAggregator implements TestAggregator {
-    private final ClassNameLocator nameLocator = new TestClassNameLocator();
-    private final EdgeClass edgeClass = new DefaultEdgeClass();
+    private final ClassLocator classLocator = new TestClassLocator();
     private Class starter;
 
     public FileSystemTestAggregator(Class starter) {
@@ -15,26 +12,20 @@ public final class FileSystemTestAggregator implements TestAggregator {
     }
 
     public Test aggregate(String suiteName, String regex) {
-        JavaClass[] matches = findMatches(regex, starter);
+        Class[] matches = findMatches(regex, starter);
         return buildSuite(suiteName, matches);
     }
 
-    private JavaClass[] findMatches(String regex, Class starter) {
+    private Class[] findMatches(String regex, Class starter) {
         RegexPattern expression = new TestRegexPattern(regex);
-        return nameLocator.locate(starter, expression);
+        return classLocator.locate(starter, expression);
     }
 
-    private Test buildSuite(String suiteName, JavaClass[] classes) {
+    private Test buildSuite(String suiteName, Class[] classes) {
         TestSuite result = new TestSuite(suiteName);
         for (int i = 0; i < classes.length; i++) {
-            addClass(classes[i], result);
+            result.addTestSuite(classes[i]);
         }
         return result;
-    }
-
-    private void addClass(JavaClass clsName, TestSuite result) {
-        String qualified = clsName.getFullyQualified();
-        Class cls = edgeClass.forName(qualified);
-        result.addTestSuite(cls);
     }
 }
