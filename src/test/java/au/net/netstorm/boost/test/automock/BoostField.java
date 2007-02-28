dc455adab3e9f8d8ecf82f3e6137e3cef65a6200
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-02-28 06:16:41 +0000

    Story:  Add support for arrays in "auto" mocking. [id=1676]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1462 05db6a09-9903-0410-bedf-ba9bc4063e48

---
+++ src/test/java/au/net/netstorm/boost/test/automock/BoostField.java
@@ -0,0 +1,19 @@
+package au.net.netstorm.boost.test.automock;
+
+public interface BoostField {
+    Object get();
+
+    void set(Object value);
+
+    boolean isNull();
+
+    boolean isFinal();
+
+    boolean isArray();
+
+    boolean isPrimitive();
+
+    boolean isSupportedConcrete();
+
+    boolean isInterface();
+}
---
+++ src/test/java/au/net/netstorm/boost/test/automock/DefaultBoostField.java
@@ -0,0 +1,62 @@
+package au.net.netstorm.boost.test.automock;
+
+import java.lang.reflect.Field;
+import au.net.netstorm.boost.test.atom.DefaultPrimitiveBoxer;
+import au.net.netstorm.boost.test.atom.DefaultRandomProvider;
+import au.net.netstorm.boost.test.atom.PrimitiveBoxer;
+import au.net.netstorm.boost.test.atom.RandomProvider;
+import au.net.netstorm.boost.test.reflect.util.DefaultFieldTestUtil;
+import au.net.netstorm.boost.test.reflect.util.DefaultModifierTestUtil;
+import au.net.netstorm.boost.test.reflect.util.FieldTestUtil;
+import au.net.netstorm.boost.test.reflect.util.ModifierTestUtil;
+
+public final class DefaultBoostField implements BoostField {
+    private final FieldTestUtil fielder = new DefaultFieldTestUtil();
+    private final ModifierTestUtil modifier = new DefaultModifierTestUtil();
+    private final PrimitiveBoxer primitiveBoxer = new DefaultPrimitiveBoxer();
+    private final RandomProvider randomProvider = new DefaultRandomProvider();
+    private final Object ref;
+    private final Field field;
+
+    public DefaultBoostField(Object ref, Field field) {
+        this.ref = ref;
+        this.field = field;
+    }
+
+    public Object get() {
+        return fielder.getInstance(ref, field);
+    }
+
+    public void set(Object value) {
+        String fieldName = field.getName();
+        fielder.setInstance(ref, fieldName, value);
+    }
+
+    public boolean isNull() {
+        return fielder.getInstance(ref, field) == null;
+    }
+
+    public boolean isFinal() {
+        return modifier.isFinal(field);
+    }
+
+    public boolean isArray() {
+        Class type = field.getType();
+        return type.isArray();
+    }
+
+    public boolean isPrimitive() {
+        Class type = field.getType();
+        return primitiveBoxer.isPrimitive(type);
+    }
+
+    public boolean isSupportedConcrete() {
+        Class type = field.getType();
+        return randomProvider.isRandomizable(type);
+    }
+
+    public boolean isInterface() {
+        Class type = field.getType();
+        return type.isInterface();
+    }
+}
