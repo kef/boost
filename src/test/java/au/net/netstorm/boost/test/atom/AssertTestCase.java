44f97db0af6c8ff1beba8531b5be3f1f76d46779
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-03-08 04:30:55 +0000

    Story:  Create InteractionTestCase that extends PrimordialTestCase and exposes AssertTestChecker. [id=1709]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1474 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/test/atom/AssertTestCase.java
+++ src/test/java/au/net/netstorm/boost/test/atom/AssertTestCase.java
@@ -10,30 +10,26 @@ import junit.framework.TestCase;
 // FIX SC600 Check no-arg (single) constructor.
 
 // FIX SC600 Redefine setUp() to setup() which does not throw an exception.
-public class AssertTestCase extends TestCase implements AssertTestChecker {
+public class AssertTestCase extends TestCase {
     private final AssertTestChecker asserter = new DefaultAssertTestChecker();
 
-    public final void assertEquals(Object[] expected, Object[] fields) {
-        asserter.checkEquals(expected, fields);
-    }
-
-    public void checkEquals(Object[] expected, Object[] actual) {
+    public void assertEquals(Object[] expected, Object[] actual) {
         asserter.checkEquals(expected, actual);
     }
 
-    public void checkEquals(byte[] expected, byte[] actual) {
+    public void assertEquals(byte[] expected, byte[] actual) {
         asserter.checkEquals(expected, actual);
     }
 
-    public void checkEquals(int[] expected, int[] actual) {
+    public void assertEquals(int[] expected, int[] actual) {
         asserter.checkEquals(expected, actual);
     }
 
-    public void checkNotEquals(byte[] value1, byte[] value2) {
+    public void assertNotEquals(byte[] value1, byte[] value2) {
         asserter.checkNotEquals(value1, value2);
     }
 
-    public void checkImmutable(byte[] value1, byte[] value2) {
+    public void assertImmutable(byte[] value1, byte[] value2) {
         asserter.checkImmutable(value1, value2);
     }
 }
