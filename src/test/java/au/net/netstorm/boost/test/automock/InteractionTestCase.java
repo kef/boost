d7f6e79311641c95eb508ea7bd64e88a3a49c3e9
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-03-08 01:48:46 +0000

    Story:  Create InteractionTestCase that extends PrimordialTestCase and exposes AssertTestChecker. [id=1709]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1472 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/test/automock/InteractionTestCase.java
+++ src/test/java/au/net/netstorm/boost/test/automock/InteractionTestCase.java
@@ -3,26 +3,26 @@ package au.net.netstorm.boost.test.automock;
 import au.net.netstorm.boost.nursery.reflect.checker.AssertTestChecker;
 import au.net.netstorm.boost.nursery.reflect.checker.DefaultAssertTestChecker;
 
-public final class InteractionTestCase extends PrimordialTestCase {
+public class InteractionTestCase extends PrimordialTestCase {
     private final AssertTestChecker assertTestChecker = new DefaultAssertTestChecker();
 
-    public void checkEquals(Object[] expected, Object[] actual) {
+    public final void checkEquals(Object[] expected, Object[] actual) {
         assertTestChecker.checkEquals(expected, actual);
     }
 
-    public void checkEquals(byte[] expected, byte[] actual) {
+    public final void checkEquals(byte[] expected, byte[] actual) {
         assertTestChecker.checkEquals(expected, actual);
     }
 
-    public void assertEquals(int[] expected, int[] actual) {
+    public final void assertEquals(int[] expected, int[] actual) {
         assertTestChecker.checkEquals(expected, actual);
     }
 
-    public void assertNotEquals(byte[] value1, byte[] value2) {
+    public final void assertNotEquals(byte[] value1, byte[] value2) {
         assertTestChecker.checkNotEquals(value1, value2);
     }
 
-    public void assertImmutable(byte[] value1, byte[] value2) {
+    public final void assertImmutable(byte[] value1, byte[] value2) {
         assertTestChecker.checkImmutable(value1, value2);
     }
 }
