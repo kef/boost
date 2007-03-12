44de16666b575db08694868d2ae1274dfc0147f0
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-03-12 06:39:26 +0000

    Story:  Implement field based injection for dependencies. [id=1715]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1507 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/pebble/create/DefaultNewDoesNotImplementNewer.java
+++
@@ -1,6 +0,0 @@
-package au.net.netstorm.boost.pebble.create;
-
-import au.net.netstorm.boost.pebble.create.fixture.NewDoesNotImplementNewer;
-
-public final class DefaultNewDoesNotImplementNewer implements NewDoesNotImplementNewer {
-}
---
+++ src/test/java/au/net/netstorm/boost/pebble/create/fixture/DefaultNewDoesNotImplementNewer.java
@@ -0,0 +1,4 @@
+package au.net.netstorm.boost.pebble.create.fixture;
+
+public final class DefaultNewDoesNotImplementNewer implements NewDoesNotImplementNewer {
+}
