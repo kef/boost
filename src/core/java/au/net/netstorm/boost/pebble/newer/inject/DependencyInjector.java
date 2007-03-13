09017fdabd079dfc331a436677ed42aa3288a1a0
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-03-13 06:45:27 +0000

    Story:  Implement field based injection for dependencies. [id=1715]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1525 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/pebble/newer/inject/DependencyInjector.java
+++ src/core/java/au/net/netstorm/boost/pebble/newer/inject/DependencyInjector.java
@@ -1,5 +1,6 @@
 package au.net.netstorm.boost.pebble.newer.inject;
 
+// FIX BREADCRUMB 1715 Build up a "demo" test for this.
 public final class DependencyInjector implements Injector {
     // FIX BREADCRUMB 1715 Need to make this find all non-"newer" fields and then inject
     // them with their default dependencies. 
