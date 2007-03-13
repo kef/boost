078bc3e84f8b1e9addeb3303c70b207314aab8b7
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-03-13 04:58:38 +0000

    Story:  Implement field based injection for dependencies. [id=1715]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1519 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/test/atom/AssertTestCase.java
+++ src/test/java/au/net/netstorm/boost/test/atom/AssertTestCase.java
@@ -4,12 +4,7 @@ import au.net.netstorm.boost.nursery.reflect.checker.AssertTestChecker;
 import au.net.netstorm.boost.nursery.reflect.checker.DefaultAssertTestChecker;
 import junit.framework.TestCase;
 
-// SUGGEST Remove the need for this altogether.
-// FIX SC600 BREADCRUMB Complete.
-// FIX SC600 Check bottom level classes are final.
-// FIX SC600 Check no-arg (single) constructor.
-
-// FIX SC600 Redefine setUp() to setup() which does not throw an exception.
+// FIX 1715 Remove me.
 public class AssertTestCase extends TestCase {
     private final AssertTestChecker asserter = new DefaultAssertTestChecker();
 
--- src/test/java/au/net/netstorm/boost/test/automock/BoooostTestCase.java
+++ src/test/java/au/net/netstorm/boost/test/automock/BoooostTestCase.java
@@ -4,6 +4,12 @@ import au.net.netstorm.boost.nursery.reflect.checker.AssertTestChecker;
 import au.net.netstorm.boost.nursery.reflect.checker.DefaultAssertTestChecker;
 import junit.framework.TestCase;
 
+// SUGGEST Remove the need for this altogether.
+// FIX SC600 BREADCRUMB Complete.
+// FIX SC600 Check bottom level classes are final.
+// FIX SC600 Check no-arg (single) constructor.
+
+// FIX SC600 Redefine setUp() to setup() which does not throw an exception.
 public abstract class BoooostTestCase extends TestCase {
     private final AssertTestChecker assertTestChecker = new DefaultAssertTestChecker();
 
