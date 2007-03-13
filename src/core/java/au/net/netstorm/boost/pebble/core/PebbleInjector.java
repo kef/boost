2143bcf6ace685697c10d12c4076b4ddce1a9211
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-03-13 23:48:31 +0000

    Story:  Implement field based injection for dependencies. [id=1715]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1542 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/pebble/core/PebbleInjector.java
+++ src/core/java/au/net/netstorm/boost/pebble/core/PebbleInjector.java
@@ -5,15 +5,15 @@ import au.net.netstorm.boost.pebble.inject.newer.core.Injector;
 // FIX BREADCRUMB 1715 Rename to PebbleInjector.  Same for ObjectProvider.
 public final class PebbleInjector implements Injector {
     private final Injector newerInjector;
-    private final Injector dependencyInjector;
+    private final Injector resolverInjector;
 
-    public PebbleInjector(Injector newerInjector, Injector dependencyInjector) {
+    public PebbleInjector(Injector newerInjector, Injector resolverInjector) {
         this.newerInjector = newerInjector;
-        this.dependencyInjector = dependencyInjector;
+        this.resolverInjector = resolverInjector;
     }
 
     public void inject(Object ref) {
         newerInjector.inject(ref);
-        dependencyInjector.inject(ref);
+        resolverInjector.inject(ref);
     }
 }
