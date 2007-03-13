6816d2ff864aafae7a76d399f9886c1ef75064fb
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-03-13 05:00:03 +0000

    Story:  Implement field based injection for dependencies. [id=1715]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1520 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/test/atom/AssertTestCase.java
+++ src/test/java/au/net/netstorm/boost/test/atom/AssertTestCase.java
@@ -5,6 +5,8 @@ import au.net.netstorm.boost.nursery.reflect.checker.DefaultAssertTestChecker;
 import junit.framework.TestCase;
 
 // FIX 1715 Remove me.
+
+// FIX BREADCRUMB 1715 Over here.
 public class AssertTestCase extends TestCase {
     private final AssertTestChecker asserter = new DefaultAssertTestChecker();
 
