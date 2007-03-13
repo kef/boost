24b734fc75fe822a2839009c2f2d307d5330c1db
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-03-13 07:07:43 +0000

    Story:  Implement field based injection for dependencies. [id=1715]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1528 05db6a09-9903-0410-bedf-ba9bc4063e48

---
+++ src/demo/java/au/net/netstorm/boost/demo/pebble/resolve/DefaultResolveDemoTest.java
@@ -0,0 +1,10 @@
+package au.net.netstorm.boost.demo.pebble.resolve;
+
+import junit.framework.TestCase;
+
+public final class DefaultResolveDemoTest extends TestCase {
+
+    public void testResolve() {
+        // FIX BREADCRUMB 1715 Complete this.
+    }
+}
