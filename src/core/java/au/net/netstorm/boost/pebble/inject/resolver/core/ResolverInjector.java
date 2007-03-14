63ee1897715a50cb8a9763f9128b92b9bfdb32bf
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-03-14 04:09:11 +0000

    Story:  Implement field based injection for dependencies. [id=1715]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1575 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/pebble/inject/resolver/core/ResolverInjector.java
+++ src/core/java/au/net/netstorm/boost/pebble/inject/resolver/core/ResolverInjector.java
@@ -5,11 +5,9 @@ import au.net.netstorm.boost.pebble.inject.newer.core.Injector;
 public final class ResolverInjector implements Injector {
     public void inject(Object object) {
         // FIX 1715 Do this.
-        // FIX 1715 Hit 3 first.
         // FIX 1715 Nail 4 next.
         // 1. For each candidate field.
         // 2. Determine field interface.
-        // 3. Call resolver to get implementation type.
         // 4. Instantiate implementation.
         // 5. Set field.
     }
