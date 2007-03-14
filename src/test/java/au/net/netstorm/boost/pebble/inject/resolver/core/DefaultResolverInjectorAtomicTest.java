c8bc822fcbd7a4ab2fab158f541e3d8be666b3dc
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-03-14 04:10:48 +0000

    Story:  Implement field based injection for dependencies. [id=1715]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1576 05db6a09-9903-0410-bedf-ba9bc4063e48

---
+++ src/test/java/au/net/netstorm/boost/pebble/inject/resolver/core/DefaultResolverInjectorAtomicTest.java
@@ -0,0 +1,10 @@
+package au.net.netstorm.boost.pebble.inject.resolver.core;
+
+import au.net.netstorm.boost.test.automock.BoooostTestCase;
+
+public final class DefaultResolverInjectorAtomicTest extends BoooostTestCase {
+
+    public void testInjector() {
+        // FIX BREADCRUMB 1715 Back here and finish.
+    }
+}
