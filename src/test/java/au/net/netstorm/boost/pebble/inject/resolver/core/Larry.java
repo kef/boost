59fd23a9144e866271c96cfee7ff2f7c3df3b819
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-03-14 06:31:48 +0000

    Story:  Implement field based injection for dependencies. [id=1715]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1586 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/pebble/inject/resolver/core/Larry.java
+++ src/test/java/au/net/netstorm/boost/pebble/inject/resolver/core/Larry.java
@@ -1,6 +1,4 @@
 package au.net.netstorm.boost.pebble.inject.resolver.core;
 
-import au.net.netstorm.boost.primordial.Primordial;
-
-final class Larry extends Primordial implements LazyBastard {
+final class Larry implements LazyBastard {
 }
