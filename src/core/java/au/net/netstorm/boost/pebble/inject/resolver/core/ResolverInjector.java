891511cf74448184be0f82e20cceeeef8c8273f0
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-03-14 01:06:52 +0000

    Story:  Implement field based injection for dependencies. [id=1715]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1562 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/pebble/inject/resolver/core/ResolverInjector.java
+++ src/core/java/au/net/netstorm/boost/pebble/inject/resolver/core/ResolverInjector.java
@@ -2,10 +2,13 @@ package au.net.netstorm.boost.pebble.inject.resolver.core;
 
 import au.net.netstorm.boost.pebble.inject.newer.core.Injector;
 
-// FIX BREADCRUMB 1715 Build up a "demo" test for this.
 public final class ResolverInjector implements Injector {
-    // FIX BREADCRUMB 1715 Need to make this find all non-"newer" fields and then inject
-    // them with their default dependencies. 
     public void inject(Object object) {
+        // FIX 1715 Do this.
+        // 1. For each candidate field.
+        // 2. Determine field interface.
+        // 3. Call resolver to get implementation type.
+        // 4. Instantiate implementation.
+        // 5. Set field.
     }
 }
