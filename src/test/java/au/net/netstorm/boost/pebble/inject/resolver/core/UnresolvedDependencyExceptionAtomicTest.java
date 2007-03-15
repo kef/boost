cb60edc5a8a70c8797c733c1e0965cd718c6a265
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-03-15 21:15:51 +0000

    Story:  Implement field based injection for dependencies. [id=1715]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1611 05db6a09-9903-0410-bedf-ba9bc4063e48

---
+++ src/test/java/au/net/netstorm/boost/pebble/inject/resolver/core/UnresolvedDependencyExceptionAtomicTest.java
@@ -0,0 +1,10 @@
+package au.net.netstorm.boost.pebble.inject.resolver.core;
+
+import junit.framework.TestCase;
+
+public final class UnresolvedDependencyExceptionAtomicTest extends TestCase {
+
+    public void testException() {
+        // FIX 1715 Complete.
+    }
+}
