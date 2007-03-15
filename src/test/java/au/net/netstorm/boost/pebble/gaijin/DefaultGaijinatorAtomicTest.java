f291c5257f5c57ad2a52df6757404caf5d899770
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-03-15 04:28:47 +0000

    Story:  Implement field based injection for dependencies. [id=1715]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1605 05db6a09-9903-0410-bedf-ba9bc4063e48

---
+++ src/test/java/au/net/netstorm/boost/pebble/gaijin/DefaultGaijinatorAtomicTest.java
@@ -0,0 +1,10 @@
+package au.net.netstorm.boost.pebble.gaijin;
+
+import junit.framework.TestCase;
+
+public final class DefaultGaijinatorAtomicTest extends TestCase {
+
+    public void testGaijin() {
+        // FIX BREADCRUMB 1715 Back here.
+    }
+}
