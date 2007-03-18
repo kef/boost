415cd2ca8ea3e991f10e2d50a0c66e909c2a23a4
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-03-18 23:54:00 +0000

    Story:  Implement field based injection for dependencies. [id=1715]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1631 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/test/automock/InteractionTestCase.java
+++ src/test/java/au/net/netstorm/boost/test/automock/InteractionTestCase.java
@@ -1,4 +1,6 @@
 package au.net.netstorm.boost.test.automock;
 
 public abstract class InteractionTestCase extends StrategyTestCase implements UsesMocks {
+    // FIX 1715 Make this final.  Use pass through.
+    protected MockExpectations expect;
 }
