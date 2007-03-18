1bb01ebc33d3845609faead17b49820d2ea39236
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-03-18 23:51:46 +0000

    Story:  Implement field based injection for dependencies. [id=1715]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1630 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/pebble/core/IllegalCitizenExceptionAtomicTest.java
+++ src/test/java/au/net/netstorm/boost/pebble/core/IllegalCitizenExceptionAtomicTest.java
@@ -2,17 +2,26 @@ package au.net.netstorm.boost.pebble.core;
 
 import au.net.netstorm.boost.pebble.type.Implementation;
 import au.net.netstorm.boost.test.automock.BoooostCase;
+import au.net.netstorm.boost.test.automock.MockExpectations;
 import au.net.netstorm.boost.util.type.Interface;
 
 public final class IllegalCitizenExceptionAtomicTest extends BoooostCase {
+    // FIX BREADCRUMB 1715 -100 Remove this.
+    private MockExpectations expect;
+    private RuntimeException subject;
     private Interface marker;
     private Implementation impl;
 
-//    throw new IllegalStateException("I know you want to be my darling, but you're not a " + marker + " -> " + impl);
+    public void setupSubjects() {
+        subject = new IllegalCitizenException(marker, impl);
+    }
 
     // FIX 1715 Complete.
 
+    // FIX BREADCRUMB 1715 Return and complete.
+
     public void testException() {
-        RuntimeException ex = new IllegalCitizenException(marker, impl);
+        setupSubjects();
+//        assertEquals("I know you want to be my darling, but you're not a " + marker + " -> " + impl, "" + subject);
     }
 }
