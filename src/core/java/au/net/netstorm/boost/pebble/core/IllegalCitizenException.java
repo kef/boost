67d47ebd1ed4f9c9a7c6fc269ed65dd6a081f681
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-03-18 23:42:07 +0000

    Story:  Implement field based injection for dependencies. [id=1715]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1629 05db6a09-9903-0410-bedf-ba9bc4063e48

---
+++ src/core/java/au/net/netstorm/boost/pebble/core/IllegalCitizenException.java
@@ -0,0 +1,9 @@
+package au.net.netstorm.boost.pebble.core;
+
+import au.net.netstorm.boost.pebble.type.Implementation;
+import au.net.netstorm.boost.util.type.Interface;
+
+public final class IllegalCitizenException extends RuntimeException {
+    public IllegalCitizenException(Interface marker, Implementation impl) {
+    }
+}
--- src/test/java/au/net/netstorm/boost/pebble/core/IllegalCitizenExceptionAtomicTest.java
+++ src/test/java/au/net/netstorm/boost/pebble/core/IllegalCitizenExceptionAtomicTest.java
@@ -1,13 +1,18 @@
 package au.net.netstorm.boost.pebble.core;
 
+import au.net.netstorm.boost.pebble.type.Implementation;
 import au.net.netstorm.boost.test.automock.BoooostCase;
+import au.net.netstorm.boost.util.type.Interface;
 
 public final class IllegalCitizenExceptionAtomicTest extends BoooostCase {
+    private Interface marker;
+    private Implementation impl;
 
 //    throw new IllegalStateException("I know you want to be my darling, but you're not a " + marker + " -> " + impl);
 
     // FIX 1715 Complete.
 
     public void testException() {
+        RuntimeException ex = new IllegalCitizenException(marker, impl);
     }
 }
