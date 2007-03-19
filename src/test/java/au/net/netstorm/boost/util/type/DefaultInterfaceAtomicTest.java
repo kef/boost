d76b8ea3ebfe68fcfde9b1ff3d1a4a92c2ab49e8
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-03-19 03:41:21 +0000

    Story:  Implement field based injection for dependencies. [id=1715]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1646 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/util/type/DefaultInterfaceAtomicTest.java
+++ src/test/java/au/net/netstorm/boost/util/type/DefaultInterfaceAtomicTest.java
@@ -1,22 +1,18 @@
 package au.net.netstorm.boost.util.type;
 
-import au.net.netstorm.boost.test.atom.AtomTestChecker;
-import au.net.netstorm.boost.test.atom.DataAtomTestChecker;
+import au.net.netstorm.boost.primordial.Primordial;
 import au.net.netstorm.boost.test.automock.BoooostCase;
-import au.net.netstorm.boost.util.introspect.DefaultFieldSpec;
-import au.net.netstorm.boost.util.introspect.FieldSpec;
+import au.net.netstorm.boost.test.reflect.checker.ClassTestChecker;
+import au.net.netstorm.boost.test.reflect.checker.DefaultClassTestChecker;
 
 public class DefaultInterfaceAtomicTest extends BoooostCase {
     private static final Class NOT_AN_INTERFACE = Object.class;
+    private final ClassTestChecker classer = new DefaultClassTestChecker();
 
-    // FIX 1715 Remove this.
-    // FIX 1715 Assert it is Primordial.
     // FIX 1715 Test is().
-    public void testIsDataObject() {
-        FieldSpec f1 = new DefaultFieldSpec("type", Class.class);
-        FieldSpec[] fields = {f1};
-        AtomTestChecker checker = new DataAtomTestChecker();
-        checker.checkAtom(DefaultInterface.class, fields);
+
+    public void testPrimordial() {
+        classer.checkSubclassOf(DefaultInterface.class, Primordial.class);
     }
 
     // FIX 1715 Complete.
