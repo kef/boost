507d24446819b044e98c808d55fdff8a6a7d595b
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-03-19 04:54:45 +0000

    Story:  Implement field based injection for dependencies. [id=1715]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1655 05db6a09-9903-0410-bedf-ba9bc4063e48

---
+++ src/test/java/au/net/netstorm/boost/primordial/DefaultPrimordialExceptionAtomicTest.java
@@ -0,0 +1,9 @@
+package au.net.netstorm.boost.primordial;
+
+import au.net.netstorm.boost.test.automock.BoooostCase;
+
+public final class DefaultPrimordialExceptionAtomicTest extends BoooostCase {
+
+    public void testException() {
+    }
+}
