435bb6b42ecb4b209f420319341627524cd9ea92
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-03-19 01:37:45 +0000

    Story:  Implement field based injection for dependencies. [id=1715]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1645 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/util/type/DefaultInterfaceAtomicTest.java
+++ src/test/java/au/net/netstorm/boost/util/type/DefaultInterfaceAtomicTest.java
@@ -9,11 +9,18 @@ import au.net.netstorm.boost.util.introspect.FieldSpec;
 public class DefaultInterfaceAtomicTest extends BoooostCase {
     private static final Class NOT_AN_INTERFACE = Object.class;
 
+    // FIX 1715 Remove this.
+    // FIX 1715 Assert it is Primordial.
+    // FIX 1715 Test is().
     public void testIsDataObject() {
         FieldSpec f1 = new DefaultFieldSpec("type", Class.class);
         FieldSpec[] fields = {f1};
         AtomTestChecker checker = new DataAtomTestChecker();
         checker.checkAtom(DefaultInterface.class, fields);
+    }
+
+    // FIX 1715 Complete.
+    public void testXxx() {
     }
 
     public void testTypeIsNotInterface() {
