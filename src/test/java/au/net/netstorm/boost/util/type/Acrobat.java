6800af14c258aedadd87f4843aee5eccfcb97429
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-03-19 03:46:32 +0000

    Story:  Implement field based injection for dependencies. [id=1715]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1647 05db6a09-9903-0410-bedf-ba9bc4063e48

---
+++ src/test/java/au/net/netstorm/boost/util/type/Acrobat.java
@@ -0,0 +1,4 @@
+package au.net.netstorm.boost.util.type;
+
+interface Acrobat {
+}
--- src/test/java/au/net/netstorm/boost/util/type/DefaultInterfaceAtomicTest.java
+++ src/test/java/au/net/netstorm/boost/util/type/DefaultInterfaceAtomicTest.java
@@ -9,14 +9,9 @@ public class DefaultInterfaceAtomicTest extends BoooostCase {
     private static final Class NOT_AN_INTERFACE = Object.class;
     private final ClassTestChecker classer = new DefaultClassTestChecker();
 
-    // FIX 1715 Test is().
-
-    public void testPrimordial() {
-        classer.checkSubclassOf(DefaultInterface.class, Primordial.class);
-    }
-
-    // FIX 1715 Complete.
-    public void testXxx() {
+    // FIX 1715 Complete this guy.
+    public void testIsA() {
+        new DefaultInterface(Jester.class);
     }
 
     public void testTypeIsNotInterface() {
@@ -24,5 +19,16 @@ public class DefaultInterfaceAtomicTest extends BoooostCase {
             new DefaultInterface(NOT_AN_INTERFACE);
             fail();
         } catch (IllegalArgumentException expected) { }
+    }
+
+    public void testNullIllegal() {
+        try {
+            new DefaultInterface(null);
+            fail();
+        } catch (IllegalArgumentException expected) { }
+    }
+
+    public void testPrimordial() {
+        classer.checkSubclassOf(DefaultInterface.class, Primordial.class);
     }
 }
