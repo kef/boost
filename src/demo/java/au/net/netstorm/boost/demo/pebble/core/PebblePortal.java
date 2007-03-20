5043aa031a642f6cfd77f1441f4494c2ae38a9f1
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-03-20 21:31:56 +0000

    Recursive dependency resolution. [id=1779]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1684 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/demo/java/au/net/netstorm/boost/demo/pebble/core/PebblePortal.java
+++ src/demo/java/au/net/netstorm/boost/demo/pebble/core/PebblePortal.java
@@ -3,6 +3,7 @@ package au.net.netstorm.boost.demo.pebble.core;
 import au.net.netstorm.boost.pebble.core.PebbleProvider;
 import au.net.netstorm.boost.pebble.inject.newer.core.Injector;
 
+// FIX 1779 Add getResolver.
 public interface PebblePortal {
     PebbleProvider getProvider();
 
