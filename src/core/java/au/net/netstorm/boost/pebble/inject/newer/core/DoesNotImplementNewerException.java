373d6092199743efa7797f6b1e73048c5e1915d4
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-03-20 03:58:36 +0000

    Story:  Implement field based injection for dependencies. [id=1715]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1668 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/pebble/inject/newer/core/DoesNotImplementNewerException.java
+++ src/core/java/au/net/netstorm/boost/pebble/inject/newer/core/DoesNotImplementNewerException.java
@@ -2,6 +2,7 @@ package au.net.netstorm.boost.pebble.inject.newer.core;
 
 public final class DoesNotImplementNewerException extends RuntimeException {
 
+    // FIX 35820 Change message to something more meaningful.
     public DoesNotImplementNewerException(Class type) {
         super("No newer interface found for class " + type.getName());
     }
