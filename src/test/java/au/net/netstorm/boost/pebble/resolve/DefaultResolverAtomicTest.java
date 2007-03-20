20e655d896788e9c885b7704067e4198f4a1026e
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-03-20 23:41:25 +0000

    Recursive dependency resolution. [id=1779]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1692 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/pebble/resolve/DefaultResolverAtomicTest.java
+++ src/test/java/au/net/netstorm/boost/pebble/resolve/DefaultResolverAtomicTest.java
@@ -34,16 +34,14 @@ public final class DefaultResolverAtomicTest extends InteractionTestCase {
     }
 
     public void testOneUnresolvedDependencies() {
-        expect.oneCall(lookup, jackImpl, "find", jack);
         expect.oneCall(lookup, jimImpl, "find", jim);
         expect.oneCall(provider, jimInstance, "provide", jimImpl, noparams);
+        expect.oneCall(lookup, jackImpl, "find", jack);
         expect.oneCall(provider, jackInstance, "provide", jackImpl, new Object[]{jimInstance});
         Object result = subject.resolve(jack);
         assertEquals(jackInstance, result);
-        // FIX 1779 Complete.
     }
 
-    // FIX 1779 Multi-arg case.
     private Implementation impl(Class cls) {
         return new DefaultImplementation(cls);
     }
@@ -52,64 +50,3 @@ public final class DefaultResolverAtomicTest extends InteractionTestCase {
         return new DefaultInterface(cls);
     }
 }
-
-// FIX 1779 Remove when done.
-/*
-
-package au.net.netstorm.boost.pebble.inject.resolver.core;
-
-import java.lang.reflect.Field;
-import au.net.netstorm.boost.pebble.core.PebbleProviderEngine;
-import au.net.netstorm.boost.util.type.DefaultInterface;
-import au.net.netstorm.boost.util.type.Implementation;
-import au.net.netstorm.boost.util.type.Interface;
-
-public final class DefaultFieldResolver implements FieldResolver {
-    private final ImplementationLookup lookup;
-    private final PebbleProviderEngine provider;
-
-    public DefaultFieldResolver(ImplementationLookup lookup, PebbleProviderEngine provider) {
-        this.provider = provider;
-        this.lookup = lookup;
-    }
-
-    public Object resolve(Field field) {
-        Interface iface = getInterface(field);
-        return resolve(iface);
-    }
-
-    // SUGGEST: This is probably the true resolver entry point and will likely take a "flavour".
-    private Object resolve(Interface iface) {
-        Implementation implementation = lookup.find(iface);
-        return resolve(implementation);
-    }
-
-//    private Object[] resolveDependencies(Implementation implementation) {
-//        List dependencyList = new ArrayList();
-//        Class impl = implementation.getImpl();
-//        Constructor constructor = impl.getDeclaredConstructors()[0];
-//        Class[] dependencyTypes = constructor.getParameterTypes();
-//        resolveEachDependency(dependencyTypes, dependencyList);
-//        return dependencyList.toArray(new Object[]{});
-//    }
-//
-//    private void resolveEachDependency(Class[] dependencyTypes, List dependencyList) {
-//        for (int i = 0; i < dependencyTypes.length; i++) {
-//            Class dependencyType = dependencyTypes[i];
-//            DefaultInterface dependencyInterface = new DefaultInterface(dependencyType);
-//            Object dependency = resolve(dependencyInterface);
-//            dependencyList.add(dependency);
-//        }
-//    }
-
-    private Object resolve(Implementation implementation) {
-        Object[] dependencies = new Object[]{};
-        return provider.provide(implementation, dependencies);
-    }
-
-    private Interface getInterface(Field field) {
-        Class type = field.getType();
-        return new DefaultInterface(type);
-    }
-}
-*/
