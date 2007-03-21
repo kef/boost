225ee9e56d11b454c4555068827461ba6e7979c8
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-03-21 23:37:22 +0000

    Recursive dependency resolution. [id=1779]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1718 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/test/atom/DefaultRandomArrayDetective.java
+++ src/test/java/au/net/netstorm/boost/test/atom/DefaultRandomArrayDetective.java
@@ -1,7 +1,11 @@
 package au.net.netstorm.boost.test.atom;
 
 public final class DefaultRandomArrayDetective implements RandomArrayDetective {
+    private PrimitiveBoxer marquessDeQueensbury = new DefaultPrimitiveBoxer();
+
     public boolean isRandomizable(Class type) {
-        return false;
+        if (!type.isArray()) return false;
+        Class componentType = type.getComponentType();
+        return marquessDeQueensbury.isPrimitive(componentType);
     }
 }
