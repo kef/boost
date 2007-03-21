b58a55d6e815e6e7c4276db35a90f2b5797445c6
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-03-21 00:18:35 +0000

    Recursive dependency resolution. [id=1779]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1698 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/util/type/DefaultInterfaceUtil.java
+++ src/core/java/au/net/netstorm/boost/util/type/DefaultInterfaceUtil.java
@@ -2,6 +2,11 @@ package au.net.netstorm.boost.util.type;
 
 public final class DefaultInterfaceUtil implements InterfaceUtil {
     public Interface[] interfaces(Class[] classes) {
-        return null;
+        int length = classes.length;
+        Interface[] result = new Interface[length];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = new DefaultInterface(classes[i]);
+        }
+        return result;
     }
 }
--- src/test/java/au/net/netstorm/boost/util/type/DefaultInterfaceUtilAtomicTest.java
+++ src/test/java/au/net/netstorm/boost/util/type/DefaultInterfaceUtilAtomicTest.java
@@ -1,11 +1,25 @@
 package au.net.netstorm.boost.util.type;
 
+import java.util.Map;
+import java.util.Set;
 import au.net.netstorm.boost.test.cases.BoooostCase;
 
 public final class DefaultInterfaceUtilAtomicTest extends BoooostCase {
     private final InterfaceUtil subject = new DefaultInterfaceUtil();
+    private static final Interface MAP = new DefaultInterface(Map.class);
+    private static final Interface SET = new DefaultInterface(Set.class);
+    private static final Class[] ZERO_CLASSES = {};
+    private static final Interface[] ZERO_INTERFACES = {};
+    private static final Class[] MULTIPLE_CLASSES = {Map.class, Map.class, Set.class};
+    private static final Interface[] MULTIPLE_INTERFACES = {MAP, MAP, SET};
 
     public void testZero() {
-        Interface[] result = subject.interfaces(new Class[]{});
+        Interface[] result = subject.interfaces(ZERO_CLASSES);
+        assertEquals(ZERO_INTERFACES, result);
+    }
+
+    public void testMultiple() {
+        Interface[] result = subject.interfaces(MULTIPLE_CLASSES);
+        assertEquals(MULTIPLE_INTERFACES, result);
     }
 }
