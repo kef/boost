dfa220c1060ed67b13fe9abbec7814e12f274d16
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-03-22 05:50:16 +0000

    Story:  Implement singletons. [id=1824]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1745 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/util/type/DefaultInstanceAtomicTest.java
+++ src/test/java/au/net/netstorm/boost/util/type/DefaultInstanceAtomicTest.java
@@ -2,13 +2,16 @@ package au.net.netstorm.boost.util.type;
 
 import java.util.HashMap;
 import java.util.Map;
+import au.net.netstorm.boost.primordial.Primordial;
 import au.net.netstorm.boost.test.cases.BoooostCase;
+import au.net.netstorm.boost.test.reflect.checker.ClassTestChecker;
+import au.net.netstorm.boost.test.reflect.checker.DefaultClassTestChecker;
 
 public final class DefaultInstanceAtomicTest extends BoooostCase {
     private static final Map MAP = new HashMap();
     private static final Soldier SOLDIER = new BooostSoldier();
+    private final ClassTestChecker classer = new DefaultClassTestChecker();
 
-    // FIX BREADCRUMB 1824 Come back here to finish testing!!!!
     public void testGet() {
         checkGet(MAP);
         checkGet(SOLDIER);
@@ -27,37 +30,7 @@ public final class DefaultInstanceAtomicTest extends BoooostCase {
         assertEquals(ref, actual);
     }
 
-//
-//    public void testIsA() {
-//        checkIsA(true, clown);
-//        checkIsA(false, soldier);
-//    }
-//
-//    public void testType() {
-//        Class actual = jester.getType();
-//        assertEquals(Jester.class, actual);
-//    }
-//
-//    public void testTypeIsNotInterface() {
-//        try {
-//            new DefaultInterface(NOT_AN_INTERFACE);
-//            fail();
-//        } catch (IllegalArgumentException expected) { }
-//    }
-//
-//    public void testNullIllegal() {
-//        try {
-//            new DefaultInterface(null);
-//            fail();
-//        } catch (IllegalArgumentException expected) { }
-//    }
-//
-//    public void testPrimordial() {
-//        classer.checkSubclassOf(DefaultInterface.class, Primordial.class);
-//    }
-//
-//    private void checkIsA(boolean expected, Interface iface) {
-//        boolean result = jester.is(iface);
-//        assertEquals(expected, result);
-//    }
+    public void testPrimordial() {
+        classer.checkSubclassOf(DefaultInstance.class, Primordial.class);
+    }
 }
