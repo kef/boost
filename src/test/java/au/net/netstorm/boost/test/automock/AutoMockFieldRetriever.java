74d245796afce265268ec859ae62fe8c74e67935
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-03-22 06:33:43 +0000

    Story: Add support for auto mocking arrays. [id=1676]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1752 05db6a09-9903-0410-bedf-ba9bc4063e48

---
+++ src/test/java/au/net/netstorm/boost/test/automock/AutoMockFieldRetriever.java
@@ -0,0 +1,35 @@
+package au.net.netstorm.boost.test.automock;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+import au.net.netstorm.boost.test.field.BoostField;
+import au.net.netstorm.boost.test.field.DefaultBoostField;
+
+// FIX BREADCRUMB 1676 FREEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEZZE!!!!!!!!!!!!!!
+public final class AutoMockFieldRetriever implements FieldRetriever {
+
+    public BoostField[] retrieve(Object ref) {
+        Field[] fields = getDeclaredFields(ref);
+        return doRetrieve(ref, fields);
+    }
+
+    // SUGGEST: Can maybe be replaced with field.isSynthetic() in 1.5?
+    private BoostField[] doRetrieve(Object ref, Field[] fields) {
+        List eligibleFields = new ArrayList();
+        for (int i = 0; i < fields.length; i++) {
+            examine(eligibleFields, ref, fields[i]);
+        }
+        return (BoostField[]) eligibleFields.toArray(new BoostField[]{});
+    }
+
+    private void examine(List list, Object ref, Field field) {
+        BoostField boostField = new DefaultBoostField(ref, field);
+        if (boostField.isInjectable()) list.add(boostField);
+    }
+
+    private Field[] getDeclaredFields(Object ref) {
+        Class cls = ref.getClass();
+        return cls.getDeclaredFields();
+    }
+}
