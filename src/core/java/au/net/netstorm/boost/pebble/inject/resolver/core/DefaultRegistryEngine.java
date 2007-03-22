110352637f2956d14df450165195c6575309a098
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-03-22 23:10:40 +0000

    Story:  Implement singletons. [id=1824]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1758 05db6a09-9903-0410-bedf-ba9bc4063e48

---
+++ src/core/java/au/net/netstorm/boost/pebble/inject/resolver/core/DefaultRegistryEngine.java
@@ -0,0 +1,23 @@
+package au.net.netstorm.boost.pebble.inject.resolver.core;
+
+import java.util.HashMap;
+import java.util.Map;
+import au.net.netstorm.boost.util.type.DefaultImplementation;
+import au.net.netstorm.boost.util.type.DefaultInterface;
+import au.net.netstorm.boost.util.type.Implementation;
+import au.net.netstorm.boost.util.type.Interface;
+
+public final class DefaultRegistryEngine implements RegistryEngine {
+    private final Map map = new HashMap();
+
+    public Implementation getImplementation(Interface type) {
+        Class cls = (Class) map.get(type);
+        if (cls == null) throw new UnresolvedDependencyException(type);
+        return new DefaultImplementation(cls);
+    }
+
+    public void prototype(Class iface, Class implementation) {
+        Interface inyerface = new DefaultInterface(iface);
+        map.put(inyerface, implementation);
+    }
+}
