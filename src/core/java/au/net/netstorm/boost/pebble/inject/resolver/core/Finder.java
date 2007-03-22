9c5cf5868c48b314d13c0e4dd72c298d9e9f1bb6
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-03-22 05:50:45 +0000

    Story:  Implement singletons. [id=1824]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1747 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/pebble/inject/resolver/core/Finder.java
+++ src/core/java/au/net/netstorm/boost/pebble/inject/resolver/core/Finder.java
@@ -1,13 +1,14 @@
 package au.net.netstorm.boost.pebble.inject.resolver.core;
 
 import au.net.netstorm.boost.util.type.Implementation;
+import au.net.netstorm.boost.util.type.Instance;
 import au.net.netstorm.boost.util.type.Interface;
 
+// FIX BREADCRUMB 1824 -99999999999 Come back here and implement!!
 public interface Finder {
     boolean hasInstance(Interface iface);
 
-    // FIX BREADCRUMB 1824 Re-instate.
-//    Instance getInstance(Interface iface);
+    Instance getInstance(Interface iface);
 
     boolean hasImplementation(Interface iface);
 
