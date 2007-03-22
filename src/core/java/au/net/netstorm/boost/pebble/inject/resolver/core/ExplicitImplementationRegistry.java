cd15cbcf6128ceb146cf18f02945a9c5a0c83a24
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-03-22 22:17:03 +0000

    Story:  Implement singletons. [id=1824]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1755 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/pebble/inject/resolver/core/ExplicitImplementationRegistry.java
+++ src/core/java/au/net/netstorm/boost/pebble/inject/resolver/core/ExplicitImplementationRegistry.java
@@ -3,6 +3,7 @@ package au.net.netstorm.boost.pebble.inject.resolver.core;
 import au.net.netstorm.boost.util.type.Implementation;
 import au.net.netstorm.boost.util.type.Interface;
 
+// FIX BREADCRUMB 1824 Remove once we have created the new RegistryEngne.
 public interface ExplicitImplementationRegistry {
     void add(Class iface, Class implementation);
 
