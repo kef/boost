4cdbc604362fd40220e94481e2f41f8b48b03ac8
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-03-22 05:50:58 +0000

    Story: Add support for auto mocking arrays. [id=1676]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1748 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/test/automock/DefaultAutoMocker.java
+++ src/test/java/au/net/netstorm/boost/test/automock/DefaultAutoMocker.java
@@ -24,7 +24,6 @@ class DefaultAutoMocker implements AutoMocker {
         throw new IllegalStateException("Mock does not exist for provided proxy.");
     }
 
-    // FIX 1676 Move into thing called FieldMocker.
     public void mock(BoostField[] fields) {
         for (int i = 0; i < fields.length; i++) {
             // FIX 1676 This does not belong.  It belongs in a FieldFinder type thing.
