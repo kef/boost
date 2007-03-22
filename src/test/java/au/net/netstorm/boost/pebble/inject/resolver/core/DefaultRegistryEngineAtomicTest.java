f734a0dec4527e86ae58ed6c9d2ed87a4444c2d8
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-03-22 23:15:29 +0000

    Story:  Implement singletons. [id=1824]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1761 05db6a09-9903-0410-bedf-ba9bc4063e48

---
+++ src/test/java/au/net/netstorm/boost/pebble/inject/resolver/core/DefaultRegistryEngineAtomicTest.java
@@ -0,0 +1,56 @@
+package au.net.netstorm.boost.pebble.inject.resolver.core;
+
+import java.util.Map;
+import au.net.netstorm.boost.test.cases.BoooostCase;
+import au.net.netstorm.boost.util.type.DefaultImplementation;
+import au.net.netstorm.boost.util.type.DefaultInterface;
+import au.net.netstorm.boost.util.type.Implementation;
+import au.net.netstorm.boost.util.type.Interface;
+
+public final class DefaultRegistryEngineAtomicTest extends BoooostCase {
+    private static final Class LAZY_BASTARD = LazyBastard.class;
+    private static final Class LARRY = Larry.class;
+    private static final Class LEGEND = Legend.class;
+    private static final Class AN_DO = AnDo.class;
+    private static final Interface NON_EXISTENT = new DefaultInterface(Map.class);
+    private final RegistryEngine subject = new DefaultRegistryEngine();
+
+    {
+        subject.prototype(LAZY_BASTARD, LARRY);
+        subject.prototype(LEGEND, AN_DO);
+    }
+
+    public void testResolve() {
+        checkResolve(LAZY_BASTARD, LARRY);
+        checkResolve(LEGEND, AN_DO);
+    }
+
+    public void testCannotResolve() {
+        try {
+            subject.getImplementation(NON_EXISTENT);
+            fail();
+        } catch (UnresolvedDependencyException expected) { }
+    }
+
+    public void testMustBeInterface() {
+        try {
+            subject.prototype(LARRY, LARRY);
+            fail();
+        } catch (IllegalArgumentException expected) { }
+    }
+
+    private void checkResolve(Class iface, Class impl) {
+        Implementation result = resolve(subject, iface);
+        checkEquals(impl, result);
+    }
+
+    private void checkEquals(Class cls, Implementation result) {
+        Implementation expected = new DefaultImplementation(cls);
+        assertEquals(expected, result);
+    }
+
+    private Implementation resolve(RegistryEngine registryEngine, Class cls) {
+        Interface iface = new DefaultInterface(cls);
+        return registryEngine.getImplementation(iface);
+    }
+}
