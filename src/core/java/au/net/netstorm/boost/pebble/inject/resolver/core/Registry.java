2c441da7d423351a05493fb5295a6d2c065aa765
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-03-22 22:11:22 +0000

    Story:  Implement singletons. [id=1824]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1754 05db6a09-9903-0410-bedf-ba9bc4063e48

---
+++ src/core/java/au/net/netstorm/boost/pebble/inject/resolver/core/Registry.java
@@ -0,0 +1,11 @@
+package au.net.netstorm.boost.pebble.inject.resolver.core;
+
+import au.net.netstorm.boost.util.type.Implementation;
+import au.net.netstorm.boost.util.type.Instance;
+import au.net.netstorm.boost.util.type.Interface;
+
+public interface Registry {
+    void prototype(Interface iface, Implementation implementation);
+
+    void instance(Interface iface, Instance instance);
+}
