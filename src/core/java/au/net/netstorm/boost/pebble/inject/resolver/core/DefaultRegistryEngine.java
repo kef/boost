7bb29facc6e45695d1d7171382aac1379b61f79c
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-03-23 00:48:19 +0000

    Story:  Implement singletons. [id=1824]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1782 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/pebble/inject/resolver/core/DefaultRegistryEngine.java
+++ src/core/java/au/net/netstorm/boost/pebble/inject/resolver/core/DefaultRegistryEngine.java
@@ -41,11 +41,9 @@ public final class DefaultRegistryEngine implements RegistryEngine {
         add(instanceMap, iface, instance);
     }
 
-    // FIX BREADCRUMB 1824 This will end up using the hasInstance method.
     private void checkInstanceNotExists(Class iface) {
         Interface inyerface = new DefaultInterface(iface);
-        boolean exists = instanceMap.containsKey(inyerface);
-        if (exists) throw new InstanceExistsException(inyerface);
+        if (hasInstance(inyerface)) throw new InstanceExistsException(inyerface);
     }
 
     private void add(Map map, Class cls, Object value) {
