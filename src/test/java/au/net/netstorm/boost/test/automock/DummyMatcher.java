89d5e222710a512ea3e3f90dddb8b41aaf7e93ca
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-03-23 00:45:40 +0000

    Story: Add support for auto mocking arrays. [id=1676]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1780 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/test/automock/DummyMatcher.java
+++ src/test/java/au/net/netstorm/boost/test/automock/DummyMatcher.java
@@ -4,11 +4,10 @@ import au.net.netstorm.boost.test.field.BoostField;
 import au.net.netstorm.boost.test.field.Matcher;
 
 public final class DummyMatcher implements Matcher {
-    MockableMatcher mockMatcher = new MockableMatcher();
 
     // OK CyclomaticComplexity|ReturnCount {
     public boolean matches(BoostField field) {
-        if (mockMatcher.matches(field)) return false;
+        if (field.isInterface()) return false;
         if (field.isPrimitive()) return false;
         if (field.isFinal()) return false;
         if (field.isStatic()) return false;
