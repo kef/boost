137d3c1b6a49177ba0563ccc54af976ed6c10e7e
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-03-23 01:02:53 +0000

    Story:  Implement singletons. [id=1824]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1786 05db6a09-9903-0410-bedf-ba9bc4063e48

---
+++ src/core/java/au/net/netstorm/boost/pebble/inject/resolver/core/DefaultRegistryFacade.java
@@ -0,0 +1,4 @@
+package au.net.netstorm.boost.pebble.inject.resolver.core;
+
+public final class DefaultRegistryFacade implements RegistryFacade {
+}
---
+++ src/test/java/au/net/netstorm/boost/pebble/inject/resolver/core/DefaultRegistryFacadeAtomicTest.java
@@ -0,0 +1,15 @@
+package au.net.netstorm.boost.pebble.inject.resolver.core;
+
+import au.net.netstorm.boost.test.automock.InteractionTestCase;
+
+public final class DefaultRegistryFacadeAtomicTest extends InteractionTestCase {
+    private RegistryFacade subject;
+
+    public void setupSubjects() {
+        subject = new DefaultRegistryFacade();
+    }
+
+    public void testSubject() {
+        // FIX BREADCRUMB 1824 Complete me.
+    }
+}
