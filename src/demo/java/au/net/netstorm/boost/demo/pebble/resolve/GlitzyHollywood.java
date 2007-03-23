a15f8a1025381c1cfa9d0219448523e9a19e82f6
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-03-23 04:33:14 +0000

    Story:  Implement singletons. [id=1824]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1807 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/demo/java/au/net/netstorm/boost/demo/pebble/resolve/GlitzyHollywood.java
+++ src/demo/java/au/net/netstorm/boost/demo/pebble/resolve/GlitzyHollywood.java
@@ -5,6 +5,11 @@ import au.net.netstorm.boost.pebble.core.Pebble;
 public final class GlitzyHollywood implements Pebble, Hollywood {
     private Celebrity celebrity;
     private Actor actor;
+    private Movie movie;
+
+    public Movie getMovie() {
+        return movie;
+    }
 
     public Celebrity getCelebrity() {
         return celebrity;
