d528e46ad7b3d4177118dcad3e5a622f8ca2b372
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-03-23 01:29:15 +0000

    Story:  Implement singletons. [id=1824]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1797 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/pebble/inject/resolver/core/DefaultRegistryFacade.java
+++ src/core/java/au/net/netstorm/boost/pebble/inject/resolver/core/DefaultRegistryFacade.java
@@ -1,7 +1,18 @@
 package au.net.netstorm.boost.pebble.inject.resolver.core;
 
+import au.net.netstorm.boost.util.type.DefaultInterface;
+import au.net.netstorm.boost.util.type.Interface;
+
+// FIX BREADCRUMB 1824 Rename.
 public final class DefaultRegistryFacade implements RegistryFacade {
+    private final RegistryEngine registryEngine;
+
+    public DefaultRegistryFacade(RegistryEngine registryEngine) {
+        this.registryEngine = registryEngine;
+    }
+
     public boolean hasImplementation(Class iface) {
-        return true;
+        Interface inyerface = new DefaultInterface(iface);
+        return registryEngine.hasImplementation(inyerface);
     }
 }
--- src/test/java/au/net/netstorm/boost/pebble/inject/resolver/core/DefaultRegistryFacadeAtomicTest.java
+++ src/test/java/au/net/netstorm/boost/pebble/inject/resolver/core/DefaultRegistryFacadeAtomicTest.java
@@ -1,17 +1,24 @@
 package au.net.netstorm.boost.pebble.inject.resolver.core;
 
 import au.net.netstorm.boost.test.automock.InteractionTestCase;
+import au.net.netstorm.boost.util.type.DefaultInterface;
+import au.net.netstorm.boost.util.type.Interface;
 
+// FIX BREADCRUMB 1824 Complete.
 public final class DefaultRegistryFacadeAtomicTest extends InteractionTestCase {
     private RegistryFacade subject;
+    private Interface inyerface = new DefaultInterface(FrenchCloison.class);
     private Class iface;
+    private RegistryEngine registryEngine;
+    private boolean hasImplementation;
 
     public void setupSubjects() {
-        subject = new DefaultRegistryFacade();
+        subject = new DefaultRegistryFacade(registryEngine);
     }
 
-    // FIX BREADCRUMB 1824 Complete.
     public void testHasImplementation() {
-        subject.hasImplementation(iface);
+        expect.oneCall(registryEngine, hasImplementation, "hasImplementation", inyerface);
+        boolean result = subject.hasImplementation(FrenchCloison.class);
+        assertEquals(hasImplementation, result);
     }
 }
