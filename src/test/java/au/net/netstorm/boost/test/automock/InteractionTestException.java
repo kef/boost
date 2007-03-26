641ad9d57073df6fd14f52abe515b4c9eb6906a6
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-03-26 00:54:50 +0000

    Story:  Add support for arrays in "auto" mocking. [id=1676]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1838 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/test/automock/InteractionTestException.java
+++ src/test/java/au/net/netstorm/boost/test/automock/InteractionTestException.java
@@ -2,5 +2,6 @@ package au.net.netstorm.boost.test.automock;
 
 public final class InteractionTestException extends RuntimeException {
     public InteractionTestException(String s) {
+        super(s);
     }
 }
