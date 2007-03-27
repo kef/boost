96ea09d7744c546cd0278e766257cf9bd131b5e3
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-03-27 04:55:06 +0000

    Story: Add support for auto mocking arrays. [id=1676]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1868 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/test/random/EverythingRandomProvider.java
+++ src/test/java/au/net/netstorm/boost/test/random/EverythingRandomProvider.java
@@ -1,15 +1,9 @@
 package au.net.netstorm.boost.test.random;
 
-import au.net.netstorm.boost.edge.java.lang.reflect.DefaultProxySupplier;
-import au.net.netstorm.boost.edge.java.lang.reflect.ProxySupplier;
 import au.net.netstorm.boost.test.atom.DefaultPrimitiveBoxer;
 import au.net.netstorm.boost.test.atom.PrimitiveBoxer;
-import au.net.netstorm.boost.util.proxy.DefaultProxyFactory;
-import au.net.netstorm.boost.util.proxy.ProxyFactory;
 
 public final class EverythingRandomProvider implements RandomProvider {
-    private ProxySupplier proxySupplier = new DefaultProxySupplier();
-    private ProxyFactory proxyFactory = new DefaultProxyFactory(proxySupplier);
     private PrimitiveBoxer primitiveBoxer = new DefaultPrimitiveBoxer();
     private RandomProvider concretes = new ConcreteRandomProvider();
     private RandomProvider arrays = new ArrayRandomProvider(this);
