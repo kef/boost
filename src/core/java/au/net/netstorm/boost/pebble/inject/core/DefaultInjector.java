78274e015d149df3e2de9849fc3e8e9ece574314
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-03-27 06:58:04 +0000

    Story:  Implement singletons. [id=1824]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1882 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/pebble/inject/core/DefaultInjector.java
+++ src/core/java/au/net/netstorm/boost/pebble/inject/core/DefaultInjector.java
@@ -3,7 +3,6 @@ package au.net.netstorm.boost.pebble.inject.core;
 import au.net.netstorm.boost.util.type.DefaultBaseReference;
 import au.net.netstorm.boost.util.type.UnresolvedInstance;
 
-// FIX 32755 Move out of here.
 public final class DefaultInjector implements Injector {
     private final InjectorEngine engine;
 
--- src/core/java/au/net/netstorm/boost/pebble/inject/core/InjectorEngine.java
+++ src/core/java/au/net/netstorm/boost/pebble/inject/core/InjectorEngine.java
@@ -2,7 +2,6 @@ package au.net.netstorm.boost.pebble.inject.core;
 
 import au.net.netstorm.boost.util.type.UnresolvedInstance;
 
-// FIX 32755 Belongs somewhere else.
 public interface InjectorEngine {
     void inject(UnresolvedInstance unresolved);
 }
