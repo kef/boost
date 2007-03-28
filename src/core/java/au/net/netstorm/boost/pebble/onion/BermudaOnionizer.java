98f0cc3af9cf35db51d5e0f232b88fcf333efc5f
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-03-28 00:57:03 +0000

    Story:  Implement singletons. [id=1824]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1899 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/pebble/onion/BermudaOnionizer.java
+++ src/core/java/au/net/netstorm/boost/pebble/onion/BermudaOnionizer.java
@@ -2,6 +2,7 @@ package au.net.netstorm.boost.pebble.onion;
 
 import au.net.netstorm.boost.util.type.ResolvedInstance;
 
+// FIX 32755 Roll following fixes into new card.
 // FIX 32755 Use the Onion interface to mark the front door proxy.
 // FIX 32755 Create a utility to determine whether an object reference is an onion or not.
 
