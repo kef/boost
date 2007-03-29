48da443ed61eb68a27257e587beb762040c477ca
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-03-29 07:12:14 +0000

    Story:  Add support for arrays in "auto" mocking. [id=1676]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1906 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/pebble/inject/resolver/field/DefaultResolverFieldFinder.java
+++ src/core/java/au/net/netstorm/boost/pebble/inject/resolver/field/DefaultResolverFieldFinder.java
@@ -30,18 +30,12 @@ public final class DefaultResolverFieldFinder implements ResolverFieldFinder {
     // DEBT CyclomaticComplexity|NCSS {
     private void optionallyAdd(List result, Object ref, Field field) {
         field.setAccessible(true);
-        if (isSynthetic(field)) return;
         if (!isNull(ref, field)) return;
         if (isFinal(field)) return;
         if (isPrivate(field)) return;
         result.add(field);
     }
     // } DEBT CyclomaticComplexity|NCSS
-
-    private boolean isSynthetic(Field field) {
-        String fieldName = field.getName();
-        return fieldName.contains("$");
-    }
 
     private boolean isPrivate(Field field) {
         int modifiers = field.getModifiers();
