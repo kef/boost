e6cb511bb18e6aee02da6d71f1947ac018cdb407
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-04-19 04:05:30 +0000

    Story:  Remove "registry" from spider. Morph Spider to SpiderEngine. [id=1914]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1949 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/spider/resolve/DefaultResolver.java
+++ src/core/java/au/net/netstorm/boost/spider/resolve/DefaultResolver.java
@@ -1,11 +1,19 @@
 package au.net.netstorm.boost.spider.resolve;
 
+import au.net.netstorm.boost.util.type.DefaultInterface;
+import au.net.netstorm.boost.util.type.Interface;
+
 public final class DefaultResolver implements Resolver {
+    private final ResolverEngine engine;
+
     // FIX BREADCRUMB 1914 Complete.
     public DefaultResolver(ResolverEngine engine) {
+        this.engine = engine;
     }
 
     public Object resolve(Class type) {
+        Interface iface = new DefaultInterface(type);
+        engine.resolve(iface);
         return null;
     }
 }
--- src/test/java/au/net/netstorm/boost/spider/resolve/DefaultResolverAtomicTest.java
+++ src/test/java/au/net/netstorm/boost/spider/resolve/DefaultResolverAtomicTest.java
@@ -1,10 +1,16 @@
 package au.net.netstorm.boost.spider.resolve;
 
 import au.net.netstorm.boost.test.automock.InteractionTestCase;
+import au.net.netstorm.boost.util.type.DefaultInterface;
+import au.net.netstorm.boost.util.type.Interface;
+import au.net.netstorm.boost.util.type.ResolvedInstance;
 
 public final class DefaultResolverAtomicTest extends InteractionTestCase {
+    private static final Class FRUITY = Fruity.class;
     Resolver subject;
     ResolverEngine resolverEngine;
+    ResolvedInstance resolved;
+    Interface fruity = new DefaultInterface(FRUITY);
 
     public void setupSubjects() {
         subject = new DefaultResolver(resolverEngine);
@@ -12,6 +18,7 @@ public final class DefaultResolverAtomicTest extends InteractionTestCase {
 
     // FIX 1914 Complete this.
     public void testResolve() {
-        Object result = subject.resolve(Fruity.class);
+        expect.oneCall(resolverEngine, resolved, "resolve", fruity);
+        Object result = subject.resolve(FRUITY);
     }
 }
