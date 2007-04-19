59a558c62d2cb2aea5ee1f84e7107c0123044f23
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-04-19 03:48:08 +0000

    Story:  Remove "registry" from spider. Morph Spider to SpiderEngine. [id=1914]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1943 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/demo/java/au/net/netstorm/boost/demo/spider/core/Spider.java
+++ src/demo/java/au/net/netstorm/boost/demo/spider/core/Spider.java
@@ -5,5 +5,6 @@ import au.net.netstorm.boost.spider.inject.core.Injector;
 import au.net.netstorm.boost.spider.resolve.Registry;
 import au.net.netstorm.boost.spider.resolve.ResolverEngine;
 
+// FIX BREADCRUMB 1914 -AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA The resolver engine should be a resolver.
 public interface Spider extends Provider, Injector, ResolverEngine, Registry {
 }
