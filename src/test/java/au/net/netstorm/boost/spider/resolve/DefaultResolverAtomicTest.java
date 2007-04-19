970351b9aa99972c3a6f93f4a6de29f33e25a2d6
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-04-19 03:51:03 +0000

    Story:  Remove "registry" from spider. Morph Spider to SpiderEngine. [id=1914]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1944 05db6a09-9903-0410-bedf-ba9bc4063e48

---
+++ src/test/java/au/net/netstorm/boost/spider/resolve/DefaultResolverAtomicTest.java
@@ -0,0 +1,12 @@
+package au.net.netstorm.boost.spider.resolve;
+
+import au.net.netstorm.boost.test.cases.BoooostCase;
+
+public final class DefaultResolverAtomicTest extends BoooostCase {
+    Resolver resolver;
+
+    // FIX 1914 Complete this.
+    // FIX 1914 Rename.
+    public void testXxx() {
+    }
+}
