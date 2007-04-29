c8c728e67652262940a16d1e1ab9eb0ed65689d4
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-04-29 02:57:55 +0000

    Story:  Implement "scrunch" on spider and create "spinner" [id=1936]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1983 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/util/exception/DefaultThrowableMasterAtomicTest.java
+++ src/test/java/au/net/netstorm/boost/util/exception/DefaultThrowableMasterAtomicTest.java
@@ -5,27 +5,19 @@ import au.net.netstorm.boost.test.cases.BoooostCase;
 public final class DefaultThrowableMasterAtomicTest extends BoooostCase {
     private static final Throwable EXCEPTION_1 = new Exception();
     private static final Throwable EXCEPTION_2 = new ClassNotFoundException();
+    private static final Throwable THROWABLE_1 = new Throwable();
+    private static final Throwable THROWABLE_2 = new DirectThrowableException();
     private static final Throwable RUNTIME_1 = new RuntimeException();
     private static final Throwable RUNTIME_2 = new IllegalArgumentException();
     private static final Throwable ERROR_1 = new Error();
     private static final Throwable ERROR_2 = new InternalError();
-    private static final Throwable THROWABLE_1 = new Throwable();
-
     private ThrowableMaster subject = new DefaultThrowableMaster();
 
-    // FIX 1936 Complete this.
-    // FIX 1936 Check Error.
-    // FIX 1936 Check Subclass of Error.
-    // FIX 1936 Check Exception.
-    // FIX 1936 Check subclass of Exception.
-    // FIX 1936 Check Throwable.
-    // FIX 1936 Check Runtime.
-    // FIX 1936 Check subclass of Runtime.
-    // FIX 1936 Check subclass of Throwable.
     public void testChecked() {
         isChecked(true, EXCEPTION_1);
         isChecked(true, EXCEPTION_2);
         isChecked(true, THROWABLE_1);
+        isChecked(true, THROWABLE_2);
         isChecked(false, RUNTIME_1);
         isChecked(false, RUNTIME_2);
         isChecked(false, ERROR_1);
