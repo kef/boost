4e03b651538f9ca882dcce307ccff068982fe48e
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-04-29 02:54:03 +0000

    Story:  Implement "scrunch" on spider and create "spinner" [id=1936]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1981 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/util/exception/DefaultThrowableMaster.java
+++ src/core/java/au/net/netstorm/boost/util/exception/DefaultThrowableMaster.java
@@ -4,6 +4,7 @@ package au.net.netstorm.boost.util.exception;
 public final class DefaultThrowableMaster implements ThrowableMaster {
     public boolean isChecked(Throwable t) {
         if (t instanceof RuntimeException) return false;
+        if (t instanceof Error) return false;
         return true;
     }
 }
--- src/test/java/au/net/netstorm/boost/util/exception/DefaultThrowableMasterAtomicTest.java
+++ src/test/java/au/net/netstorm/boost/util/exception/DefaultThrowableMasterAtomicTest.java
@@ -3,15 +3,17 @@ package au.net.netstorm.boost.util.exception;
 import au.net.netstorm.boost.test.cases.BoooostCase;
 
 public final class DefaultThrowableMasterAtomicTest extends BoooostCase {
-    private static final Throwable RUNTIME_1 = new RuntimeException();
-    private static final Throwable RUNTIME_2 = new IllegalArgumentException();
     private static final Throwable EXCEPTION_1 = new Exception();
     private static final Throwable EXCEPTION_2 = new ClassNotFoundException();
+    private static final Throwable RUNTIME_1 = new RuntimeException();
+    private static final Throwable RUNTIME_2 = new IllegalArgumentException();
+    private static final Throwable ERROR_1 = new Error();
 
     private ThrowableMaster subject = new DefaultThrowableMaster();
 
     // FIX 1936 Complete this.
     // FIX 1936 Check Error.
+    // FIX 1936 Check Subclass of Error.
     // FIX 1936 Check Exception.
     // FIX 1936 Check subclass of Exception.
     // FIX 1936 Check Throwable.
@@ -23,6 +25,7 @@ public final class DefaultThrowableMasterAtomicTest extends BoooostCase {
         isChecked(true, EXCEPTION_2);
         isChecked(false, RUNTIME_1);
         isChecked(false, RUNTIME_2);
+        isChecked(false, ERROR_1);
     }
 
     private void isChecked(boolean expected, Throwable throwable) {
