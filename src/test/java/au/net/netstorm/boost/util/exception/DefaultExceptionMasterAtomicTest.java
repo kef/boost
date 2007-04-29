962070eb38c2d700ea017a9c36170b9a66b55d11
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-04-29 02:48:17 +0000

    Story:  Implement "scrunch" on spider and create "spinner" [id=1936]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1977 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/util/exception/DefaultExceptionMasterAtomicTest.java
+++ src/test/java/au/net/netstorm/boost/util/exception/DefaultExceptionMasterAtomicTest.java
@@ -3,7 +3,10 @@ package au.net.netstorm.boost.util.exception;
 import au.net.netstorm.boost.test.cases.BoooostCase;
 
 public final class DefaultExceptionMasterAtomicTest extends BoooostCase {
-    private static final RuntimeException RUNTIME_1 = new RuntimeException();
+    private static final Throwable RUNTIME_1 = new RuntimeException();
+    // FIX 1936 Incorporate #2.
+    private static final Throwable RUNTIME_2 = new IllegalArgumentException();
+    private static final Throwable EXCEPTION_1 = new Exception();
     private ExceptionMaster subject = new DefaultExceptionMaster();
 
     // FIX 1936 Complete this.
@@ -14,6 +17,7 @@ public final class DefaultExceptionMasterAtomicTest extends BoooostCase {
     // FIX 1936 Check Runtime.
     // FIX 1936 Check subclass of Runtime.
     public void testChecked() {
+        isChecked(true, EXCEPTION_1);
         isChecked(false, RUNTIME_1);
     }
 
