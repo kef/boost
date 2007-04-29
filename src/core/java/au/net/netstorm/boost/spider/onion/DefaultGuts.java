b8df83a5137acf6f27691351cb2826b95e889379
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-04-29 04:19:56 +0000

    Story:  Implement "scrunch" on spider and create "spinner" [id=1936]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1998 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/spider/onion/DefaultGuts.java
+++ src/core/java/au/net/netstorm/boost/spider/onion/DefaultGuts.java
@@ -1,4 +1,6 @@
 package au.net.netstorm.boost.spider.onion;
 
 public final class DefaultGuts implements Guts {
+    public DefaultGuts(Object ref) {
+    }
 }
--- src/test/java/au/net/netstorm/boost/spider/onion/DefaultGutsAtomicTest.java
+++ src/test/java/au/net/netstorm/boost/spider/onion/DefaultGutsAtomicTest.java
@@ -1,12 +1,15 @@
 package au.net.netstorm.boost.spider.onion;
 
+import java.util.HashSet;
+import java.util.Set;
 import au.net.netstorm.boost.test.automock.InteractionTestCase;
 
 public final class DefaultGutsAtomicTest extends InteractionTestCase {
     Guts subject;
+    Set guts = new HashSet();
 
     public void setupSubjects() {
-        subject = new DefaultGuts();
+        subject = new DefaultGuts(guts);
     }
 
     // FIX 1936 Complete.
