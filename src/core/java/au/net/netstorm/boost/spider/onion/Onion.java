11acc119d595ac2fe6bbbef378b90079b441875c
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-04-29 03:37:54 +0000

    Story:  Implement "scrunch" on spider and create "spinner" [id=1936]
    git-svn-id: https://geekscape.org/svn/boost/trunk@1991 05db6a09-9903-0410-bedf-ba9bc4063e48

---
+++ src/core/java/au/net/netstorm/boost/spider/onion/Onion.java
@@ -0,0 +1,7 @@
+package au.net.netstorm.boost.spider.onion;
+
+// FIX 1936 Create "Core".  This should implement Layer and call the core object.
+
+// FIX 1936 Create "Skin".
+public interface Onion {
+}
