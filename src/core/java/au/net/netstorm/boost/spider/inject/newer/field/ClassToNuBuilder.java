7d1858adeb9c032fc62e895daaacd19030ac82d6
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-05-09 05:43:27 +0000

    Story:  Split "create"/"put" key out of KeyClass. [id=39663]
    git-svn-id: https://geekscape.org/svn/boost/trunk@2014 05db6a09-9903-0410-bedf-ba9bc4063e48

---
+++ src/core/java/au/net/netstorm/boost/spider/inject/newer/field/ClassToNuBuilder.java
@@ -0,0 +1,7 @@
+package au.net.netstorm.boost.spider.inject.newer.field;
+
+import au.net.netstorm.boost.util.type.Implementation;
+
+public interface ClassToNuBuilder {
+    Implementation build(Class newerType);
+}
--- src/core/java/au/net/netstorm/boost/spider/inject/newer/field/DefaultNewerFieldBuilder.java
+++ src/core/java/au/net/netstorm/boost/spider/inject/newer/field/DefaultNewerFieldBuilder.java
@@ -1,32 +1,18 @@
 package au.net.netstorm.boost.spider.inject.newer.field;
 
 import java.lang.reflect.Field;
-import au.net.netstorm.boost.edge.java.lang.DefaultEdgeClass;
-import au.net.netstorm.boost.edge.java.lang.EdgeClass;
-import au.net.netstorm.boost.edge.java.lang.reflect.DefaultEdgeField;
-import au.net.netstorm.boost.edge.java.lang.reflect.EdgeField;
-import au.net.netstorm.boost.util.type.DefaultImplementation;
 import au.net.netstorm.boost.util.type.DefaultInterface;
 import au.net.netstorm.boost.util.type.Implementation;
 import au.net.netstorm.boost.util.type.Interface;
 
 public final class DefaultNewerFieldBuilder implements NewerFieldBuilder {
-    private static final Object IGNORED = new Object();
-    private final EdgeClass edgeClass = new DefaultEdgeClass();
-    private final EdgeField edgeField = new DefaultEdgeField();
+    private final ClassToNuBuilder classtoNuBuilder = new DefaultClassToNuBuilder();
 
     public NewerField build(Field field) {
         Class newerType = field.getType();
         Interface newerInterface = new DefaultInterface(newerType);
-        Implementation classToNu = getClasstoNu(newerType);
+        Implementation classToNu = classtoNuBuilder.build(newerType);
         String fieldName = field.getName();
         return new DefaultNewerField(newerInterface, classToNu, fieldName);
-    }
-
-    private Implementation getClasstoNu(Class newerType) {
-        Field classToNuField = edgeClass.getDeclaredField(newerType, "CLASS_TO_NU");
-        classToNuField.setAccessible(true);
-        Class classToNu = (Class) edgeField.get(classToNuField, IGNORED);
-        return new DefaultImplementation(classToNu);
     }
 }
