c8f711a4b57f4a1c1124a303fbee616c02c37bb5
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-05-09 07:56:48 +0000

    Story:  Split "create"/"put" key out of KeyClass. [id=39663]
    git-svn-id: https://geekscape.org/svn/boost/trunk@2017 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/spider/inject/newer/DefaultNewerBuilderAtomicTest.java
+++ src/test/java/au/net/netstorm/boost/spider/inject/newer/DefaultNewerBuilderAtomicTest.java
@@ -1,13 +1,25 @@
 package au.net.netstorm.boost.spider.inject.newer;
 
+import au.net.netstorm.boost.edge.EdgeException;
 import au.net.netstorm.boost.test.cases.BoooostCase;
 import au.net.netstorm.boost.util.type.DefaultInterface;
 
 public final class DefaultNewerBuilderAtomicTest extends BoooostCase {
     NewerAssembler subject = new DefaultNewerAssembler();
 
-    public void testBuild() {
+    public void testAssemble() {
         Object result = subject.assemble(new DefaultInterface(NewDefaultTestDummy.class));
         assertEquals(true, result instanceof NewDefaultTestDummy);
+    }
+
+    public void testAssembleFailsForNonNewers() {
+        try {
+            subject.assemble(new DefaultInterface(CharSequence.class));
+            fail();
+        } catch (EdgeException result) {
+            String expectedMessage = "There is no such field called \"CLASS_TO_NU\" in class java.lang.CharSequence";
+            String actualMessage = result.getMessage();
+            assertEquals(expectedMessage, actualMessage);
+        }
     }
 }
