688cba32a066e2e2d925b1fa28e7d07e6c8d34f7
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-05-17 00:23:33 +0000

    Story:  Split "create"/"put" key out of KeyClass. [id=39663]
    git-svn-id: https://geekscape.org/svn/boost/trunk@2044 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/test/inject/DefaultSubjectInjector.java
+++ src/test/java/au/net/netstorm/boost/test/inject/DefaultSubjectInjector.java
@@ -19,7 +19,7 @@ public final class DefaultSubjectInjector implements SubjectInjector {
     private final EdgeClass classer = new DefaultEdgeClass();
 
     public void inject(UsesMocks testCase) {
-        Object subject = tryGetSubject(testCase);
+        Object subject = nullGetSubject(testCase);
         // FIX 39663 Is this ok for tests to have no subject.
         if (subject != null) {
             Field[] unresolvedFields = finder.find(subject);
@@ -28,7 +28,7 @@ public final class DefaultSubjectInjector implements SubjectInjector {
         }
     }
 
-    private Object tryGetSubject(UsesMocks testCase) {
+    private Object nullGetSubject(UsesMocks testCase) {
         try {
             return fielder.getInstance(testCase, "subject");
         } catch (EdgeException e) {
