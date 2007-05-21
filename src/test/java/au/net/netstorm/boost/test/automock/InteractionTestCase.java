2ca6a036d003c76c19b898250fdf4a107d24f334
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-05-21 03:45:32 +0000

    1524 Broken window fixing.
    git-svn-id: https://geekscape.org/svn/boost/trunk@2066 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/test/automock/InteractionTestCase.java
+++ src/test/java/au/net/netstorm/boost/test/automock/InteractionTestCase.java
@@ -2,6 +2,6 @@ package au.net.netstorm.boost.test.automock;
 
 import au.net.netstorm.boost.test.cases.StrategyTestCase;
 
+// FIX 1524 Remove.
 public abstract class InteractionTestCase extends StrategyTestCase implements UsesMocks {
-    public MockExpectations expect;
 }
--- src/test/java/au/net/netstorm/boost/test/cases/StrategyTestCase.java
+++ src/test/java/au/net/netstorm/boost/test/cases/StrategyTestCase.java
@@ -1,10 +1,12 @@
 package au.net.netstorm.boost.test.cases;
 
 import au.net.netstorm.boost.test.automock.InteractionTestLifecycle;
+import au.net.netstorm.boost.test.automock.MockExpectations;
 
 // FIX 1524 Rename to something nicer.
 public class StrategyTestCase extends BoooostCase {
     private TestLifecycle lifecycle = new InteractionTestLifecycle(this);
+    public MockExpectations expect;
 
     public void runBare() throws Throwable {
         lifecycle.initialise();
