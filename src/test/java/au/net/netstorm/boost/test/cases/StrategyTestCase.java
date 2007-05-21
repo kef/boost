4d5d7bec5e620b86646863bc5392a9cc048ce88e
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-05-21 01:57:43 +0000

    1524 Broken window fixing.
    git-svn-id: https://geekscape.org/svn/boost/trunk@2060 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/test/cases/StrategyTestCase.java
+++ src/test/java/au/net/netstorm/boost/test/cases/StrategyTestCase.java
@@ -3,6 +3,7 @@ package au.net.netstorm.boost.test.cases;
 import au.net.netstorm.boost.test.automock.InteractionTestStrategy;
 import au.net.netstorm.boost.test.automock.UsesMocks;
 
+// FIX 1524 Rename to something nicer.
 public class StrategyTestCase extends BoooostCase {
     // FIX 1524 Remove the cast.
     private TestStrategy strategy = new InteractionTestStrategy((UsesMocks) this);
