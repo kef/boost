d54b67f70af25c28ea6c6aabcb6a15b5bdd33939
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-05-24 03:30:13 +0000

    Story: New interceptor ("pebble"). [id=1665]
    git-svn-id: https://geekscape.org/svn/boost/trunk@2095 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/demo/java/au/net/netstorm/boost/demo/spider/resolve/ComplexCyclicDependenciesDemoTest.java
+++ src/demo/java/au/net/netstorm/boost/demo/spider/resolve/ComplexCyclicDependenciesDemoTest.java
@@ -4,20 +4,20 @@ import au.net.netstorm.boost.demo.spider.core.Spider;
 import au.net.netstorm.boost.demo.spider.newer.DefaultSpiderAssembler;
 import au.net.netstorm.boost.demo.spider.newer.SpiderAssembler;
 import au.net.netstorm.boost.spider.core.GoodCitizen;
-import au.net.netstorm.boost.spider.resolve.Registry;
+import au.net.netstorm.boost.spider.resolve.SpiderWeb;
 import au.net.netstorm.boost.test.cases.BoooostCase;
 
 // FIX 1971 Consider a single object referencing itself.
 public final class ComplexCyclicDependenciesDemoTest extends BoooostCase {
     private final SpiderAssembler spiderAssembler = new DefaultSpiderAssembler(GoodCitizen.class);
     private final Spider spider = spiderAssembler.assemble();
-    private final Registry registry = spider;
+    private final SpiderWeb web = spider;
 
     {
-        registry.prototype(Teacher.class, DefaultTeacher.class);
-        registry.prototype(Student.class, DefaultStudent.class);
-        registry.prototype(Homework.class, DefaultHomework.class);
-        registry.prototype(Exam.class, DefaultExam.class);
+        web.prototype(Teacher.class, DefaultTeacher.class);
+        web.prototype(Student.class, DefaultStudent.class);
+        web.prototype(Homework.class, DefaultHomework.class);
+        web.prototype(Exam.class, DefaultExam.class);
     }
 
     // FIX BREADCRUMB 1971 Rename.
--- src/demo/java/au/net/netstorm/boost/demo/spider/resolve/SimpleCyclicDependenciesDemoTest.java
+++ src/demo/java/au/net/netstorm/boost/demo/spider/resolve/SimpleCyclicDependenciesDemoTest.java
@@ -4,7 +4,7 @@ import au.net.netstorm.boost.demo.spider.core.Spider;
 import au.net.netstorm.boost.demo.spider.newer.DefaultSpiderAssembler;
 import au.net.netstorm.boost.demo.spider.newer.SpiderAssembler;
 import au.net.netstorm.boost.spider.core.GoodCitizen;
-import au.net.netstorm.boost.spider.resolve.Registry;
+import au.net.netstorm.boost.spider.resolve.SpiderWeb;
 import au.net.netstorm.boost.test.cases.BoooostCase;
 import au.net.netstorm.boost.test.reflect.util.DefaultFieldTestUtil;
 import au.net.netstorm.boost.test.reflect.util.FieldTestUtil;
@@ -13,12 +13,12 @@ import au.net.netstorm.boost.test.reflect.util.FieldTestUtil;
 public final class SimpleCyclicDependenciesDemoTest extends BoooostCase {
     private final SpiderAssembler spiderAssembler = new DefaultSpiderAssembler(GoodCitizen.class);
     private final Spider spider = spiderAssembler.assemble();
-    private final Registry registry = spider;
+    private final SpiderWeb web = spider;
     private final FieldTestUtil fielder = new DefaultFieldTestUtil();
 
     {
-        registry.prototype(Ball.class, DefaultBall.class);
-        registry.prototype(Runner.class, DefaultRunner.class);
+        web.prototype(Ball.class, DefaultBall.class);
+        web.prototype(Runner.class, DefaultRunner.class);
     }
 
     public void testDependencyWithTwoObjects() {
