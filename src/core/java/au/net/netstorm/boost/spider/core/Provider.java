a2e99d5ac74cabd1e83a6e64d72732735f7fb1ab
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-06-01 12:20:55 +0000

    Story:  Add flavours to Spider. [id=1977]
    git-svn-id: https://geekscape.org/svn/boost/trunk@2168 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/spider/core/Provider.java
+++ src/core/java/au/net/netstorm/boost/spider/core/Provider.java
@@ -3,5 +3,6 @@ package au.net.netstorm.boost.spider.core;
 public interface Provider {
     Object provide(Class type);
 
+    // FIX BREADCRUMB 1977 FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF Remove me.
     Object provide(Class type, Object[] parameters);
 }
