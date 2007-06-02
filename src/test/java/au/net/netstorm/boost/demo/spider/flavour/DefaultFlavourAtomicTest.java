d8f1e61ab0d07b36c92eb17872344778b2269624
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-06-02 03:02:56 +0000

    Story: Add flavours to Spider. [id=1977]
    git-svn-id: https://geekscape.org/svn/boost/trunk@2196 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/demo/spider/flavour/DefaultFlavourAtomicTest.java
+++ src/test/java/au/net/netstorm/boost/demo/spider/flavour/DefaultFlavourAtomicTest.java
@@ -3,10 +3,12 @@ package au.net.netstorm.boost.demo.spider.flavour;
 import au.net.netstorm.boost.test.cases.BoooostCase;
 
 // FIX BREADCRUMB 1977 JJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJ Complete.
+
+// FIX 1977 Drive up InterfaceFlavour composite.
 public final class DefaultFlavourAtomicTest extends BoooostCase {
     FlavourMap subject;
 
-    public void testFlavours() {
-        // FIX 1977 Complete.
+    public void testFlavoured() {
+        // FIX 1977 Complete.  Delegate after having composed InterfaceFlavour delegate.
     }
 }
