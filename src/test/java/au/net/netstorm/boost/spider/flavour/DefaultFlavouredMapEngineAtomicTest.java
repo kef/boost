aeb5f2c4939628a8a310b0373c2df4c02de86e71
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-06-02 06:41:01 +0000

    Story: Add flavours to Spider. [id=1977]
    git-svn-id: https://geekscape.org/svn/boost/trunk@2213 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/spider/flavour/DefaultFlavouredMapEngineAtomicTest.java
+++ src/test/java/au/net/netstorm/boost/spider/flavour/DefaultFlavouredMapEngineAtomicTest.java
@@ -7,6 +7,8 @@ import au.net.netstorm.boost.util.type.DefaultInterface;
 import au.net.netstorm.boost.util.type.Interface;
 
 // FIX BREADCRUMB 1977 LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL Finish this.
+
+// OK NCSS {
 public final class DefaultFlavouredMapEngineAtomicTest extends InteractionTestCase implements UsesAutoMocks, HasSubjects {
     FlavouredMapEngine subject;
     Interface milkshake = new DefaultInterface(Milkshake.class);
@@ -23,6 +25,7 @@ public final class DefaultFlavouredMapEngineAtomicTest extends InteractionTestCa
     FlavouredInterface icecreamChocolate = mix(icecream, chocolate);
     FlavouredInterface icecreamVanilla = mix(icecream, vanilla);
     FlavouredInterface icecreamStrawberry = mix(icecream, strawberry);
+    FlavouredInterface icecreamUnflavoured = mix(icecream, unflavoured);
     FlavouredInterface chipsUnflavoured = mix(chips, unflavoured);
     FlavouredInterface pieStrawberry = mix(pie, strawberry);
     Object value, value1, value2, value3, value4, value5;
@@ -40,31 +43,42 @@ public final class DefaultFlavouredMapEngineAtomicTest extends InteractionTestCa
         subject = new DefaultFlavouredMapEngine();
     }
 
-    // OK NCSS {
     public void testMainFlow() {
-        subject.put(milkshakeUnflavoured, value1);
-        subject.put(chipsUnflavoured, value2);
-        subject.put(icecreamChocolate, value3);
-        subject.put(icecreamStrawberry, value4);
-        subject.put(pieStrawberry, value5);
-        checkGet(value1, milkshakeUnflavoured);
-        checkGet(value1, milkshakeChocolate);
-        checkGet(value1, milkshakeVanilla);
-        checkGet(value2, chipsUnflavoured);
-        checkGet(value3, icecreamChocolate);
-        checkGet(value4, icecreamStrawberry);
-        checkGet(value5, pieStrawberry);
+        put(milkshakeUnflavoured, value1);
+        put(chipsUnflavoured, value2);
+        put(icecreamChocolate, value3);
+        put(icecreamStrawberry, value4);
+        put(pieStrawberry, value5);
+        checkGet(milkshakeUnflavoured, value1);
+        checkGet(milkshakeChocolate, value1);
+        checkGet(milkshakeVanilla, value1);
+        checkGet(chipsUnflavoured, value2);
+        checkGet(icecreamChocolate, value3);
+        checkGet(icecreamStrawberry, value4);
+        checkGet(pieStrawberry, value5);
     }
-    // } OK NCSS - A juicy scenario :-) 
 
-    public void testNothingMatches() {
-        subject.put(milkshakeUnflavoured, value);
+    public void testUnflavouredInMap() {
+        put(milkshakeUnflavoured, value);
+        checkGet(milkshakeUnflavoured, value);
+        checkGet(milkshakeChocolate, value);
+        checkGet(milkshakeVanilla, value);
+        checkGetFails(icecreamStrawberry);
         checkGetFails(chipsUnflavoured);
         checkGetFails(pieStrawberry);
-        checkGetFails(icecreamStrawberry);
     }
 
-    private void checkGet(Object expected, FlavouredInterface key) {
+    public void testFlavouredInMap() {
+        put(icecreamChocolate, value);
+        checkGet(icecreamChocolate, value);
+        checkGetFails(icecreamStrawberry);
+        checkGetFails(icecreamUnflavoured);
+        checkGetFails(icecreamVanilla);
+        checkGetFails(pieStrawberry);
+        checkGetFails(chipsUnflavoured);
+    }
+
+    private void checkGet(FlavouredInterface key, Object expected) {
         Object result = subject.get(key);
         assertEquals(expected, result);
     }
@@ -79,4 +93,10 @@ public final class DefaultFlavouredMapEngineAtomicTest extends InteractionTestCa
     private FlavouredInterface mix(Interface iface, Flavour flavour) {
         return new DefaultFlavouredInterface(iface, flavour);
     }
+
+    // } OK NCSS - A juicy scenario :-)
+    private void put(FlavouredInterface flavour, Object value) {
+        subject.put(flavour, value);
+    }
 }
+// } OK NCSS - Some nice juicy mixes.
