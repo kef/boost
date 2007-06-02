105ea9d3027c9adcec2a17b560a1e50b65b2cd6a
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-06-02 12:11:25 +0000

    Story: Add flavours to Spider. [id=1977]
    git-svn-id: https://geekscape.org/svn/boost/trunk@2233 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/spider/flavour/DefaultFlavouredMapAtomicTest.java
+++ src/test/java/au/net/netstorm/boost/spider/flavour/DefaultFlavouredMapAtomicTest.java
@@ -1,16 +1,36 @@
 package au.net.netstorm.boost.spider.flavour;
 
-import au.net.netstorm.boost.test.cases.BoooostCase;
+import au.net.netstorm.boost.test.automock.HasSubjects;
+import au.net.netstorm.boost.test.automock.InteractionTestCase;
+import au.net.netstorm.boost.test.automock.UsesAutoMocks;
+import au.net.netstorm.boost.util.type.Interface;
 
 // FIX BREADCRUMB 1977 JJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJ Complete.
 
 // FIX 1977 Drive up InterfaceFlavour composite.
 
 // FIX BREADCRUMB 1977 KKKKKKKKKKKKKKKKKKKKKKKKKKKKK Back here when the FlavouredMapEngine is complete.
-public final class DefaultFlavouredMapAtomicTest extends BoooostCase {
+public final class DefaultFlavouredMapAtomicTest extends InteractionTestCase implements HasSubjects, UsesAutoMocks {
     FlavouredMap subject;
+    FlavouredMapEngine engine;
+    FlavouredInterface flavoured;
+    Interface iface;
+    Flavour flavour;
+    Object value;
 
-    public void testFlavoured() {
-        // FIX 1977 Complete.  Delegate after having composed InterfaceFlavour delegate.
+    public void setupSubjects() {
+        flavoured = new DefaultFlavouredInterface(iface, flavour);
+        subject = new DefaultFlavouredMap(engine);
+    }
+
+    public void testGet() {
+        expect.oneCall(engine, value, "get", flavoured);
+        Object result = subject.get(iface, flavour);
+        assertEquals(value, result);
+    }
+
+    public void testPut() {
+        expect.oneCall(engine, VOID, "put", flavoured, value);
+        subject.put(iface, flavour, value);
     }
 }
