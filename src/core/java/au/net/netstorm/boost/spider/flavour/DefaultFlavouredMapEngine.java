113c9ae14eb77d0cf8a1830b0696c5d8e1e01edf
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-06-02 03:33:58 +0000

    Story: Add flavours to Spider. [id=1977]
    git-svn-id: https://geekscape.org/svn/boost/trunk@2201 05db6a09-9903-0410-bedf-ba9bc4063e48

---
+++ src/core/java/au/net/netstorm/boost/spider/flavour/DefaultFlavouredMapEngine.java
@@ -0,0 +1,6 @@
+package au.net.netstorm.boost.spider.flavour;
+
+final class DefaultFlavouredMapEngine implements FlavouredMapEngine {
+    public void put(FlavouredInterface flavoured, Object value) {
+    }
+}
--- src/test/java/au/net/netstorm/boost/spider/flavour/DefaultFlavouredMapEngineAtomicTest.java
+++ src/test/java/au/net/netstorm/boost/spider/flavour/DefaultFlavouredMapEngineAtomicTest.java
@@ -1,11 +1,33 @@
 package au.net.netstorm.boost.spider.flavour;
 
-import au.net.netstorm.boost.test.cases.BoooostCase;
+import au.net.netstorm.boost.test.automock.HasSubjects;
+import au.net.netstorm.boost.test.automock.InteractionTestCase;
+import au.net.netstorm.boost.test.automock.UsesAutoMocks;
+import au.net.netstorm.boost.util.type.DefaultInterface;
+import au.net.netstorm.boost.util.type.Interface;
 
 // FIX BREADCRUMB 1977 LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL Finish this.
-public final class DefaultFlavouredMapEngineAtomicTest extends BoooostCase {
+public final class DefaultFlavouredMapEngineAtomicTest extends InteractionTestCase implements UsesAutoMocks, HasSubjects {
+    FlavouredMapEngine subject;
+    Interface milkshake = new DefaultInterface(Milkshake.class);
+    Flavour chocolate = new DefaultFlavour("chocolate");
+    Flavour vanilla = new DefaultFlavour("vanilla");
+    Flavour unflavoured = Flavour.UNFLAVOURED;
+    FlavouredInterface milkshakeChocolate = mix(milkshake, chocolate);
+    FlavouredInterface milkshakeVanilla = mix(milkshake, vanilla);
+    FlavouredInterface milkshakeUnflavoured = mix(milkshake, unflavoured);
+    Object value1;
     // FIX 1977 Complete.
 
+    public void setupSubjects() {
+        subject = new DefaultFlavouredMapEngine();
+    }
+
     public void testUnflavoured() {
+        subject.put(milkshakeUnflavoured, value1);
+    }
+
+    private FlavouredInterface mix(Interface iface, Flavour flavour) {
+        return new DefaultFlavouredInterface(iface, flavour);
     }
 }
