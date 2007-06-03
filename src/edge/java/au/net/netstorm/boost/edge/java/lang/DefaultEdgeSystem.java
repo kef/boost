5a50384060aa700304f7f813aed59b543229c0f1
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-06-03 23:44:01 +0000

    Story:  Reorganise "acceptance" and "molecular" test structures. [id=52894]
    git-svn-id: https://geekscape.org/svn/boost/trunk@2249 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/edge/java/au/net/netstorm/boost/edge/java/lang/DefaultEdgeSystem.java
+++ src/edge/java/au/net/netstorm/boost/edge/java/lang/DefaultEdgeSystem.java
@@ -1,6 +1,8 @@
 package au.net.netstorm.boost.edge.java.lang;
 
-public final class DefaultEdgeSystem implements EdgeSystem {
+import au.net.netstorm.boost.spider.core.GoodCitizen;
+
+public final class DefaultEdgeSystem implements EdgeSystem, GoodCitizen {
 
     public String getProperty(String key) {
         return System.getProperty(key);
