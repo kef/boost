6ff8974d7a1740e255500b56d2fce557592b1cf5
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-07-02 01:44:54 +0000

    Story: New interceptor ("pebble"). [id=1665]
    git-svn-id: https://geekscape.org/svn/boost/trunk@2301 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/util/exception/DefaultThrowableMaster.java
+++ src/core/java/au/net/netstorm/boost/util/exception/DefaultThrowableMaster.java
@@ -1,6 +1,15 @@
 package au.net.netstorm.boost.util.exception;
 
 public final class DefaultThrowableMaster implements ThrowableMaster {
+
+/*
+    public void rethrow(Throwable t) {
+        if (t instanceof RuntimeException) throw (RuntimeException) t;
+        if (t instanceof Error) throw (Error) t;
+        throw new UndeclaredThrowableException(t);
+    }
+*/
+
     public boolean isChecked(Throwable t) {
         if (t instanceof RuntimeException) return false;
         if (t instanceof Error) return false;
