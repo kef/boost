d0056c3ae1697692cf02503c9d84f40a0f57d457
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-07-10 00:56:28 +0000

    Dummy Stuff 2076
    git-svn-id: https://geekscape.org/svn/boost/trunk@2343 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/test/specific/SpecificProviderRegistry.java
+++ src/test/java/au/net/netstorm/boost/test/specific/SpecificProviderRegistry.java
@@ -3,6 +3,8 @@ package au.net.netstorm.boost.test.specific;
 import au.net.netstorm.boost.spider.core.Provider;
 
 // FIX 2076 Split this interface.
+
+// FIX 2076 Rename to Specifics.
 public interface SpecificProviderRegistry extends Provider {
 
     boolean canProvide(Class type);
