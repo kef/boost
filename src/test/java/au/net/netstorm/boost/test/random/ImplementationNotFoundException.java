14341fd67c43874dba889a710bdff709e8a79475
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-08-01 01:42:18 +0000

    Story:  STATE - Complete mock/dummy support. [id=61705]
    git-svn-id: https://geekscape.org/svn/boost/trunk@2380 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/test/random/ImplementationNotFoundException.java
+++ src/test/java/au/net/netstorm/boost/test/random/ImplementationNotFoundException.java
@@ -1,18 +1,13 @@
 package au.net.netstorm.boost.test.random;
 
 import au.net.netstorm.boost.provider.ProviderException;
-import au.net.netstorm.boost.reflect.ClassMaster;
-import au.net.netstorm.boost.reflect.DefaultClassMaster;
+import au.net.netstorm.boost.test.specific.DataProvider;
 import au.net.netstorm.boost.util.type.Interface;
 
 public final class ImplementationNotFoundException extends ProviderException {
-    private static final ClassMaster CLASSER = new DefaultClassMaster();
+    private static final String DATA_PROVIDER = DataProvider.class.getName();
 
     public ImplementationNotFoundException(Interface type) {
-        super("No implementation found for '" + type.getType() + "'. You may need to register a " + shortName(type));
-    }
-
-    private static String shortName(Interface type) {
-        return CLASSER.getShortName(type);
+        super("No implementation found for '" + type.getType() + "'. You may need to register a " + DATA_PROVIDER);
     }
 }
