3d765baa13ca621d6e64680533a158af5024c0b4
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-08-03 03:27:14 +0000

    Story:  Implement Onion. [id=1887]
    git-svn-id: https://geekscape.org/svn/boost/trunk@2411 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/nursery/spider/onion/core/OnionHandler.java
+++ src/core/java/au/net/netstorm/boost/nursery/spider/onion/core/OnionHandler.java
@@ -4,6 +4,7 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 
 public final class OnionHandler implements InvocationHandler {
+    //    private final EdgeClass classer = new DefaultEdgeClass();
     private final Object real;
 
     public OnionHandler(Object real) {
@@ -12,6 +13,41 @@ public final class OnionHandler implements InvocationHandler {
 
     // FIX 1887 Complete this.
     public Object invoke(Object ref, Method method, Object[] params) throws Throwable {
+        method.setAccessible(true);
         return method.invoke(real, params);
     }
+
+    // FIX 1887 Remove.
+/*
+    // FIX BREADCRUMB 1887 This would be a good cache point.
+    private Method realMethod(Method ifaceMethod, Object[] params) {
+        Class cls = real.getClass();
+        Method[] methods = cls.getMethods();
+        for (int i = 0; i < methods.length; i++) {
+            Method method = methods[i];
+            if (matches(method, ifaceMethod)) return method;
+        }
+        throw new IllegalStateException("No matching method " + ifaceMethod + " on " + cls);
+    }
+
+    private boolean matches(Method method, Method ifaceMethod) {
+        if (!nameMatches(method, ifaceMethod)) return false;
+        Class[] sig = method.getParameterTypes();
+        Class[] ifaceSig = ifaceMethod.getParameterTypes();
+        if (sig.length != ifaceSig.length) return false;
+        for (int i = 0; i < sig.length; i++) {
+            Class type = sig[i];
+            Class ifaceType = ifaceSig[i];
+            if (!ifaceType.isAssignableFrom(type)) return false;
+        }
+        return true;
+    }
+
+    private boolean nameMatches(Method method, Method ifaceMethod) {
+        String name = method.getName();
+        String ifaceName = ifaceMethod.getName();
+        if (!name.equals(ifaceName)) return false;
+        return true;
+    }
+*/
 }
