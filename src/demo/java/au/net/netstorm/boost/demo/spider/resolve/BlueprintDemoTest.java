63444f1c682f4c50dc6a0cfc4cbba2bbcd90b62b
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-08-11 09:52:59 +0000

    Story:  Enable specification of single instances in spider registry. [id=2081]
    git-svn-id: https://geekscape.org/svn/boost/trunk@2457 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/demo/java/au/net/netstorm/boost/demo/spider/resolve/BlueprintDemoTest.java
+++ src/demo/java/au/net/netstorm/boost/demo/spider/resolve/BlueprintDemoTest.java
@@ -7,23 +7,31 @@ public final class BlueprintDemoTest extends ResolverDemooooTest {
         registry.single(FlyingDoctor.class, FnqFlyingDoctor.class);
     }
 
-    // FIX 2081 Refactor.
     public void testSingle() {
         Town t1 = resolveTown();
         Town t2 = resolveTown();
-        assertEquals(true, t1 != t2);
+        checkDifferent(t1, t2);
         FlyingDoctor d1 = getDoctor(t1);
         FlyingDoctor d2 = getDoctor(t2);
-        assertNotNull(d1);
-        // FIX 2081 Reinstate as acceptance test.
-        assertEquals(true, d1 == d2);
+        checkSame(d1, d2);
+    }
+
+    private Town resolveTown() {
+        return (Town) resolver.resolve(Town.class);
     }
 
     private FlyingDoctor getDoctor(Town t1) {
         return (FlyingDoctor) grapher.get(t1, "doctor");
     }
 
-    private Town resolveTown() {
-        return (Town) resolver.resolve(Town.class);
+    private void checkSame(Object o1, Object o2) {
+        assertNotNull(o1);
+        assertEquals(true, o1 == o2);
+    }
+
+    private void checkDifferent(Object o1, Object o2) {
+        assertNotNull(o1);
+        assertNotNull(o2);
+        assertEquals(true, o1 != o2);
     }
 }
