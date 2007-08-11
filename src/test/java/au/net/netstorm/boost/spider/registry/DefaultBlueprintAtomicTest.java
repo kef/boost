41d91e70be7d33cc3c1c2627fceb36b2411e69ca
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-08-11 10:37:02 +0000

    Story:  Enable specification of single instances in spider registry. [id=2081]
    git-svn-id: https://geekscape.org/svn/boost/trunk@2463 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/spider/registry/DefaultBlueprintAtomicTest.java
+++ src/test/java/au/net/netstorm/boost/spider/registry/DefaultBlueprintAtomicTest.java
@@ -7,14 +7,13 @@ import au.net.netstorm.boost.util.introspect.DefaultFieldSpec;
 import au.net.netstorm.boost.util.introspect.FieldSpec;
 import au.net.netstorm.boost.util.type.Implementation;
 
-// FIX 2081 Use or lose.
 public final class DefaultBlueprintAtomicTest extends InteractionTestCase {
     AtomTestChecker checker = new DataAtomTestChecker(random);
     FieldSpec f1 = new DefaultFieldSpec("stamp", Stamp.class);
     FieldSpec f2 = new DefaultFieldSpec("implementation", Implementation.class);
     FieldSpec[] fields = {f1, f2};
 
-    public void testXxx() {
+    public void testAtom() {
         checker.checkAtom(DefaultBlueprint.class, fields);
     }
 }
