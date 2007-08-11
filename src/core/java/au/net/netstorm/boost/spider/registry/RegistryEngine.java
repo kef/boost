a5760a11e42063206f8f462112eabeccbc865073
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-08-11 10:21:04 +0000

    Story:  Enable specification of single instances in spider registry. [id=2081]
    git-svn-id: https://geekscape.org/svn/boost/trunk@2460 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/spider/registry/RegistryEngine.java
+++
@@ -1,8 +0,0 @@
-package au.net.netstorm.boost.spider.registry;
-
-// FIX 2081 Add instance(Interface iface, Implementation implementation, Flavour flavour).
-
-// FIX 2081 Delete?  Use or lose.
-public interface RegistryEngine {
-
-}
