8e1f3e96a471ef895226097e4bfec44199a4017a
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-08-12 08:12:32 +0000

    Story:  Enable specification of single instances in spider registry. [id=2081]
    git-svn-id: https://geekscape.org/svn/boost/trunk@2480 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/spider/registry/Stamp.java
+++ src/core/java/au/net/netstorm/boost/spider/registry/Stamp.java
@@ -2,8 +2,6 @@ package au.net.netstorm.boost.spider.registry;
 
 import au.net.netstorm.boost.util.type.Immutable;
 
-// FIX 2081 Make extend enumeration.
-
 // OK InterfaceIsType {
 public interface Stamp extends Immutable {
     Stamp MULTIPLE = new DefaultStamp("MULTIPLE");
