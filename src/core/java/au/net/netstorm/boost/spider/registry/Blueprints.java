550262ac5b98c121220070e7ff86830cb843d126
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-08-12 08:14:57 +0000

    Story:  Enable specification of single instances in spider registry. [id=2081]
    git-svn-id: https://geekscape.org/svn/boost/trunk@2481 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/spider/registry/Blueprints.java
+++ src/core/java/au/net/netstorm/boost/spider/registry/Blueprints.java
@@ -3,7 +3,6 @@ package au.net.netstorm.boost.spider.registry;
 import au.net.netstorm.boost.spider.flavour.Flavour;
 import au.net.netstorm.boost.util.type.Interface;
 
-// FIX 2081 Remove any unused test fixtures (ie. CruisyMole).
 public interface Blueprints {
     void put(Interface iface, Flavour flavour, Blueprint blueprint);
 
