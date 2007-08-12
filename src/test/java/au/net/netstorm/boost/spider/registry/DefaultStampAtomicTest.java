459d484974b7ee811d2402d5127aef0da4e980af
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-08-12 08:09:36 +0000

    Story:  Enable specification of single instances in spider registry. [id=2081]
    git-svn-id: https://geekscape.org/svn/boost/trunk@2477 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/spider/registry/DefaultStampAtomicTest.java
+++ src/test/java/au/net/netstorm/boost/spider/registry/DefaultStampAtomicTest.java
@@ -1,11 +1,15 @@
 package au.net.netstorm.boost.spider.registry;
 
+import au.net.netstorm.boost.primordial.Primordial;
 import au.net.netstorm.boost.test.automock.InteractionTestCase;
+import au.net.netstorm.boost.test.reflect.checker.ClassTestChecker;
+import au.net.netstorm.boost.test.reflect.checker.DefaultClassTestChecker;
 import au.net.netstorm.boost.test.reflect.util.DefaultFieldTestUtil;
 import au.net.netstorm.boost.test.reflect.util.FieldTestUtil;
 
 public final class DefaultStampAtomicTest extends InteractionTestCase {
     FieldTestUtil fielder = new DefaultFieldTestUtil();
+    ClassTestChecker classer = new DefaultClassTestChecker();
     String value;
 
     // FIX 2081 Ensure extends Primordial.
@@ -14,5 +18,9 @@ public final class DefaultStampAtomicTest extends InteractionTestCase {
         Stamp stamp = new DefaultStamp(value);
         Object actual = fielder.getInstance(stamp, "value");
         assertEquals(value, actual);
+    }
+
+    public void testCharacteristics() {
+        classer.checkSubclassOf(DefaultStamp.class, Primordial.class);
     }
 }
