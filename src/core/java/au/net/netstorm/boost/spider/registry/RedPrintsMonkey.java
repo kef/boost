adb4604399eacccad507276390ac9eb6a11dd1fb
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-08-14 22:54:20 +0000

    Story: New interceptor ("pebble"). [id=1665]
    git-svn-id: https://geekscape.org/svn/boost/trunk@2496 05db6a09-9903-0410-bedf-ba9bc4063e48

---
+++ src/core/java/au/net/netstorm/boost/spider/registry/RedPrintsMonkey.java
@@ -0,0 +1,9 @@
+package au.net.netstorm.boost.spider.registry;
+
+import au.net.netstorm.boost.spider.flavour.Flavour;
+import au.net.netstorm.boost.util.type.Interface;
+
+// FIX 1914 Lowercase the "P".
+public interface RedPrintsMonkey {
+    void put(Interface iface, Flavour flavour, Blueprint blueprint);
+}
