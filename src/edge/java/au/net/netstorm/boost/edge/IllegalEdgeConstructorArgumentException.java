a9bb2aaf73628c76c21399ec7902aaef151e899f
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-08-19 01:02:12 +0000

    Added more information to edgeContructor exceptions.
    git-svn-id: https://geekscape.org/svn/boost/trunk@2514 05db6a09-9903-0410-bedf-ba9bc4063e48

---
+++ src/edge/java/au/net/netstorm/boost/edge/IllegalEdgeConstructorArgumentException.java
@@ -0,0 +1,8 @@
+package au.net.netstorm.boost.edge;
+
+public final class IllegalEdgeConstructorArgumentException extends IllegalArgumentException {
+
+    public IllegalEdgeConstructorArgumentException(String string, Throwable throwable) {
+        super(string, throwable);
+    }
+}
--- src/edge/java/au/net/netstorm/boost/edge/java/lang/reflect/DefaultEdgeConstructor.java
+++ src/edge/java/au/net/netstorm/boost/edge/java/lang/reflect/DefaultEdgeConstructor.java
@@ -1,19 +1,31 @@
 package au.net.netstorm.boost.edge.java.lang.reflect;
 
 import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 import au.net.netstorm.boost.edge.EdgeException;
+import au.net.netstorm.boost.edge.IllegalEdgeConstructorArgumentException;
 
 public final class DefaultEdgeConstructor implements EdgeConstructor {
+    private final String newLine = System.getProperty("line.separator");
+
     public Object newInstance(Constructor constructor, Object[] initArgs) {
         try {
             return constructor.newInstance(initArgs);
-        } catch (InstantiationException e) {
-            throw new EdgeException(e);
-        } catch (IllegalAccessException e) {
-            throw new EdgeException(e);
-        } catch (InvocationTargetException e) {
-            throw new EdgeException(e);
+        } catch (Exception e) {
+            String errorString = createErrorString(constructor, initArgs);
+            if (e instanceof IllegalArgumentException)
+                throw new IllegalEdgeConstructorArgumentException(errorString, e);
+            throw new EdgeException(errorString, e);
         }
     }
+
+    private String createErrorString(Constructor constructor, Object[] initArgs) {
+        String errorString = constructor + newLine;
+        if (initArgs != null) {
+            for (int i = 0; i < initArgs.length; i++) {
+                errorString += initArgs[i] + newLine;
+            }
+        }
+        return errorString;
+    }
 }
+
