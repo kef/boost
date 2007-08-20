eaad646fa8231cf9599ba4dc048f587c19147640
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-08-20 01:12:15 +0000

    8888
    git-svn-id: https://geekscape.org/svn/boost/trunk@2529 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/nursery/type/AsciiBytes.java
+++ src/core/java/au/net/netstorm/boost/nursery/type/AsciiBytes.java
@@ -2,6 +2,7 @@ package au.net.netstorm.boost.nursery.type;
 
 import au.net.netstorm.boost.util.type.Data;
 
+// FIX BREADCRUMB 8888 This doesn't really belong here. 
 public interface AsciiBytes extends Data {
     byte[] getValue();
 }
