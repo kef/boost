06b40c5312248094870d827edcb88785fcff50bd
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-08-20 01:11:31 +0000

    Story: New interceptor ("pebble"). [id=1665]
    git-svn-id: https://geekscape.org/svn/boost/trunk@2528 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/nursery/time/StartEnd.java
+++ src/core/java/au/net/netstorm/boost/nursery/time/StartEnd.java
@@ -3,4 +3,7 @@ package au.net.netstorm.boost.nursery.time;
 import au.net.netstorm.boost.util.type.Data;
 
 public interface StartEnd extends Data {
+    TimeSpec getStart();
+
+    TimeSpec getEnd();
 }
