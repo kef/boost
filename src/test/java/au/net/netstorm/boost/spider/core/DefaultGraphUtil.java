0982854850bf3a4ea1f787e4e9b1ac68e85e4e3e
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-08-21 03:58:09 +0000

    SSD.
    git-svn-id: https://geekscape.org/svn/boost/trunk@2537 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/spider/core/DefaultGraphUtil.java
+++ src/test/java/au/net/netstorm/boost/spider/core/DefaultGraphUtil.java
@@ -17,6 +17,18 @@ public final class DefaultGraphUtil implements GraphUtil {
         return peeler.peel(o);
     }
 
+    public void set(Object ref, String chain, Object instance) {
+        String[] links = toLinks(chain);
+        set(ref, links, instance, 0);
+    }
+
+    private void set(Object ref, String[] links, Object instance, int i) {
+        if (i == links.length) return;
+        Object value = field(ref, links[i]);
+        if (i != links.length - 1) set(value, links, instance, i + 1);
+        fielder.setInstance(ref, links[i], instance);
+    }
+
     private Object get(Object ref, String[] links, int i) {
         if (i == links.length) return ref;
         Object field = field(ref, links[i]);
--- src/test/java/au/net/netstorm/boost/spider/core/GraphUtil.java
+++ src/test/java/au/net/netstorm/boost/spider/core/GraphUtil.java
@@ -2,4 +2,6 @@ package au.net.netstorm.boost.spider.core;
 
 public interface GraphUtil {
     Object get(Object ref, String chain);
+
+    void set(Object ref, String chain, Object instance);
 }
