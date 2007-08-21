eb7a75b813ac8cdfc298e5a47ec275d0eaf8f350
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-08-21 07:02:25 +0000

    8888
    git-svn-id: https://geekscape.org/svn/boost/trunk@2542 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/nursery/string/StringConverter.java
+++
@@ -1,7 +0,0 @@
-package au.net.netstorm.boost.nursery.string;
-
-public interface StringConverter {
-    String convert(byte[] bytes, String charSet);
-
-    byte[] convert(String str, String charSet);
-}
