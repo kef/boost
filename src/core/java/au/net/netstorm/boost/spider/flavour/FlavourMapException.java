0d5944c2d90808ebe215b6d473870ce344d2b0a9
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-08-23 08:41:31 +0000

    Story: New interceptor ("pebble"). [id=1665]
    git-svn-id: https://geekscape.org/svn/boost/trunk@2549 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/spider/flavour/FlavourMapException.java
+++ src/core/java/au/net/netstorm/boost/spider/flavour/FlavourMapException.java
@@ -5,13 +5,15 @@ import au.net.netstorm.boost.demo.spider.newer.ResolvedThings;
 import au.net.netstorm.boost.primordial.BoooostException;
 
 public final class FlavourMapException extends RuntimeException implements BoooostException {
-    private final ResolvedThings things = new DefaultResolvedThings();
     private final FlavouredInterface flavour;
     private final String reason;
+    private final String things;
 
     public FlavourMapException(FlavouredInterface flavour, String reason) {
         this.flavour = flavour;
         this.reason = reason;
+        ResolvedThings things = new DefaultResolvedThings();
+        this.things = "" + things;
     }
 
     public String getMessage() {
