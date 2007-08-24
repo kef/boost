985d3fbac2644645c08eb32542d26c13915e523a
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-08-24 07:00:51 +0000

    Story: New interceptor ("pebble"). [id=1665]
    git-svn-id: https://geekscape.org/svn/boost/trunk@2551 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/spider/registry/Stamp.java
+++ src/core/java/au/net/netstorm/boost/spider/registry/Stamp.java
@@ -4,6 +4,7 @@ import au.net.netstorm.boost.util.type.Immutable;
 
 // OK InterfaceIsType {
 public interface Stamp extends Immutable {
+    // FIX 1914 Multiples go!!!!!!!!!!!!!
     Stamp MULTIPLE = new DefaultStamp("MULTIPLE");
     Stamp SINGLE = new DefaultStamp("SINGLE");
 } // } OK InterfaceIsType
