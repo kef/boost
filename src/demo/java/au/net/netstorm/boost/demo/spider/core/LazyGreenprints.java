5e57278a01463e42c5c0be2b209bdcbfd3178297
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-08-28 04:12:09 +0000

    Story:  Enable specification of single instances in spider registry. [id=2081]
    git-svn-id: https://geekscape.org/svn/boost/trunk@2558 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/demo/java/au/net/netstorm/boost/demo/spider/core/LazyGreenprints.java
+++ src/demo/java/au/net/netstorm/boost/demo/spider/core/LazyGreenprints.java
@@ -1,58 +1,30 @@
 package au.net.netstorm.boost.demo.spider.core;
 
-import au.net.netstorm.boost.edge.EdgeException;
-import au.net.netstorm.boost.edge.java.lang.DefaultEdgeClass;
-import au.net.netstorm.boost.edge.java.lang.EdgeClass;
-import au.net.netstorm.boost.reflect.ClassMaster;
-import au.net.netstorm.boost.reflect.DefaultClassMaster;
 import au.net.netstorm.boost.spider.flavour.Flavour;
 import au.net.netstorm.boost.spider.registry.Blueprint;
 import au.net.netstorm.boost.spider.registry.DefaultBlueprint;
 import au.net.netstorm.boost.spider.registry.Greenprints;
 import au.net.netstorm.boost.spider.registry.Stamp;
-import au.net.netstorm.boost.util.type.DefaultImplementation;
+import au.net.netstorm.boost.util.impl.DefaultImplMaster;
+import au.net.netstorm.boost.util.impl.ImplMaster;
 import au.net.netstorm.boost.util.type.Implementation;
 import au.net.netstorm.boost.util.type.Interface;
 
 public final class LazyGreenprints implements Greenprints {
     private static final Stamp SINGLE = Stamp.SINGLE;
     private static final Flavour UNFLAVOURED = Flavour.UNFLAVOURED;
-    private final EdgeClass classer = new DefaultEdgeClass();
-    private final ClassMaster master = new DefaultClassMaster();
+    private final ImplMaster impler = new DefaultImplMaster();
 
     public Blueprint get(Interface iface, Flavour flavour) {
-        String implName = impl(iface);
-        return blueprint(implName);
+        Implementation impl = impler.defaultImpl(iface);
+        return blueprint(impl);
     }
 
     public boolean exists(Interface iface, Flavour flavour) {
-        String impl = impl(iface);
-        return exists(impl);
+        return impler.hasDefaultImpl(iface);
     }
 
-    private String impl(Interface iface) {
-        String name = master.getShortName(iface);
-        String pkg = master.getPackageName(iface);
-        return pkg + "." + "Default" + name;
-    }
-
-    private Class get(String impl) {
-        try {
-            return classer.forName(impl);
-        } catch (EdgeException e) {
-            if (e.causeIs(ClassNotFoundException.class)) return null;
-            throw e;
-        }
-    }
-
-    private boolean exists(String impl) {
-        Class result = get(impl);
-        return result != null;
-    }
-
-    private Blueprint blueprint(String implName) {
-        Class cls = get(implName);
-        Implementation impl = new DefaultImplementation(cls);
+    private Blueprint blueprint(Implementation impl) {
         return new DefaultBlueprint(SINGLE, impl, UNFLAVOURED);
     }
 }
