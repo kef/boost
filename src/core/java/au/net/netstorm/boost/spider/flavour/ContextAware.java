91680bbe5c5f0bc89d12bfc67532e8246f056dbf
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-09-20 22:56:27 +0000

    1887
    git-svn-id: https://geekscape.org/svn/boost/trunk@2629 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/spider/flavour/ContextAware.java
+++
@@ -1,8 +0,0 @@
-package au.net.netstorm.boost.spider.flavour;
-
-import au.net.netstorm.boost.util.type.Implementation;
-
-public interface ContextAware {
-    // FIX BREADCRUMB 73022 Better name?
-    void setContext(Implementation impl);
-}
