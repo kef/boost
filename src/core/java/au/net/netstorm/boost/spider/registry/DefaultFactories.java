3502ff6573f26f57013432f89726f3d42859ea3b
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-09-20 23:37:49 +0000

    Story:  Incorporate "factories" into spider. [id=2145]
    git-svn-id: https://geekscape.org/svn/boost/trunk@2635 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/spider/registry/DefaultFactories.java
+++ src/core/java/au/net/netstorm/boost/spider/registry/DefaultFactories.java
@@ -1,7 +1,6 @@
 package au.net.netstorm.boost.spider.registry;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import au.net.netstorm.boost.spider.core.ProviderEngine;
 import au.net.netstorm.boost.util.type.Interface;
@@ -11,17 +10,16 @@ public final class DefaultFactories implements Factories {
     private final List factories = new ArrayList();
 
     public ResolvedInstance get(Interface iface, ImplementationRef host, ProviderEngine provider, Instances instances) {
-        // FIX 2145 Iterator be gone.
-        for (Iterator itr = factories.iterator(); itr.hasNext();) {
-            Factory factory = (Factory) itr.next();
+        int size = factories.size();
+        for (int i = size - 1; i >= 0; i--) {
+            Factory factory = (Factory) factories.get(i);
             boolean canHandle = factory.canHandle(iface);
             if (canHandle) return factory.get(iface, host, provider, instances);
         }
         throw new CannotProvideException(iface);
     }
 
-    // FIX 2145 Does this HAVE to be 0.
     public void add(Factory factory) {
-        factories.add(0, factory);
+        factories.add(factory);
     }
 }
