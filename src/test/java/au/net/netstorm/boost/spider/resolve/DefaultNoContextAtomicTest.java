322b31b80ab4e94d92d330a33992acfbe7d2cf7f
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-09-26 04:57:26 +0000

    Story:  Incorporate "factories" into spider. [id=2145]
    git-svn-id: https://geekscape.org/svn/boost/trunk@2642 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/spider/resolve/DefaultNoContextAtomicTest.java
+++ src/test/java/au/net/netstorm/boost/spider/resolve/DefaultNoContextAtomicTest.java
@@ -4,7 +4,6 @@ import au.net.netstorm.boost.test.automock.InteractionTestCase;
 
 public final class DefaultNoContextAtomicTest extends InteractionTestCase {
 
-    // FIX 74285 Is this what we want to do to test this bad boy?
     public void testNoContext() {
         NoContext actual = new NoContext();
         assertNotNull(actual);
