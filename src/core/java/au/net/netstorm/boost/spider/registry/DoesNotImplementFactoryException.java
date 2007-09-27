370ac56b80905147a5cb0f1599452187c5a98648
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-09-27 00:27:51 +0000

    Story:  Incorporate "factories" into spider. [id=2145]
    git-svn-id: https://geekscape.org/svn/boost/trunk@2650 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/spider/registry/DoesNotImplementFactoryException.java
+++ src/core/java/au/net/netstorm/boost/spider/registry/DoesNotImplementFactoryException.java
@@ -3,7 +3,6 @@ package au.net.netstorm.boost.spider.registry;
 import au.net.netstorm.boost.primordial.PrimordialException;
 import au.net.netstorm.boost.util.type.Interface;
 
-// FIX 2145 This class looks exactly like the newer version of this exception.
 public final class DoesNotImplementFactoryException extends PrimordialException {
     public DoesNotImplementFactoryException(Class cls, Interface marker) {
         super(cls + " should implement " + marker);
