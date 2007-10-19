0e56735ad6ea87a958a98ca155569082240b65e4
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-10-19 03:07:26 +0000

    Story:  SERVER PERFORMANCE - Implement multi-threaded testing framework. [id=78464]
    git-svn-id: https://geekscape.org/svn/boost/trunk@2720 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/test/timing/DefaultTimingSupport.java
+++ src/test/java/au/net/netstorm/boost/test/timing/DefaultTimingSupport.java
@@ -4,5 +4,12 @@ package au.net.netstorm.boost.test.timing;
 public class DefaultTimingSupport implements TimingSupport {
     public void time(Class cls, String method, long start, long end) {
         // FIX 2000 Complete me.
+        // FIX 2000 Delete the buffer stuff below...
+        StringBuffer buffer = new StringBuffer();
+        buffer.append(" cls: " + cls);
+        buffer.append(" method: " + method);
+        buffer.append(" start: " + start);
+        buffer.append(" end: " + end);
+        buffer.append(" duration: " + (end - start));
     }
 }
