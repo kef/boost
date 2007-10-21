0a9cc5cc67a9ab45ba3949176876ed3b3f9f6778
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-10-21 23:54:10 +0000

    Story:  SERVER PERFORMANCE - Implement multi-threaded testing framework. [id=78464]
    git-svn-id: https://geekscape.org/svn/boost/trunk@2733 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/test/parallel/BasicRunner.java
+++
@@ -1,8 +0,0 @@
-package au.net.netstorm.boost.test.parallel;
-
-import au.net.netstorm.boost.test.lifecycle.LifecycleTest;
-import au.net.netstorm.boost.test.lifecycle.TestLifecycle;
-
-public interface BasicRunner {
-    void run(LifecycleTest test, TestLifecycle lifecycle) throws Throwable;
-}
