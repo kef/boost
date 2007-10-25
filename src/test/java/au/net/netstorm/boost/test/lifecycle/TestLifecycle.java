df8d3658ca757f60a11755d12bde48bfd54b64ef
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-10-25 01:30:48 +0000

    multi-threaded testing framework.
    git-svn-id: https://geekscape.org/svn/boost/trunk@2755 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/test/lifecycle/TestLifecycle.java
+++ src/test/java/au/net/netstorm/boost/test/lifecycle/TestLifecycle.java
@@ -1,8 +1,5 @@
 package au.net.netstorm.boost.test.lifecycle;
 
-// FIX 2000 Split into two interfaces.
-
-// FIX 2000 More than that it looks like one interface used twice.
 public interface TestLifecycle {
     void pre();
 
