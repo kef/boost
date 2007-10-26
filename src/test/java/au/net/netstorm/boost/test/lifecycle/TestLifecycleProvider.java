306d1327fd90de3ba289b49265f106c3d47bad3f
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-10-26 06:07:38 +0000

    Story:  Add parallelisation to testing. [id=2180]
    git-svn-id: https://geekscape.org/svn/boost/trunk@2772 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/test/lifecycle/TestLifecycleProvider.java
+++ src/test/java/au/net/netstorm/boost/test/lifecycle/TestLifecycleProvider.java
@@ -1,6 +1,7 @@
 package au.net.netstorm.boost.test.lifecycle;
 
 public interface TestLifecycleProvider {
+    // FIX 2180 Rename to methodTest...
     MethodTestLifecycle classTestLifecycle();
 
     ThreadTestLifecycle threadTestLifecycle();
