f5c6612a63b00fdb6695b73ba6b92d86e35d7c62
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-11-06 08:13:24 +0000

    Story:  Remove duplication in spider greenprints/factories. [id=2215]
    git-svn-id: https://geekscape.org/svn/boost/trunk@2824 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/spider/registry/StampedResolvedInstance.java
+++ src/core/java/au/net/netstorm/boost/spider/registry/StampedResolvedInstance.java
@@ -3,6 +3,9 @@ package au.net.netstorm.boost.spider.registry;
 import au.net.netstorm.boost.util.type.Data;
 import au.net.netstorm.boost.util.type.ResolvedInstance;
 
+// FIX 2215 Get rid of this transitional item.
+
+// FIX 2215 Either ResolvedInstance gets a Stamp, or we go a completely different direction in Factories.
 public interface StampedResolvedInstance extends Data {
     ResolvedInstance getInstance();
 
