068d6fdd57a04e5958720b397b69f7b45f7f2938
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-11-26 08:06:37 +0000

    Remove integrated concurrency support in tests.
    git-svn-id: https://geekscape.org/svn/boost/trunk@2868 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/test/parallel/DefaultTestThreadEngine.java
+++ src/test/java/au/net/netstorm/boost/test/parallel/DefaultTestThreadEngine.java
@@ -1,14 +1,11 @@
 package au.net.netstorm.boost.test.parallel;
 
-import au.net.netstorm.boost.edge.java.lang.DefaultEdgeClass;
 import au.net.netstorm.boost.edge.java.lang.DefaultEdgeThread;
-import au.net.netstorm.boost.edge.java.lang.EdgeClass;
 import au.net.netstorm.boost.edge.java.lang.EdgeThread;
 import au.net.netstorm.boost.test.lifecycle.LifecycleTest;
 
 // FIX (Nov 26, 2007) TESTING 83271 Delete this.
 public class DefaultTestThreadEngine implements TestThreadEngine {
-    private final EdgeClass classer = new DefaultEdgeClass();
     private final EdgeThread threader = new DefaultEdgeThread();
 
     public void start(LifecycleTest test) {
@@ -17,17 +14,8 @@ public class DefaultTestThreadEngine implements TestThreadEngine {
         threader.join(thread);
     }
 
-    private Thread createThread(LifecycleTest prototype) {
-        LifecycleTest test = clone(prototype);
+    private Thread createThread(LifecycleTest test) {
         Runnable runnable = new DefaultRunnableTest(test);
         return new Thread(runnable);
-    }
-
-    private LifecycleTest clone(LifecycleTest prototype) {
-        Class cls = prototype.getClass();
-        LifecycleTest test = (LifecycleTest) classer.newInstance(cls);
-        String name = prototype.getName();
-        test.setName(name);
-        return test;
     }
 }
