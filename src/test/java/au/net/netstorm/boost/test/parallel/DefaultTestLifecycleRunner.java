05fae350899c5240e792d5a873226cfa1a305e6c
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-11-26 08:22:09 +0000

    Remove integrated concurrency support in tests.
    git-svn-id: https://geekscape.org/svn/boost/trunk@2872 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/test/parallel/DefaultTestLifecycleRunner.java
+++ src/test/java/au/net/netstorm/boost/test/parallel/DefaultTestLifecycleRunner.java
@@ -1,28 +1,13 @@
 package au.net.netstorm.boost.test.parallel;
 
-import au.net.netstorm.boost.test.automock.NothingTestUberLifecycle;
 import au.net.netstorm.boost.test.lifecycle.LifecycleTest;
-import au.net.netstorm.boost.test.lifecycle.TestUberLifecycle;
 
 public class DefaultTestLifecycleRunner implements TestLifecycleRunner {
     private final TestExceptionHandler handler = new DefaultTestExceptionHandler();
 
     public void run(LifecycleTest test) throws Throwable {
-        TestUberLifecycle lifecycle = new NothingTestUberLifecycle();
-        boolean successful = false;
-        try {
-            doExecute(test, lifecycle);
-            handler.checkExceptions();
-            successful = true;
-        } finally {
-            lifecycle.cleanup(successful);
-        }
-    }
-
-    private void doExecute(LifecycleTest test, TestUberLifecycle lifecycle) {
-        lifecycle.pre();
         Runnable runnable = new DefaultRunnableTest(test);
         runnable.run();
-        lifecycle.post();
+        handler.checkExceptions();
     }
 }
