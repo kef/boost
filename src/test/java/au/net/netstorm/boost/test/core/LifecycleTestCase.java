4c231162b2334275f82715d2f3514fa569ec4aaa
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-11-27 00:44:47 +0000

    Story:  Remove parallelisation from testing. [id=2233]
    git-svn-id: https://geekscape.org/svn/boost/trunk@2883 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/test/core/LifecycleTestCase.java
+++ src/test/java/au/net/netstorm/boost/test/core/LifecycleTestCase.java
@@ -2,9 +2,9 @@ package au.net.netstorm.boost.test.core;
 
 import au.net.netstorm.boost.test.exception.DefaultThrowableSupport;
 import au.net.netstorm.boost.test.exception.ThrowableSupport;
+import au.net.netstorm.boost.test.lifecycle.DefaultTestLifecycleRunner;
 import au.net.netstorm.boost.test.lifecycle.LifecycleTest;
-import au.net.netstorm.boost.test.parallel.DefaultTestLifecycleRunner;
-import au.net.netstorm.boost.test.parallel.TestLifecycleRunner;
+import au.net.netstorm.boost.test.lifecycle.TestLifecycleRunner;
 
 public abstract class LifecycleTestCase extends CleanTestCase implements LifecycleTest {
     private final TestLifecycleRunner runner = new DefaultTestLifecycleRunner();
---
+++ src/test/java/au/net/netstorm/boost/test/lifecycle/TestLifecycleRunner.java
@@ -0,0 +1,5 @@
+package au.net.netstorm.boost.test.lifecycle;
+
+public interface TestLifecycleRunner {
+    void run(LifecycleTest test) throws Throwable;
+}
