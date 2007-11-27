9dee829840993803a727dff85cb382a64cbbb35e
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-11-27 00:51:36 +0000

    Story:  Remove parallelisation from testing. [id=2233]
    git-svn-id: https://geekscape.org/svn/boost/trunk@2884 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/test/parallel/TestLifecycleRunner.java
+++
@@ -1,7 +0,0 @@
-package au.net.netstorm.boost.test.parallel;
-
-import au.net.netstorm.boost.test.lifecycle.LifecycleTest;
-
-public interface TestLifecycleRunner {
-    void run(LifecycleTest test) throws Throwable;
-}
