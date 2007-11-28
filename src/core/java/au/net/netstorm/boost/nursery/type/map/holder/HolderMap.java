10d33a33667c005315bf910abb590370717995e5
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-11-28 04:52:41 +0000

    Story:  Remove parallelisation from testing. [id=2233]
    git-svn-id: https://geekscape.org/svn/boost/trunk@2896 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/nursery/type/map/holder/HolderMap.java
+++ src/core/java/au/net/netstorm/boost/nursery/type/map/holder/HolderMap.java
@@ -3,5 +3,5 @@ package au.net.netstorm.boost.nursery.type.map.holder;
 // FIX (Nov 27, 2007) 2233 Put Data atom checks in to disallow primitives (except maybe byte[]).
 
 // FIX (Nov 27, 2007) 2233 Move into common infrastructure.
-public interface HolderMap extends HolderMapRead, au.net.netstorm.boost.nursery.type.map.holder.HolderMapWrite {
+public interface HolderMap extends HolderMapRead, HolderMapWrite {
 }
