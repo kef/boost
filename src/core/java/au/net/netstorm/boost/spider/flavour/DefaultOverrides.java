32bf0befa32c30297b10019038fd79d051c35102
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-11-29 06:15:11 +0000

    Story:  Remove parallelisation from testing. [id=2233]
    git-svn-id: https://geekscape.org/svn/boost/trunk@2912 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/spider/flavour/DefaultOverrides.java
+++ src/core/java/au/net/netstorm/boost/spider/flavour/DefaultOverrides.java
@@ -4,7 +4,6 @@ import au.net.netstorm.boost.edge.java.lang.DefaultEdgeSystem;
 import au.net.netstorm.boost.edge.java.lang.EdgeSystem;
 
 public class DefaultOverrides implements Overrides {
-    private static final String ALLOW_OVERRIDES = "spider.allow.overrides";
     private final EdgeSystem system = new DefaultEdgeSystem();
 
     public boolean allowed() {
--- src/core/java/au/net/netstorm/boost/spider/flavour/Overrides.java
+++ src/core/java/au/net/netstorm/boost/spider/flavour/Overrides.java
@@ -1,5 +1,7 @@
 package au.net.netstorm.boost.spider.flavour;
 
 public interface Overrides {
+    String ALLOW_OVERRIDES = "spider.allow.overrides";
+
     boolean allowed();
 }
