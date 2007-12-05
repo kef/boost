cab153864d4416fe40aa182e142c0b147d8b7623
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-12-05 07:40:00 +0000

    Story:  CORE SPLIT - Juggernaut. [id=87471]
    git-svn-id: https://geekscape.org/svn/boost/trunk@2949 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/test/suite/collector/AllTestSuite.java
+++ src/test/java/au/net/netstorm/boost/test/suite/collector/AllTestSuite.java
@@ -1,10 +1,10 @@
 package au.net.netstorm.boost.test.suite.collector;
 
-import java.io.File;
 import au.net.netstorm.boost.test.core.BoooostCase;
 import junit.framework.Test;
 
-// FIX (Dec 5, 2007) CORE SPLIT 87471 Is there any way to clean this up?
+import java.io.File;
+
 public class AllTestSuite extends BoooostCase {
     private static final TestSuiteBuilder BUILDER = new DefaultTestSuiteBuilder("The Whole Box and Dice", AtomicTestSuite.class, DemoTestSuite.class);
 
