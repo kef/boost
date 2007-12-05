25519a0d0e5bcf3319ff05b9ec4a0760ebae04b5
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-12-05 05:15:04 +0000

    IOC - Replace newers with nu.nu()
    git-svn-id: https://geekscape.org/svn/boost/trunk@2942 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/test/automock/DefaultMockExpectations.java
+++ src/test/java/au/net/netstorm/boost/test/automock/DefaultMockExpectations.java
@@ -1,6 +1,5 @@
 package au.net.netstorm.boost.test.automock;
 
-// OK ParameterNumber|LineLength {
 public final class DefaultMockExpectations implements MockExpectations {
     private MockExpectationEngine delegate;
 
@@ -8,140 +7,21 @@ public final class DefaultMockExpectations implements MockExpectations {
         delegate = new DefaultMockExpectationEngine(mocks);
     }
 
-    public void oneCall(Object ref, Object returnValue, String methodName) {
-        Object[] parameters = {};
-        oneCall(ref, returnValue, methodName, parameters);
-    }
-
-    public void oneCall(Object ref, Object returnValue, String methodName, Object parameter1) {
-        Object[] parameters = {parameter1};
-        oneCall(ref, returnValue, methodName, parameters);
-    }
-
-    public void oneCall(Object ref, Object returnValue, String methodName, Object parameter1, Object parameter2) {
-        Object[] parameters = {parameter1, parameter2};
-        oneCall(ref, returnValue, methodName, parameters);
-    }
-
-    public void oneCall(Object ref, Object returnValue, String methodName, Object parameter1, Object parameter2, Object parameter3) {
-        Object[] parameters = {parameter1, parameter2, parameter3};
-        oneCall(ref, returnValue, methodName, parameters);
-    }
-
-    public void oneCall(Object ref, Object returnValue, String methodName, Object parameter1, Object parameter2, Object parameter3, Object parameter4) {
-        Object[] parameters = {parameter1, parameter2, parameter3, parameter4};
-        oneCall(ref, returnValue, methodName, parameters);
-    }
-
-    public void oneCall(Object ref, Object returnValue, String methodName, Object parameter1, Object parameter2, Object parameter3, Object parameter4, Object parameter5) {
-        Object[] parameters = {parameter1, parameter2, parameter3, parameter4, parameter5};
-        oneCall(ref, returnValue, methodName, parameters);
-    }
-
-    public void oneCall(Object ref, Object returnValue, String methodName, Object parameter1, Object parameter2, Object parameter3, Object parameter4, Object parameter5, Object parameter6) {
-        Object[] parameters = {parameter1, parameter2, parameter3, parameter4, parameter5, parameter6};
-        oneCall(ref, returnValue, methodName, parameters);
-    }
-
-    public void oneCall(Object ref, Object returnValue, String methodName, Object parameter1, Object parameter2, Object parameter3, Object parameter4, Object parameter5, Object parameter6, Object parameter7) {
-        Object[] parameters = {parameter1, parameter2, parameter3, parameter4, parameter5, parameter6, parameter7};
-        oneCall(ref, returnValue, methodName, parameters);
-    }
-
-    public void oneCall(Object ref, Throwable throwable, String methodName, Object parameter1, Object parameter2, Object parameter3, Object parameter4, Object parameter5) {
-        Object[] parameters = {parameter1, parameter2, parameter3, parameter4, parameter5};
-        oneCall(ref, throwable, methodName, parameters);
-    }
-
-    public void oneCall(Object ref, Throwable throwable, String methodName, Object parameter1, Object parameter2, Object parameter3, Object parameter4, Object parameter5, Object parameter6) {
-        Object[] parameters = {parameter1, parameter2, parameter3, parameter4, parameter5, parameter6};
-        oneCall(ref, throwable, methodName, parameters);
-    }
-
-    public void oneCall(Object ref, Throwable throwable, String methodName, Object parameter1, Object parameter2, Object parameter3, Object parameter4, Object parameter5, Object parameter6, Object parameter7) {
-        Object[] parameters = {parameter1, parameter2, parameter3, parameter4, parameter5, parameter6, parameter7};
-        oneCall(ref, throwable, methodName, parameters);
-    }
-
-    public void manyCalls(Object ref, Object returnValue, String methodName) {
-        Object[] parameters = {};
-        canCall(ref, returnValue, methodName, parameters);
-    }
-
-    public void manyCalls(Object ref, Object returnValue, String methodName, Object parameter1) {
-        Object[] parameters = {parameter1};
-        canCall(ref, returnValue, methodName, parameters);
-    }
-
-    public void manyCalls(Object ref, Object returnValue, String methodName, Object parameter1, Object parameter2) {
-        Object[] parameters = {parameter1, parameter2};
-        canCall(ref, returnValue, methodName, parameters);
-    }
-
-    public void manyCalls(Object ref, Object returnValue, String methodName, Object parameter1, Object parameter2, Object parameter3) {
-        Object[] parameters = {parameter1, parameter2, parameter3};
-        canCall(ref, returnValue, methodName, parameters);
-    }
-
-    public void manyCalls(Object ref, Object returnValue, String methodName, Object parameter1, Object parameter2, Object parameter3, Object parameter4) {
-        Object[] parameters = {parameter1, parameter2, parameter3, parameter4};
-        canCall(ref, returnValue, methodName, parameters);
-    }
-
-    public void manyCalls(Object ref, Object returnValue, String methodName, Object parameter1, Object parameter2, Object parameter3, Object parameter4, Object parameter5) {
-        Object[] parameters = {parameter1, parameter2, parameter3, parameter4, parameter5};
-        canCall(ref, returnValue, methodName, parameters);
-    }
-
-    public void manyCalls(Object ref, Object returnValue, String methodName, Object parameter1, Object parameter2, Object parameter3, Object parameter4, Object parameter5, Object parameter6) {
-        Object[] parameters = {parameter1, parameter2, parameter3, parameter4, parameter5, parameter6};
-        canCall(ref, returnValue, methodName, parameters);
-    }
-
-    public void manyCalls(Object ref, Object returnValue, String methodName, Object parameter1, Object parameter2, Object parameter3, Object parameter4, Object parameter5, Object parameter6, Object parameter7) {
-        Object[] parameters = {parameter1, parameter2, parameter3, parameter4, parameter5, parameter6, parameter7};
-        canCall(ref, returnValue, methodName, parameters);
-    }
-
-    public void oneCall(Object ref, Throwable throwable, String methodName) {
-        Object[] parameters = {};
-        oneCall(ref, throwable, methodName, parameters);
-    }
-
-    public void oneCall(Object ref, Throwable throwable, String methodName, Object parameter1) {
-        Object[] parameters = {parameter1};
-        oneCall(ref, throwable, methodName, parameters);
-    }
-
-    public void oneCall(Object ref, Throwable throwable, String methodName, Object parameter1, Object parameter2) {
-        Object[] parameters = {parameter1, parameter2};
-        oneCall(ref, throwable, methodName, parameters);
-    }
-
-    public void oneCall(Object ref, Throwable throwable, String methodName, Object parameter1, Object parameter2, Object parameter3) {
-        Object[] parameters = {parameter1, parameter2, parameter3};
-        oneCall(ref, throwable, methodName, parameters);
-    }
-
-    public void oneCall(Object ref, Throwable throwable, String methodName, Object parameter1, Object parameter2, Object parameter3, Object parameter4) {
-        Object[] parameters = {parameter1, parameter2, parameter3, parameter4};
-        oneCall(ref, throwable, methodName, parameters);
-    }
-
-    // Delegates...
-
-    private void oneCall(Object ref, Object returnValue, String methodName, Object[] parameters) {
+    public void oneCall(Object ref, Object returnValue, String methodName, Object... parameters) {
         MockMethodSpec spec = new DefaultMockMethodSpec(returnValue, methodName, parameters);
         delegate.oneCall(ref, spec);
     }
 
-    private void canCall(Object ref, Object returnValue, String methodName, Object[] parameters) {
+    public void oneCall(Object ref, Throwable throwable, String methodName, Object... params) {
+        delegate.oneCall(ref, throwable, methodName, params);
+    }
+
+    public void manyCalls(Object ref, Object returnValue, String methodName, Object... parameters) {
         MockMethodSpec spec = new DefaultMockMethodSpec(returnValue, methodName, parameters);
         delegate.manyCalls(ref, spec);
     }
 
-    private void oneCall(Object ref, Throwable throwable, String methodName, Object[] parameters) {
-        delegate.oneCall(ref, throwable, methodName, parameters);
+    public void nu(Object ref, Object returnValue, Class impl, Object... params) {
+        oneCall(ref, returnValue, "nu", impl, params);
     }
 }
-// } OK ParameterNumber|LineLength - See interface.
--- src/test/java/au/net/netstorm/boost/test/automock/MockExpectations.java
+++ src/test/java/au/net/netstorm/boost/test/automock/MockExpectations.java
@@ -1,55 +1,13 @@
 package au.net.netstorm.boost.test.automock;
 
-// OK ParameterNumber|LineLength {
 public interface MockExpectations {
     Object VOID = "void";
 
-    void oneCall(Object ref, Object returnValue, String methodName);
+    void oneCall(Object ref, Object returnValue, String methodName, Object... parameters);
 
-    void oneCall(Object ref, Object returnValue, String methodName, Object parameter1);
+    void oneCall(Object ref, Throwable throwable, String methodName, Object... params);
 
-    void oneCall(Object ref, Object returnValue, String methodName, Object parameter1, Object parameter2);
+    void manyCalls(Object ref, Object returnValue, String methodName, Object... parameters);
 
-    void oneCall(Object ref, Object returnValue, String methodName, Object parameter1, Object parameter2, Object parameter3);
-
-    void oneCall(Object ref, Object returnValue, String methodName, Object parameter1, Object parameter2, Object parameter3, Object parameter4);
-
-    void oneCall(Object ref, Object returnValue, String methodName, Object parameter1, Object parameter2, Object parameter3, Object parameter4, Object parameter5);
-
-    void oneCall(Object ref, Object returnValue, String methodName, Object parameter1, Object parameter2, Object parameter3, Object parameter4, Object parameter5, Object parameter6);
-
-    void oneCall(Object ref, Object returnValue, String methodName, Object parameter1, Object parameter2, Object parameter3, Object parameter4, Object parameter5, Object parameter6, Object parameter7);
-
-    void oneCall(Object ref, Throwable throwable, String methodName);
-
-    void oneCall(Object ref, Throwable throwable, String methodName, Object parameter1);
-
-    void oneCall(Object ref, Throwable throwable, String methodName, Object parameter1, Object parameter2);
-
-    void oneCall(Object ref, Throwable throwable, String methodName, Object parameter1, Object parameter2, Object parameter3);
-
-    void oneCall(Object ref, Throwable throwable, String methodName, Object parameter1, Object parameter2, Object parameter3, Object parameter4);
-
-    void oneCall(Object ref, Throwable throwable, String methodName, Object parameter1, Object parameter2, Object parameter3, Object parameter4, Object parameter5);
-
-    void oneCall(Object ref, Throwable throwable, String methodName, Object parameter1, Object parameter2, Object parameter3, Object parameter4, Object parameter5, Object parameter6);
-
-    void oneCall(Object ref, Throwable throwable, String methodName, Object parameter1, Object parameter2, Object parameter3, Object parameter4, Object parameter5, Object parameter6, Object parameter7);
-
-    void manyCalls(Object ref, Object returnValue, String methodName);
-
-    void manyCalls(Object ref, Object returnValue, String methodName, Object parameter1);
-
-    void manyCalls(Object ref, Object returnValue, String methodName, Object parameter1, Object parameter2);
-
-    void manyCalls(Object ref, Object returnValue, String methodName, Object parameter1, Object parameter2, Object parameter3);
-
-    void manyCalls(Object ref, Object returnValue, String methodName, Object parameter1, Object parameter2, Object parameter3, Object parameter4);
-
-    void manyCalls(Object ref, Object returnValue, String methodName, Object parameter1, Object parameter2, Object parameter3, Object parameter4, Object parameter5);
-
-    void manyCalls(Object ref, Object returnValue, String methodName, Object parameter1, Object parameter2, Object parameter3, Object parameter4, Object parameter5, Object parameter6);
-
-    void manyCalls(Object ref, Object returnValue, String methodName, Object parameter1, Object parameter2, Object parameter3, Object parameter4, Object parameter5, Object parameter6, Object parameter7);
+    void nu(Object ref, Object returnValue, Class impl, Object... params);
 }
-// } OK ParameterNumber|LineLength - We are providing flattened array support ... like jMock.
