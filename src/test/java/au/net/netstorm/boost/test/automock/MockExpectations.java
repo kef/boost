a85c95a700a3fcb093bd53ccbe2acfdb2a598cde
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-12-05 05:17:29 +0000

    IOC - Replace newers with nu.nu()
    git-svn-id: https://geekscape.org/svn/boost/trunk@2943 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/test/automock/MockExpectations.java
+++ src/test/java/au/net/netstorm/boost/test/automock/MockExpectations.java
@@ -1,5 +1,6 @@
 package au.net.netstorm.boost.test.automock;
 
+// FIX (Dec 5, 2007) IOC 85875 Merge with MockExpectationsEngine.
 public interface MockExpectations {
     Object VOID = "void";
 
@@ -9,5 +10,6 @@ public interface MockExpectations {
 
     void manyCalls(Object ref, Object returnValue, String methodName, Object... parameters);
 
+    // FIX (Dec 5, 2007) IOC 85875 MOve the nu on the TestCase maybe?
     void nu(Object ref, Object returnValue, Class impl, Object... params);
 }
