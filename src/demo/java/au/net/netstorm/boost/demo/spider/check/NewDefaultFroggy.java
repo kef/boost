ef53863c6e468aad86e514673b0c072adfebcf25
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-12-06 22:59:53 +0000

    Story:  CORE SPLIT - Juggernaut. [id=87471]
    git-svn-id: https://geekscape.org/svn/boost/trunk@2980 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/demo/java/au/net/netstorm/boost/demo/spider/check/NewDefaultFroggy.java
+++
@@ -1,5 +0,0 @@
-package au.net.netstorm.boost.demo.spider.check;
-
-interface NewDefaultFroggy {
-    Froggy nu(int legs, float maximumLeap);
-}
--- src/demo/java/au/net/netstorm/boost/demo/spider/check/NewDefaultPricklyWithNonMatchingParams.java
+++
@@ -1,5 +0,0 @@
-package au.net.netstorm.boost.demo.spider.check;
-
-interface NewDefaultPricklyWithNonMatchingParams {
-    DefaultPricklyWithNonMatchingParams create(int prickles, String wrongType);
-}
