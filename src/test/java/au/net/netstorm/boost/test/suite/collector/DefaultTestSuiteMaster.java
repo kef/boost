c82cff98ffd3d50e6985e4cc3c57ba274cd92d86
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-12-06 07:56:19 +0000

    bugger off
    git-svn-id: https://geekscape.org/svn/boost/trunk@2976 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/test/suite/collector/DefaultTestSuiteMaster.java
+++ src/test/java/au/net/netstorm/boost/test/suite/collector/DefaultTestSuiteMaster.java
@@ -7,7 +7,6 @@ public class DefaultTestSuiteMaster implements TestSuiteMaster {
     EdgeClass classer = new DefaultEdgeClass();
 
     public <T extends BoostSuite> TestSuites suite(Class<T> suiteCls) {
-        System.out.println("suiteCls = " + suiteCls);
         BoostSuite booster = (BoostSuite) classer.newInstance(suiteCls);
         return booster.suites();
     }
