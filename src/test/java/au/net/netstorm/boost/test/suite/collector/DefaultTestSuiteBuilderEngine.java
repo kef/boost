47c984281837021a63d4684c8943cd3acf211b89
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-12-06 23:42:34 +0000

    Story:  CORE SPLIT - Juggernaut. [id=87471]
    git-svn-id: https://geekscape.org/svn/boost/trunk@2981 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/test/suite/collector/DefaultTestSuiteBuilderEngine.java
+++
@@ -1,6 +0,0 @@
-package au.net.netstorm.boost.test.suite.collector;
-
-public class DefaultTestSuiteBuilderEngine implements TestSuiteBuilderEngine {
-    // FIX (Dec 6, 2007)   87471 Use or lose.
-//    Test suite(Class suiteCls, Object... args);
-}
--- src/test/java/au/net/netstorm/boost/test/suite/collector/TestSuiteBuilderEngine.java
+++
@@ -1,5 +0,0 @@
-package au.net.netstorm.boost.test.suite.collector;
-
-// FIX (Dec 6, 2007)   87471 Rename.  Use or lose.
-public interface TestSuiteBuilderEngine {
-}
