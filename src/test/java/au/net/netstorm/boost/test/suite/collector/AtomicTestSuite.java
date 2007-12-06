d0feb539cfe8a542d14ab3b3317dc71b9db6a1d3
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-12-06 07:59:22 +0000

    bugger off
    git-svn-id: https://geekscape.org/svn/boost/trunk@2977 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/test/suite/collector/AtomicTestSuite.java
+++ src/test/java/au/net/netstorm/boost/test/suite/collector/AtomicTestSuite.java
@@ -3,13 +3,13 @@ package au.net.netstorm.boost.test.suite.collector;
 import junit.framework.Test;
 
 public class AtomicTestSuite implements BoostSuite {
-    private static final TestSuites SUITES = new FileBasedTestSuites("Atomic", AtomicTestSuite.class);
+    private static final TestSuites ATOMIC = new FileBasedTestSuites("Atomic", AtomicTestSuite.class);
 
     public static Test suite() {
-        return SUITES.suite();
+        return ATOMIC.suite();
     }
 
     public TestSuites suites() {
-        return SUITES;
+        return ATOMIC;
     }
 }
--- src/test/java/au/net/netstorm/boost/test/suite/collector/DemoTestSuite.java
+++ src/test/java/au/net/netstorm/boost/test/suite/collector/DemoTestSuite.java
@@ -4,13 +4,13 @@ import junit.framework.Test;
 
 // FIX SC600 Rename "build" area to "gen".
 public class DemoTestSuite implements BoostSuite {
-    private static final TestSuites SUITES = new FileBasedTestSuites("Demo", DemoTestSuite.class);
+    private static final TestSuites DEMO = new FileBasedTestSuites("Demo", DemoTestSuite.class);
 
     public static Test suite() {
-        return SUITES.suite();
+        return DEMO.suite();
     }
 
     public TestSuites suites() {
-        return SUITES;
+        return DEMO;
     }
 }
