2453857a9237f2b28868beb8e26c748ec1e0e64d
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2007-12-06 07:16:03 +0000

    Story:  CORE SPLIT - Juggernaut. [id=87471]
    git-svn-id: https://geekscape.org/svn/boost/trunk@2974 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/test/suite/collector/AllTestSuite.java
+++ src/test/java/au/net/netstorm/boost/test/suite/collector/AllTestSuite.java
@@ -1,11 +1,10 @@
 package au.net.netstorm.boost.test.suite.collector;
 
-import au.net.netstorm.boost.test.core.BoooostCase;
 import junit.framework.Test;
 
 import java.io.File;
 
-public class AllTestSuite extends BoooostCase {
+public class AllTestSuite implements BoostSuite {
     private static final TestSuites COMPOSITES = new CompositeTestSuites("The Whole Box and Dice", AtomicTestSuite.class, DemoTestSuite.class);
 
     public static Test suite() {
@@ -13,6 +12,12 @@ public class AllTestSuite extends BoooostCase {
     }
 
     // FIX (Dec 6, 2007) BREADCRUMB CORE SPLIT 87471 CCCCCCCCCCCCCCCCCC Introduce BoostSuite for this.
+    // FIX (Dec 6, 2007) CORE SPLIT 87471 And do it everywhere.
+    public TestSuites suites() {
+        return COMPOSITES;
+    }
+
+    // FIX (Dec 6, 2007) CORE SPLIT 87471 Remove me when the above magic has been wielded.
     public static Test suite(File root) {
         return COMPOSITES.suite(root);
     }
