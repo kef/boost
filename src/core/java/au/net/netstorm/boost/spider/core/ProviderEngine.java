b434810ac4ff9fc2338b250db35a2ca44093514f
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-01-09 03:43:41 +0000

    Story:  Reinstate flavours. [id=2237]
    git-svn-id: https://geekscape.org/svn/boost/trunk@3020 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/spider/core/ProviderEngine.java
+++ src/core/java/au/net/netstorm/boost/spider/core/ProviderEngine.java
@@ -3,9 +3,6 @@ package au.net.netstorm.boost.spider.core;
 import au.net.netstorm.boost.util.type.Implementation;
 import au.net.netstorm.boost.util.type.ResolvedInstance;
 
-// SUGGEST: SM and others are not comfortable with iface being passed in.
-
-// FIX NOW THIS IS A NuEngine not a ProviderEngine now DefaultProvider is
 public interface ProviderEngine {
     ResolvedInstance provide(Implementation implementation, Object[] params);
 }
