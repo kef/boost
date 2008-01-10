15dfe93f73a86391ebeb76554b9276c526b5ac25
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-01-10 04:15:51 +0000

    Story:  Reinstate flavours. [id=2237]
    git-svn-id: https://geekscape.org/svn/boost/trunk@3058 05db6a09-9903-0410-bedf-ba9bc4063e48

---
+++ src/core/java/au/net/netstorm/boost/nursery/spider/linkage/DefaultLinkageWidener.java
@@ -0,0 +1,42 @@
+package au.net.netstorm.boost.nursery.spider.linkage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class DefaultLinkageWidener implements LinkageWidener {
+    LinkageWildcard wildcard = new DefaultLinkageWildcard();
+
+    public Linkage[] widen(Linkage linkage) {
+        List result = new ArrayList();
+        widest(linkage, result);
+        widenName(linkage, result);
+        widenHost(linkage, result);
+        widenBoth(linkage, result);
+        return (Linkage[]) result.toArray(new Linkage[]{});
+    }
+
+    private void widest(Linkage linkage, List result) {
+        result.add(linkage);
+    }
+
+    private void widenName(Linkage linkage, List result) {
+        if (linkage.named()) {
+            Linkage widened = wildcard.name(linkage);
+            result.add(widened);
+        }
+    }
+
+    private void widenHost(Linkage linkage, List result) {
+        if (linkage.hosted()) {
+            Linkage widened = wildcard.host(linkage);
+            result.add(widened);
+        }
+    }
+
+    private void widenBoth(Linkage linkage, List result) {
+        if (linkage.hosted() && linkage.named()) {
+            Linkage widened = wildcard.both(linkage);
+            result.add(widened);
+        }
+    }
+}
---
+++ src/core/java/au/net/netstorm/boost/nursery/spider/linkage/LinkageWidener.java
@@ -0,0 +1,5 @@
+package au.net.netstorm.boost.nursery.spider.linkage;
+
+public interface LinkageWidener {
+    Linkage[] widen(Linkage linkage);
+}
