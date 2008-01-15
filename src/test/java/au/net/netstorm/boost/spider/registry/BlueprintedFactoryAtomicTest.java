7eb9ef48967a6648ad1575ec10a0296cd73ebc7b
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-01-15 02:13:56 +0000

    Repair.
    git-svn-id: https://geekscape.org/svn/boost/trunk@3081 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/spider/registry/BlueprintedFactoryAtomicTest.java
+++ src/test/java/au/net/netstorm/boost/spider/registry/BlueprintedFactoryAtomicTest.java
@@ -1,42 +1,30 @@
 package au.net.netstorm.boost.spider.registry;
 
+import au.net.netstorm.boost.nursery.spider.linkage.Linkage;
 import au.net.netstorm.boost.test.core.LifecycleTestCase;
 import au.net.netstorm.boost.test.marker.HasFixtures;
 import au.net.netstorm.boost.test.marker.LazyFields;
-import au.net.netstorm.boost.util.type.Implementation;
-import au.net.netstorm.boost.util.type.Interface;
-import au.net.netstorm.boost.util.type.ResolvedInstance;
 
 public final class BlueprintedFactoryAtomicTest extends LifecycleTestCase implements HasFixtures, LazyFields {
     BlueprintedFactory subject;
-    ResolvedInstance instanceMock;
-    ImplementationRef hostDummy;
     BlueprintsRead blueprintsReadMock;
-    Implementation implDummy;
     Blueprint blueprintDummy;
-    Interface ifaceDummy;
+    Linkage linkageDummy;
     Boolean exists;
 
     public void setUpFixtures() {
-//        subject = new BlueprintedFactory(blueprintsReadMock);
+        subject = new BlueprintedFactory(blueprintsReadMock);
     }
 
-    public void testReinstateME() {
-        // FIX ()   2237 Reinstate.
-    }
-
-    // FIX ()   2237 Reinstate.
-/*
     public void testCanHandle() {
-        expect.oneCall(blueprintsReadMock, exists, "exists", ifaceDummy);
-        boolean actual = subject.canHandle(ifaceDummy);
+        expect.oneCall(blueprintsReadMock, exists, "exists", linkageDummy);
+        boolean actual = subject.canHandle(linkageDummy);
         assertEquals(exists, actual);
     }
 
     public void testGet() {
-        expect.oneCall(blueprintsReadMock, blueprintDummy, "get", implDummy, ifaceDummy);
-        Blueprint actual = subject.get(linkage);
+        expect.oneCall(blueprintsReadMock, blueprintDummy, "get", linkageDummy);
+        Blueprint actual = subject.get(linkageDummy);
         assertEquals(blueprintDummy, actual);
     }
-*/
 }
