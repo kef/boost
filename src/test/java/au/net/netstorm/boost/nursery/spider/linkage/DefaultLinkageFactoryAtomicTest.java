37f1c04114b834f2df1281c3479fc51bd9363a18
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-01-15 05:04:30 +0000

    Story:  Reinstate flavours. [id=2237]
    git-svn-id: https://geekscape.org/svn/boost/trunk@3085 05db6a09-9903-0410-bedf-ba9bc4063e48

---
+++ src/test/java/au/net/netstorm/boost/nursery/spider/linkage/DefaultLinkageFactoryAtomicTest.java
@@ -0,0 +1,92 @@
+package au.net.netstorm.boost.nursery.spider.linkage;
+
+import au.net.netstorm.boost.test.core.LifecycleTestCase;
+import au.net.netstorm.boost.test.marker.InjectableTest;
+import au.net.netstorm.boost.test.marker.LazyFields;
+import au.net.netstorm.boost.test.reflect.util.FieldTestUtil;
+import au.net.netstorm.boost.util.type.DefaultImplementation;
+import au.net.netstorm.boost.util.type.DefaultInterface;
+import au.net.netstorm.boost.util.type.Implementation;
+import au.net.netstorm.boost.util.type.Interface;
+
+import java.lang.reflect.Field;
+
+public final class DefaultLinkageFactoryAtomicTest extends LifecycleTestCase implements LazyFields, InjectableTest {
+    private static final String FIELD_NAME = "monkey";
+    Interface iface = new DefaultInterface(Monkey.class);
+    Implementation host = new DefaultImplementation(Zoo.class);
+    LinkageFactory subject;
+    FieldTestUtil fielder;
+    String name;
+
+    public void testNuField() {
+        DefaultLinkage expected = new DefaultLinkage(host, iface, FIELD_NAME);
+        Field field = fielder.get(Zoo.class, FIELD_NAME);
+        Linkage actual = subject.nu(field);
+        assertEquals(expected, actual);
+    }
+
+    public void testNuIface() {
+        Linkage expected = new DefaultLinkage(null, iface, null);
+        checkNuIface(expected);
+        checkNuIfaceStrong(expected);
+    }
+
+    public void testNuHostIface() {
+        Linkage expected = new DefaultLinkage(host, iface, null);
+        checkNuHostIface(expected);
+        checkNuHostIfaceStrong(expected);
+    }
+
+    public void testNuHostIfaceName() {
+        Linkage expected = new DefaultLinkage(host, iface, name);
+        checkNuHostIfaceName(expected);
+        checkNuHostIfaceNameStrong(expected);
+    }
+
+    public void testNuIfaceName() {
+        DefaultLinkage expected = new DefaultLinkage(null, iface, name);
+        checkNuIfaceName(expected);
+        checkNuIfaceNameStrong(expected);
+    }
+
+    private void checkNuIface(Linkage expected) {
+        Linkage actual = subject.nu(Monkey.class);
+        assertEquals(expected, actual);
+    }
+
+    private void checkNuIfaceStrong(Linkage expected) {
+        Linkage actual = subject.nu(iface);
+        assertEquals(expected, actual);
+    }
+
+    private void checkNuHostIface(Linkage expected) {
+        Linkage actual = subject.nu(Zoo.class, Monkey.class);
+        assertEquals(expected, actual);
+    }
+
+    private void checkNuHostIfaceStrong(Linkage expected) {
+        Linkage actual = subject.nu(host, iface);
+        assertEquals(expected, actual);
+    }
+
+    private void checkNuHostIfaceName(Linkage expected) {
+        Linkage actual = subject.nu(Zoo.class, Monkey.class, name);
+        assertEquals(expected, actual);
+    }
+
+    private void checkNuHostIfaceNameStrong(Linkage expected) {
+        Linkage actual = subject.nu(host, iface, name);
+        assertEquals(expected, actual);
+    }
+
+    private void checkNuIfaceName(DefaultLinkage expected) {
+        Linkage actual = subject.nu(null, iface, name);
+        assertEquals(expected, actual);
+    }
+
+    private void checkNuIfaceNameStrong(DefaultLinkage expected) {
+        Linkage actual = subject.nu(null, iface, name);
+        assertEquals(expected, actual);
+    }
+}
---
+++ src/test/java/au/net/netstorm/boost/nursery/spider/linkage/Zoo.java
@@ -0,0 +1,5 @@
+package au.net.netstorm.boost.nursery.spider.linkage;
+
+public final class Zoo {
+    Monkey monkey;
+}
