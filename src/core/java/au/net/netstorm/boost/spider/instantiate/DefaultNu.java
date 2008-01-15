fae37e351fe7b0544a29a9dd55a1efa063a70fb8
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-01-15 08:23:44 +0000

    Story:  SOD - Create a KeyAdmin Role. [id=92405]
    git-svn-id: https://geekscape.org/svn/boost/trunk@3089 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/spider/instantiate/DefaultNu.java
+++ src/core/java/au/net/netstorm/boost/spider/instantiate/DefaultNu.java
@@ -1,11 +1,8 @@
 package au.net.netstorm.boost.spider.instantiate;
 
-import au.net.netstorm.boost.spider.core.NoInterface;
 import au.net.netstorm.boost.spider.core.ProviderEngine;
 import au.net.netstorm.boost.util.type.DefaultImplementation;
-import au.net.netstorm.boost.util.type.DefaultInterface;
 import au.net.netstorm.boost.util.type.Implementation;
-import au.net.netstorm.boost.util.type.Interface;
 import au.net.netstorm.boost.util.type.ResolvedInstance;
 
 public final class DefaultNu implements Nu {
@@ -16,7 +13,6 @@ public final class DefaultNu implements Nu {
     }
 
     public <T> T nu(Class<T> impl, Object... params) {
-        Interface iface = new DefaultInterface(NoInterface.class);
         Implementation implementation = new DefaultImplementation(impl);
         ResolvedInstance resolved = engine.provide(implementation, params);
         Object ref = resolved.getRef();
