8caf25def6abdd85892a5919d7ccdee49f0fa5a5
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-01-16 07:36:30 +0000

    Story:  Reinstate flavours. [id=2237]
    git-svn-id: https://geekscape.org/svn/boost/trunk@3092 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/spider/registry/DefaultBlueprintsAtomicTest.java
+++ src/test/java/au/net/netstorm/boost/spider/registry/DefaultBlueprintsAtomicTest.java
@@ -1,60 +1,66 @@
 package au.net.netstorm.boost.spider.registry;
 
 import au.net.netstorm.boost.nursery.spider.registry.DefaultBlueprints;
-import au.net.netstorm.boost.spider.flavour.NiceMap;
+import au.net.netstorm.boost.nursery.spider.registry.WrongRegistrationException;
+import au.net.netstorm.boost.spider.linkage.DefaultLinkageFactory;
+import au.net.netstorm.boost.spider.linkage.Linkage;
+import au.net.netstorm.boost.spider.linkage.LinkageFactory;
 import au.net.netstorm.boost.test.core.LifecycleTestCase;
 import au.net.netstorm.boost.test.marker.HasFixtures;
-import au.net.netstorm.boost.test.marker.LazyFields;
 import au.net.netstorm.boost.test.reflect.util.DefaultFieldTestUtil;
 import au.net.netstorm.boost.test.reflect.util.FieldTestUtil;
+import au.net.netstorm.boost.util.type.DefaultImplementation;
+import au.net.netstorm.boost.util.type.DefaultInterface;
 import au.net.netstorm.boost.util.type.Implementation;
+import au.net.netstorm.boost.util.type.Interface;
 import au.net.netstorm.boost.util.type.TypeMaster;
 
-public final class DefaultBlueprintsAtomicTest extends LifecycleTestCase implements HasFixtures, LazyFields {
+// FIX 2237 Complete.
+public final class DefaultBlueprintsAtomicTest extends LifecycleTestCase implements HasFixtures {
     private static final Object[] NO_PARAMS = {};
     FieldTestUtil fielder = new DefaultFieldTestUtil();
-    //    Interface iface = iface(Dinosaur.class);
-    //    Interface dodgy = iface(Tree.class);
+    Interface iface = iface(Dinosaur.class);
+    Interface dodgy = iface(Tree.class);
+    LinkageFactory linkageFactory = new DefaultLinkageFactory();
     Blueprint blueprint;
-    NiceMap mapMock;
     Boolean exists;
     Implementation impl;
     TypeMaster typerMock;
     Blueprints subject;
     Implementation hostDummy;
 
-    public void testReinstate() {
-        // FIX ()   2237 Reinstate.
-    }
-
-    // FIX ()   2237 Reinstate.
     public void setUpFixtures() {
         subject = new DefaultBlueprints();
-        fielder.setInstance(subject, "map", mapMock);
-//        blueprint = blueprint(Tyrannosaurus.class);
+        blueprint = blueprint(Tyrannosaurus.class);
     }
 
+    // FIX 2237 Reinstate.
 /*
     public void testExists() {
         expect.oneCall(mapMock, exists, "exists", iface);
         boolean actual = subject.exists(iface);
         assertEquals(exists, actual);
     }
+*/
 
+    // FIX 2237 Reinstate.
+/*
     public void testGet() {
         expect.oneCall(mapMock, blueprint, "get", iface);
         Blueprint actual = subject.get(hostDummy, iface);
         assertEquals(blueprint, actual);
     }
+*/
 
     public void testPut() {
-        expect.oneCall(mapMock, VOID, "put", iface, blueprint);
-        subject.put(zzzzzzz, blueprint);
+        Linkage linkage = linkageFactory.nu(iface);
+        subject.put(linkage, blueprint);
     }
 
     public void testIllegalPut() {
+        Linkage linkage = linkageFactory.nu(dodgy);
         try {
-            subject.put(zzzzzzz, blueprint);
+            subject.put(linkage, blueprint);
             fail();
         } catch (WrongRegistrationException expected) { }
     }
@@ -67,5 +73,4 @@ public final class DefaultBlueprintsAtomicTest extends LifecycleTestCase impleme
         Implementation impl = new DefaultImplementation(cls);
         return new DefaultBlueprint(Stamp.MULTIPLE, impl, NO_PARAMS);
     }
-*/
 }
