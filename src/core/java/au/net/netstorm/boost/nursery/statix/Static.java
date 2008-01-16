ebd5751b3c3f8915e77821f9b28cbcbc803e1e8d
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-01-16 23:32:10 +0000

    2248
    git-svn-id: https://geekscape.org/svn/boost/trunk@3095 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/nursery/statix/Static.java
+++ src/core/java/au/net/netstorm/boost/nursery/statix/Static.java
@@ -4,17 +4,14 @@ import au.net.netstorm.boost.demo.spider.core.Spider;
 import au.net.netstorm.boost.nursery.spider.core.BoostSpiderBuilder;
 import au.net.netstorm.boost.nursery.spider.core.DefaultBoostSpiderBuilder;
 import au.net.netstorm.boost.nursery.type.core.Types;
-import au.net.netstorm.boost.spider.instantiate.Nu;
 
 // OK ConstantName {
 public final class Static {
     public static final Types types;
-    public static final Nu nu;
 
     static {
         Spider spider = spider();
         types = spider.resolve(Types.class);
-        nu = spider.resolve(Nu.class);
     }
 
     private static Spider spider() {
