43b9ca99928626d3f2f0268cb5c8ff324b2538b5
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-01-17 05:38:06 +0000

    Story:  Permit specification of specific implementations in registry. [id=93260]
    git-svn-id: https://geekscape.org/svn/boost/trunk@3103 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/nursery/spider/linkage/DefaultLinkageWildcard.java
+++ src/core/java/au/net/netstorm/boost/nursery/spider/linkage/DefaultLinkageWildcard.java
@@ -6,18 +6,19 @@ import au.net.netstorm.boost.spider.linkage.LinkageFactory;
 import au.net.netstorm.boost.util.type.Implementation;
 import au.net.netstorm.boost.util.type.Interface;
 
+// FIX ()  2237 Perhaps remove Linkage nulls by making different Linkage objects.
 public final class DefaultLinkageWildcard implements LinkageWildcard {
     LinkageFactory linkages = new DefaultLinkageFactory();
 
     public Linkage name(Linkage linkage) {
-        Implementation host = linkage.getHost();
+        Implementation host = linkage.hosted() ? linkage.getHost() : null;
         Interface iface = linkage.getIface();
         return linkages.nu(host, iface, null);
     }
 
     public Linkage host(Linkage linkage) {
         Interface iface = linkage.getIface();
-        String name = linkage.getName();
+        String name = linkage.named() ? linkage.getName() : null;
         return linkages.nu(null, iface, name);
     }
 
--- src/test/java/au/net/netstorm/boost/spider/linkage/DefaultLinkageWidenerAtomicTest.java
+++ src/test/java/au/net/netstorm/boost/spider/linkage/DefaultLinkageWidenerAtomicTest.java
@@ -3,18 +3,37 @@ package au.net.netstorm.boost.spider.linkage;
 import au.net.netstorm.boost.nursery.spider.linkage.DefaultLinkageWidener;
 import au.net.netstorm.boost.nursery.spider.linkage.LinkageWidener;
 import au.net.netstorm.boost.test.core.BoooostCase;
+import au.net.netstorm.boost.util.type.DefaultImplementation;
 import au.net.netstorm.boost.util.type.DefaultInterface;
+import au.net.netstorm.boost.util.type.Implementation;
 import au.net.netstorm.boost.util.type.Interface;
 
 public final class DefaultLinkageWidenerAtomicTest extends BoooostCase {
     private LinkageFactory linkageFactory = new DefaultLinkageFactory();
     LinkageWidener subject = new DefaultLinkageWidener();
     Interface iface = new DefaultInterface(Monkey.class);
+    Implementation host = new DefaultImplementation(Zoo.class);
+    private String name = "aName";
 
-    public void test() {
+    public void testIface() {
         Linkage linkage = linkageFactory.nu(iface);
         Linkage[] linkages = subject.widen(linkage);
         assertEquals(1, linkages.length);
         assertEquals(linkage, linkages[0]);
+    }
+
+    public void testHostIface() {
+        Linkage linkage = linkageFactory.nu(host, iface);
+        Linkage[] linkages = subject.widen(linkage);
+        assertEquals(2, linkages.length);
+        // FIX ()  93260 complete.
+    }
+
+    public void testIfaceName() {
+        Linkage linkage = linkageFactory.nu(null, iface, name);
+        Linkage[] linkages = subject.widen(linkage);
+        assertEquals(2, linkages.length);
+        assertEquals(linkage, linkages[0]);
+        // FIX ()  93260 complete.
     }
 }
