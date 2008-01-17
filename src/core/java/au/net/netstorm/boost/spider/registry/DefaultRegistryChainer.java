31b7b362321989842f7c6848a78dfaf4c9b069fc
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-01-17 07:50:29 +0000

    Story:  Permit specification of specific implementations in registry. [id=93260]
    git-svn-id: https://geekscape.org/svn/boost/trunk@3110 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/spider/registry/DefaultRegistryChainer.java
+++ src/core/java/au/net/netstorm/boost/spider/registry/DefaultRegistryChainer.java
@@ -1,10 +1,10 @@
 package au.net.netstorm.boost.spider.registry;
 
 public final class DefaultRegistryChainer implements RegistryChainer {
-    public <T> void chain(Registry registry, Class<T> iface, Class<? extends T>... impl) {
-        registry.single(iface, impl[0]);
-        for (int i = 0; i < impl.length - 1; i++) {
-            registry.single(impl[i], iface, impl[i + 1]);
+    public <T> void chain(Registry registry, Class<T> iface, Class<? extends T>... impls) {
+        registry.single(iface, impls[0]);
+        for (int i = 0; i < impls.length - 1; i++) {
+            registry.single(impls[i], iface, impls[i + 1]);
         }
     }
 }
