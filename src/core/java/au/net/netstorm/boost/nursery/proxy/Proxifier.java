b2a36279e11a2c92481512c8403a152b6125bc4f
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-01-18 05:32:57 +0000

    Story:  Introduce "core entry" closures replacing servlet filters. [id=93685]
    git-svn-id: https://geekscape.org/svn/boost/trunk@3121 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/nursery/proxy/Proxifier.java
+++ src/core/java/au/net/netstorm/boost/nursery/proxy/Proxifier.java
@@ -3,7 +3,6 @@ package au.net.netstorm.boost.nursery.proxy;
 import au.net.netstorm.boost.spider.onion.core.Layer;
 
 public interface Proxifier {
-    // FIX 2248 Rename?
     <T> T proxy(T ref, ProxySpec spec);
 
     <T> T proxy(T ref, Class<? extends Layer>... layers);
