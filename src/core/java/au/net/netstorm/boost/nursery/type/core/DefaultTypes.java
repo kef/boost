92fa947cce1cfae512f9181ffadceea379da5915
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-01-21 08:08:52 +0000

    Story:  Boost moves. [id=88531]
    git-svn-id: https://geekscape.org/svn/boost/trunk@3133 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/nursery/type/core/DefaultTypes.java
+++ src/core/java/au/net/netstorm/boost/nursery/type/core/DefaultTypes.java
@@ -2,18 +2,17 @@ package au.net.netstorm.boost.nursery.type.core;
 
 import au.net.netstorm.boost.spider.instantiate.Nu;
 import au.net.netstorm.boost.util.impl.ImplMaster;
-import au.net.netstorm.boost.util.type.Data;
 
 // FIX (Nov 21, 2007) 2233 Move out of nursery
 public class DefaultTypes implements Types {
     ImplMaster impler;
     Nu nu;
 
-    public <T extends Holder> T nu(Class<T> iface, Object value) {
+    public <T> T nu(Class<T> iface, Object value) {
         return doNu(iface, value);
     }
 
-    public <T extends Data> T nu(Class<T> iface, Object... values) {
+    public <T> T nu(Class<T> iface, Object... values) {
         return doNu(iface, values);
     }
 
--- src/core/java/au/net/netstorm/boost/nursery/type/core/Types.java
+++ src/core/java/au/net/netstorm/boost/nursery/type/core/Types.java
@@ -1,10 +1,8 @@
 package au.net.netstorm.boost.nursery.type.core;
 
-import au.net.netstorm.boost.util.type.Data;
-
 // FIX (Nov 21, 2007) 2233 Move out of nursery
 public interface Types {
-    <T extends Holder> T nu(Class<T> iface, Object value);
+    <T> T nu(Class<T> iface, Object value);
 
-    <T extends Data> T nu(Class<T> iface, Object... values);
+    <T> T nu(Class<T> iface, Object... values);
 }
