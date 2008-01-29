2aecad5e47393467041943117bf941a647a9203f
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-01-29 04:24:08 +0000

    Story:  Security audit. [id=95456]
    git-svn-id: https://geekscape.org/svn/boost/trunk@3167 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/spider/flavour/Flavour.java
+++
@@ -1,9 +0,0 @@
-package au.net.netstorm.boost.spider.flavour;
-
-import au.net.netstorm.boost.util.type.Data;
-
-public interface Flavour extends Data {
-    Flavour UNFLAVOURED = new DefaultFlavour("Like XXXX ... totally unflavoured");
-
-    String getValue();
-}
--- src/core/java/au/net/netstorm/boost/spider/flavour/FlavouredMapEngine.java
+++
@@ -1,9 +0,0 @@
-package au.net.netstorm.boost.spider.flavour;
-
-public interface FlavouredMapEngine {
-    void put(FlavouredInterface flavour, Object value);
-
-    Object get(FlavouredInterface flavour);
-
-    boolean exists(FlavouredInterface flavour);
-}
