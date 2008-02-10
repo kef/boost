f27a8adc9baa86df1eedda38655309ff961565ec
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-02-10 23:50:14 +0000

    Story:  Sort out enums. Lock them down. [id=97432]
    git-svn-id: https://geekscape.org/svn/boost/trunk@3177 05db6a09-9903-0410-bedf-ba9bc4063e48

---
+++ src/core/java/au/net/netstorm/boost/util/tostring/Frogger.java
@@ -0,0 +1,5 @@
+package au.net.netstorm.boost.util.tostring;
+
+public interface Frogger {
+    String froggify(String text);
+}
---
+++ src/test/java/au/net/netstorm/boost/util/tostring/DefaultFroggerAtomicTest.java
@@ -0,0 +1,20 @@
+package au.net.netstorm.boost.util.tostring;
+
+import au.net.netstorm.boost.test.core.LifecycleTestCase;
+import junit.framework.Assert;
+
+public final class DefaultFroggerAtomicTest extends LifecycleTestCase {
+    Frogger subject = new DefaultFrogger();
+
+    public void testFroggification() {
+        checkFroggification("LILY_PAD", "Lily Pad");
+        checkFroggification("TASTY_FAT_FLY", "Tasty Fat Fly");
+        checkFroggification("", "");
+        checkFroggification("_A_LEADING_UNDERSCORE_", "A Leading Underscore");
+    }
+
+    private void checkFroggification(String input, String output) {
+        String actual = subject.froggify(input);
+        Assert.assertEquals(output, actual);
+    }
+}
