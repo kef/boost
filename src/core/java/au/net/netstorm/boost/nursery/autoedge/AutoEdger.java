57020b316685c79ccaab7754c75b6775af0056fb
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-04-28 23:14:14 +0000

    Story:  Renaming, reorg and packaging. [id=2318]
    git-svn-id: https://geekscape.org/svn/boost/trunk@3234 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/nursery/autoedge/AutoEdger.java
+++ src/core/java/au/net/netstorm/boost/nursery/autoedge/AutoEdger.java
@@ -1,6 +1,7 @@
 package au.net.netstorm.boost.nursery.autoedge;
 
 public interface AutoEdger {
-    <E,T> E edge(Class<E> edge, T target);
+    <E, T> E edge(Class<E> edge, T target);
+
     <E, T> T unedge(E candidate);
 }
