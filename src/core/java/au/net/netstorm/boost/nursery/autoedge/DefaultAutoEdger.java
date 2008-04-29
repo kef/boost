08d187b310911085bedcd82a018d0b6bc9e42ca7
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-04-29 22:07:16 +0000

    Fixed yet another mind explosion.
    git-svn-id: https://geekscape.org/svn/boost/trunk@3238 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/nursery/autoedge/DefaultAutoEdger.java
+++ src/core/java/au/net/netstorm/boost/nursery/autoedge/DefaultAutoEdger.java
@@ -1,14 +1,12 @@
 package au.net.netstorm.boost.nursery.autoedge;
 
 import au.net.netstorm.boost.edge.java.lang.reflect.ProxySupplier;
-import au.net.netstorm.boost.nursery.autoedge.genericnu.TempGenericNu;
 import au.net.netstorm.boost.spider.instantiate.Nu;
 
 public final class DefaultAutoEdger implements AutoEdger {
     ProxySupplier proxier;
     TempMultiNu multiNu;
     Nu nu;
-    TempGenericNu gnu;
 
     @SuppressWarnings("unchecked") // FIXME-MH case for a nuer that supports generics using super type token
     public <E extends Edge<T>, T> E edge(Class<E> edge, T target) {
