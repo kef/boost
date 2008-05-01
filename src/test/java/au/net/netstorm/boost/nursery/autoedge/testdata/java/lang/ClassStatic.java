2aea415ebe3d6e6394461eae5b07d9e6fc4617ab
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-05-01 10:04:21 +0000

    Added a couple of new interfaces to support statics.
    git-svn-id: https://geekscape.org/svn/boost/trunk@3253 05db6a09-9903-0410-bedf-ba9bc4063e48

---
+++ src/test/java/au/net/netstorm/boost/nursery/autoedge/testdata/java/lang/ClassStatic.java
@@ -0,0 +1,7 @@
+package au.net.netstorm.boost.nursery.autoedge.testdata.java.lang;
+
+import au.net.netstorm.boost.nursery.autoedge.StaticEdge;
+
+public interface ClassStatic extends StaticEdge<Class<?>> {
+    Class<?> forName();
+}
