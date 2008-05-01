6a764170760af9c256f91bb82686c2c6eebf1acd
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-05-01 05:59:21 +0000

    Removed old demo test. No longer representitive.
    git-svn-id: https://geekscape.org/svn/boost/trunk@3249 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/demo/java/au/net/netstorm/boost/demo/nursery/autoedge/EdgeToBeEdgedClass.java
+++
@@ -1,7 +0,0 @@
-package au.net.netstorm.boost.demo.nursery.autoedge;
-
-import au.net.netstorm.boost.nursery.autoedge.Edge;
-
-public interface EdgeToBeEdgedClass extends Edge<ToBeEdgedClass> {
-    String edgeMethod();
-}
--- src/demo/java/au/net/netstorm/boost/demo/nursery/autoedge/EdgeToBeEdgedObject.java
+++
@@ -1,8 +0,0 @@
-package au.net.netstorm.boost.demo.nursery.autoedge;
-
-import au.net.netstorm.boost.nursery.autoedge.Edge;
-
-public interface EdgeToBeEdgedObject extends Edge<ToBeEdgedObject> {
-    String getState();
-    void setState(String state);
-}
