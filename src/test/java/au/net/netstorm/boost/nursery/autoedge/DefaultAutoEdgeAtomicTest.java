8091fafd49197b87a1aa155f9182ec359d311593
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-05-02 04:32:45 +0000

    Removed actioned FIX.
    git-svn-id: https://geekscape.org/svn/boost/trunk@3291 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/nursery/autoedge/DefaultAutoEdgeAtomicTest.java
+++ src/test/java/au/net/netstorm/boost/nursery/autoedge/DefaultAutoEdgeAtomicTest.java
@@ -12,9 +12,7 @@ import au.net.netstorm.boost.sniper.marker.InjectableSubject;
 import au.net.netstorm.boost.sniper.marker.InjectableTest;
 import au.net.netstorm.boost.sniper.marker.LazyFields;
 
-// FIX 2328 need to test case where real object is null (static method case)
-
-// FIX 2328 need to test what happens on failures - this is not the test for it, but need somewhere to leave a trail.
+// FIX 2328 need to add test case where real object is null (static method case)
 public final class DefaultAutoEdgeAtomicTest extends LifecycleTestCase implements HasFixtures, InjectableTest, InjectableSubject, LazyFields {
     private AutoEdge subject;
     private Method unedge;
