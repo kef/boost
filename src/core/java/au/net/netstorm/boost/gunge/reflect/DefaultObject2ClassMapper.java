67bc17602d5a1f7620bc32cd5b12682c16696315
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-05-05 04:12:53 +0000

    Fixed broken build (looked like svn move did not remove old '2' named classes).
    Added FIX to remind of a thought I had while reviewing check-ins.
    
    git-svn-id: https://geekscape.org/svn/boost/trunk@3310 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/gunge/reflect/DefaultObject2ClassMapper.java
+++
@@ -1,6 +0,0 @@
-package au.net.netstorm.boost.gunge.reflect;
-
-
-public class DefaultObject2ClassMapper implements Object2ClassMapper {
-    public Class<?> map(Object src) { return src.getClass(); }
-}
--- src/demo/java/au/net/netstorm/boost/demo/nursery/autoedge/IntegerDemoTest.java
+++ src/demo/java/au/net/netstorm/boost/demo/nursery/autoedge/IntegerDemoTest.java
@@ -16,6 +16,10 @@ public class IntegerDemoTest extends LifecycleTestCase implements InjectableTest
         assertEquals(expected, real);
     }
 
+    // FIX 2328 this has just triggered a thought on a possible gotcha...
+    // FIX 2328 there are certain classes where a referential equality is maintained by jdk, see java.lang.Class
+    // FIX 2328 is there anything to stop or identify a brain explosion where some one goes (edgeClass1 == edgeClass2)
+    // FIX 2328 basically if someone assumes that the edges maintain identity semantics
     public void testEquality() {
         Integer i1 = edger.nu(Integer.class, 5);
         Integer i2 = edger.nu(Integer.class, 5);
