5623d86ec0397bf5d1cfc967a9c865fb98bd8664
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-05-05 03:01:06 +0000

    Story:  Implement "auto edge" spike. [id=2328]
    git-svn-id: https://geekscape.org/svn/boost/trunk@3304 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/demo/java/demo/edge/bad/pack/age/String.java
+++ src/demo/java/demo/edge/bad/pack/age/String.java
@@ -2,5 +2,6 @@ package demo.edge.bad.pack.age;
 
 import au.net.netstorm.boost.nursery.autoedge.Edge;
 
+// FIX 2328 MAG NICE SNEAKY PACKAGE NAMING ... LOVE IT!  (pack.age)
 public interface String extends Edge<java.lang.String> {
 }
