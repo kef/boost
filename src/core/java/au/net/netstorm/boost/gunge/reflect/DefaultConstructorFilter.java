272c1f5bb618799b376db3d67438e47504fe0276
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-05-05 02:43:42 +0000

    Story:  Implement "auto edge" spike. [id=2328]
    git-svn-id: https://geekscape.org/svn/boost/trunk@3299 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/gunge/reflect/DefaultConstructorFilter.java
+++ src/core/java/au/net/netstorm/boost/gunge/reflect/DefaultConstructorFilter.java
@@ -2,18 +2,17 @@ package au.net.netstorm.boost.gunge.reflect;
 
 import java.lang.reflect.Constructor;
 import java.util.List;
-
 import au.net.netstorm.boost.gunge.collection.FunctionalCollection;
-import au.net.netstorm.boost.spider.instantiate.Nu;
+import au.net.netstorm.boost.nursery.type.core.Types;
 
 public class DefaultConstructorFilter implements ConstructorFilter {
     FunctionalCollection collection;
     Object2ClassMapper mapper;
-    Nu nu;
+    Types types;
 
     public List<Constructor<?>> filter(Class<?> type, Object... params) {
         List<Class<?>> paramTypes = collection.map(params, mapper);
-        CompatibleSignaturesFilter filter = nu.nu(DefaultCompatibleSignaturesFilter.class, paramTypes);
+        CompatibleSignaturesFilter filter = types.nu(CompatibleSignaturesFilter.class, paramTypes);
         Constructor<?>[] ctors = type.getConstructors();
         return collection.filter(ctors, filter);
     }
--- src/test/java/au/net/netstorm/boost/gunge/reflect/DefaultConstructorFilterAtomicTest.java
+++ src/test/java/au/net/netstorm/boost/gunge/reflect/DefaultConstructorFilterAtomicTest.java
@@ -2,14 +2,13 @@ package au.net.netstorm.boost.gunge.reflect;
 
 import java.lang.reflect.Constructor;
 import java.util.List;
-
 import au.net.netstorm.boost.gunge.collection.FunctionalCollection;
+import au.net.netstorm.boost.nursery.type.core.Types;
 import au.net.netstorm.boost.sniper.core.LifecycleTestCase;
 import au.net.netstorm.boost.sniper.marker.HasFixtures;
 import au.net.netstorm.boost.sniper.marker.InjectableSubject;
 import au.net.netstorm.boost.sniper.marker.InjectableTest;
 import au.net.netstorm.boost.sniper.marker.LazyFields;
-import au.net.netstorm.boost.spider.instantiate.Nu;
 
 public final class DefaultConstructorFilterAtomicTest extends LifecycleTestCase
         implements HasFixtures, InjectableSubject, InjectableTest, LazyFields {
@@ -18,22 +17,22 @@ public final class DefaultConstructorFilterAtomicTest extends LifecycleTestCase
     ConstructorFixture fixture;
     FunctionalCollection collectionMock;
     Object2ClassMapper mapperMock;
-    Nu nuMock;
+    Types typesMock;
     CompatibleSignaturesFilter compatMock;
     List<Class<?>> classesMock;
     List<Constructor<?>> ctorMock;
 
+    // FIX 2328 The testing framework should do this for us!!!!
     public void setUpFixtures() {
         subject = new DefaultConstructorFilter();
     }
 
     public void testFilter() {
-        expect.oneCall(collectionMock, classesMock, "map", new Object[] { fixture.vector() }, mapperMock);
-        expect.oneCall(nuMock, compatMock, "nu", DefaultCompatibleSignaturesFilter.class, new Object[] { classesMock });
+        List<?> vector = fixture.vector();
+        expect.oneCall(collectionMock, classesMock, "map", new Object[]{vector}, mapperMock);
+        expect.oneCall(typesMock, compatMock, "nu", CompatibleSignaturesFilter.class, classesMock);
         expect.oneCall(collectionMock, ctorMock, "filter", fixture.constructors(), compatMock);
-
-        List<Constructor<?>> result = subject.filter(DualOverloadCtor.class, fixture.vector());
-
+        List<Constructor<?>> result = subject.filter(DualOverloadCtor.class, vector);
         assertEquals(ctorMock, result);
     }
 }
