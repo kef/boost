2433b4c50c15b82dc1636f11acc9746116a0b8ad
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-05-06 12:57:40 +0000

    Story: Renaming, reorg and packaging. [id=2318]
    git-svn-id: https://geekscape.org/svn/boost/trunk@3355 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/edge/testfixtures/EdgeStreamFixture.java
+++ src/test/java/au/net/netstorm/boost/edge/testfixtures/EdgeStreamFixture.java
@@ -15,6 +15,9 @@ public interface EdgeStreamFixture {
 
     byte[] data();
 
+    // FIX 2328 Return Integer.  Primitive suck.
+    // FIX 2328 This will get rid of warnings in DAEAT.
+
     int length();
 
     InputStream stream();
