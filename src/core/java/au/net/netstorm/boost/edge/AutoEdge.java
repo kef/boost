51549e42f366cf5f13297865c3a4b5cee912e561
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-05-06 12:54:42 +0000

    Story: Renaming, reorg and packaging. [id=2318]
    git-svn-id: https://geekscape.org/svn/boost/trunk@3354 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/edge/AutoEdge.java
+++ src/core/java/au/net/netstorm/boost/edge/AutoEdge.java
@@ -3,6 +3,7 @@ package au.net.netstorm.boost.edge;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 
+// FIX 2328 Split this package so the goodness is clear in "core".
 interface AutoEdge extends InvocationHandler {
     Object invoke(Object edge, Method edgeMethod, Object[] edgedArgs);
 }
