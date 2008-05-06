8b39bdea209ca6f93505039bd8aea7c783d2e70c
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-05-06 12:25:55 +0000

    Story: Renaming, reorg and packaging. [id=2318]
    git-svn-id: https://geekscape.org/svn/boost/trunk@3344 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/sniper/core/Test.java
+++ src/test/java/au/net/netstorm/boost/sniper/core/Test.java
@@ -1,10 +1,5 @@
 package au.net.netstorm.boost.sniper.core;
 
 public interface Test {
-    String getName();
-
-    // FIX 2318 Delete this.  Not used.
-    void setName(String name);
-
     void runTest() throws Throwable;
 }
