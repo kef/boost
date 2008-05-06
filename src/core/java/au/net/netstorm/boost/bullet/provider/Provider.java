95f189405c69c7e676f275e05c9358f312261a31
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-05-06 13:01:54 +0000

    Story: Renaming, reorg and packaging. [id=2318]
    git-svn-id: https://geekscape.org/svn/boost/trunk@3358 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/bullet/provider/Provider.java
+++ src/core/java/au/net/netstorm/boost/bullet/provider/Provider.java
@@ -1,5 +1,8 @@
 package au.net.netstorm.boost.bullet.provider;
 
+// FIX 2318 Looks just like the "Resolver" too.
+// FIX 2318 Where do you really belong my lovely puff pastry.
+
 // FIX 2290 Move into "spider" area.  This is not a top level package.
 public interface Provider {
     <T> T provide(Class<T> type);
