8dce1679b8a703746dfd9b64c77be5a4dea09b6b
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-05-07 05:43:38 +0000

    Story: Spike mechanism for generic nuing [id=2363]
    git-svn-id: https://geekscape.org/svn/boost/trunk@3367 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/gunge/type/Anchor.java
+++
@@ -1,7 +0,0 @@
-package au.net.netstorm.boost.gunge.type;
-
-import java.lang.reflect.Field;
-
-public interface Anchor {
-    Field getField();
-}
--- src/test/java/au/net/netstorm/boost/gunge/type/Dude.java
+++
@@ -1,7 +0,0 @@
-package au.net.netstorm.boost.gunge.type;
-
-import java.util.List;
-
-final class Dude implements Acrobat {
-    List<Thing> tricks;
-}
