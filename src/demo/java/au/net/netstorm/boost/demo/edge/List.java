de0b4dbdf4ca345aa1662a8486b2f336fac7fcf3
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-05-08 02:53:58 +0000

    Story: Renaming, reorg and packaging. [id=2318]
    git-svn-id: https://geekscape.org/svn/boost/trunk@3379 05db6a09-9903-0410-bedf-ba9bc4063e48

---
+++ src/demo/java/au/net/netstorm/boost/demo/edge/List.java
@@ -0,0 +1,7 @@
+package au.net.netstorm.boost.demo.edge;
+
+import au.net.netstorm.boost.edge.core.Edge;
+
+// FIX BREADCRUMB 2328 AAAAAAAAAAAAAAAAAAAAA Move to d.e.j.u.
+public interface List extends Edge<java.util.List> {
+}
---
+++ src/demo/java/au/net/netstorm/boost/demo/edge/ListDemoTest.java
@@ -0,0 +1,15 @@
+package au.net.netstorm.boost.demo.edge;
+
+import au.net.netstorm.boost.edge.core.AutoEdger;
+import au.net.netstorm.boost.sniper.core.LifecycleTestCase;
+import au.net.netstorm.boost.sniper.marker.InjectableTest;
+
+// FIX 2328 Rename.
+public final class ListDemoTest extends LifecycleTestCase implements InjectableTest {
+    AutoEdger edger;
+
+    // FIX 2328 Complete.
+    public void testNuSubType() {
+//        List edge = edger.nu(List.class, 5);
+    }
+}
