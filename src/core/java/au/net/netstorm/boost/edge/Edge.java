b2a9e8feedf72b1c991989683f21348d5d2ff2a3
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-05-08 00:43:11 +0000

    Story: Implement "auto edge" spike. [id=2328]
    git-svn-id: https://geekscape.org/svn/boost/trunk@3374 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/edge/Edge.java
+++ src/core/java/au/net/netstorm/boost/edge/Edge.java
@@ -1,5 +1,5 @@
 package au.net.netstorm.boost.edge;
 
 // FIX 2328 there is no longer a strong reason to separate out edge and static edge
-// FIX 2328 could now just be a naming convention... need to think about
+// FIX 2328 static edge could just be a naming convention... need to think about
 public interface Edge<R> {}
