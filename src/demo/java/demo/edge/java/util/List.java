f0122ddbcc37c61ef03e034bcfa2824289591982
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-05-08 02:55:47 +0000

    Story: Renaming, reorg and packaging. [id=2318]
    git-svn-id: https://geekscape.org/svn/boost/trunk@3380 05db6a09-9903-0410-bedf-ba9bc4063e48

---
+++ src/demo/java/demo/edge/java/util/List.java
@@ -0,0 +1,7 @@
+package demo.edge.java.util;
+
+import au.net.netstorm.boost.edge.core.Edge;
+
+// FIX BREADCRUMB 2328 AAAAAAAAAAAAAAAAAAAAA Move to d.e.j.u.
+public interface List extends Edge<java.util.List> {
+}
