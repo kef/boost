7b5748bf7d3f81a2e2c3d17c88dce57751e1d36e
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-05-09 13:10:44 +0000

    Story: Implement "auto edge" spike. [id=2328]
    git-svn-id: https://geekscape.org/svn/boost/trunk@3433 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/edge/guts/EdgeMapper.java
+++ src/core/java/au/net/netstorm/boost/edge/guts/EdgeMapper.java
@@ -1,7 +1,6 @@
 package au.net.netstorm.boost.edge.guts;
 
 public interface EdgeMapper {
-    // FIX 2328 wire into and validator
     Class<?> staticEdgeToReal(Class<?> edge);
     Class<?> edgeToReal(Class<?> edge);
     Class<?> realToEdge(Class<?> real);
