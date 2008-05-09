b0c33509577d00b11b4f8c508a9767937d2591e9
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-05-09 04:30:19 +0000

    Story: Implement "auto edge" spike. [id=2328]
    git-svn-id: https://geekscape.org/svn/boost/trunk@3399 05db6a09-9903-0410-bedf-ba9bc4063e48

---
+++ src/core/java/au/net/netstorm/boost/gunge/string/DefaultStringTransform.java
@@ -0,0 +1,19 @@
+package au.net.netstorm.boost.gunge.string;
+
+public class DefaultStringTransform implements StringTransform {
+    public String stripPrefix(String s, String prefix) {
+        if (!s.startsWith(prefix)) fail(s, "start", prefix);
+        int newStart = prefix.length();
+        return s.substring(newStart);
+    }
+
+    public String stripSuffix(String s, String suffix) {
+        if (!s.endsWith(suffix)) fail(s, "end", suffix);
+        int newEnd = s.length() - suffix.length();
+        return s.substring(0, newEnd);
+    }
+
+    private void fail(String s, String reason, String token) {
+        throw new IllegalArgumentException("Can't transform " + s + " does not " + reason + " with" + token);
+    }
+}
