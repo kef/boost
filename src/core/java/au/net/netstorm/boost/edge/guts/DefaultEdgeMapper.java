9873940a49c154ab887ce75204e617e7bd7c380c
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-05-09 12:20:18 +0000

    Story: Implement "auto edge" spike. [id=2328]
    git-svn-id: https://geekscape.org/svn/boost/trunk@3422 05db6a09-9903-0410-bedf-ba9bc4063e48

---
+++ src/core/java/au/net/netstorm/boost/edge/guts/DefaultEdgeMapper.java
@@ -0,0 +1,27 @@
+package au.net.netstorm.boost.edge.guts;
+
+import au.net.netstorm.boost.edge.java.lang.EdgeClass;
+
+// FIX 2328 another case for the exception handling strategy
+public final class DefaultEdgeMapper implements EdgeMapper {
+    EdgeClass classer;
+    EdgeNameMapper mapper;
+
+    public Class<?> staticEdgeToReal(Class<?> edge) {
+        String edgeName = edge.getName();
+        String realName = mapper.staticEdgeToReal(edgeName);
+        return classer.forName(realName);
+    }
+
+    public Class<?> edgeToReal(Class<?> edge) {
+        String edgeName = edge.getName();
+        String realName = mapper.edgeToReal(edgeName);
+        return classer.forName(realName);
+    }
+
+    public Class<?> realToEdge(Class<?> real) {
+        String realName = real.getName();
+        String edgeName = mapper.realToEdge(realName);
+        return classer.forName(edgeName);
+    }
+}
--- src/test/java/au/net/netstorm/boost/edge/guts/DefaultEdgeMapperAtomicTest.java
+++ src/test/java/au/net/netstorm/boost/edge/guts/DefaultEdgeMapperAtomicTest.java
@@ -1,26 +1,44 @@
 package au.net.netstorm.boost.edge.guts;
 
+import au.net.netstorm.boost.edge.java.lang.EdgeClass;
+import au.net.netstorm.boost.edge.testdata.java.lang.ClassStatic;
+import au.net.netstorm.boost.edge.testdata.java.net.URL;
 import au.net.netstorm.boost.sniper.core.LifecycleTestCase;
 import au.net.netstorm.boost.sniper.marker.HasFixtures;
 import au.net.netstorm.boost.sniper.marker.InjectableSubject;
 import au.net.netstorm.boost.sniper.marker.InjectableTest;
 import au.net.netstorm.boost.sniper.marker.LazyFields;
 
-// FIX 2328 reinstate me when implemented
 public class DefaultEdgeMapperAtomicTest extends LifecycleTestCase implements HasFixtures, InjectableSubject, InjectableTest, LazyFields {
     private EdgeMapper subject;
     EdgeNameMapper mapperMock;
+    EdgeClass classerMock;
 
     public void setUpFixtures() {
-//        subject = new DefaultEdgeMapper();
+        subject = new DefaultEdgeMapper();
     }
 
     public void testStaticEdgeToReal() {
+        expects("staticEdgeToReal", ClassStatic.class, Class.class);
+        Class<?> result = subject.staticEdgeToReal(ClassStatic.class);
+        assertEquals(Class.class, result);
     }
 
     public void testEdgeToReal() {
+        expects("edgeToReal", URL.class, java.net.URL.class);
+        Class<?> result = subject.edgeToReal(URL.class);
+        assertEquals(java.net.URL.class, result);
     }
 
     public void testRealToEdge() {
+        expects("realToEdge", java.net.URL.class, URL.class);
+        Class<?> result = subject.realToEdge(java.net.URL.class);
+        assertEquals(URL.class, result);
+    }
+
+    private void expects(String method, Class<?> from, Class<?> to) {
+        String edgeName = from.getName();
+        expect.oneCall(mapperMock, "realname", method, edgeName);
+        expect.oneCall(classerMock, to, "forName", "realname");
     }
 }
