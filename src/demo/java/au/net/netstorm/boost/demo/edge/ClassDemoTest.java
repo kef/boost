cedc8b5693f207b53b762f0cecd19bcc28915c8f
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-05-09 14:01:14 +0000

    Story: Implement "auto edge" spike. [id=2328]
    git-svn-id: https://geekscape.org/svn/boost/trunk@3441 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/demo/java/au/net/netstorm/boost/demo/edge/ClassDemoTest.java
+++ src/demo/java/au/net/netstorm/boost/demo/edge/ClassDemoTest.java
@@ -1,23 +1,17 @@
 package au.net.netstorm.boost.demo.edge;
 
-import au.net.netstorm.boost.sniper.core.LifecycleTestCase;
-import au.net.netstorm.boost.sniper.marker.HasFixtures;
-import au.net.netstorm.boost.sniper.marker.InjectableTest;
+import au.net.netstorm.boost.edge.core.AutoEdger;
 import demo.edge.java.lang.Class;
 
 //FIX 2328 reinstate when default package mapping implemented
-public class ClassDemoTest extends LifecycleTestCase implements HasFixtures, InjectableTest {
-    private Class<String> subject;
-//    AutoEdger edger;
-
-    public void setUpFixtures() {
-        // FIX 2328 unchecked cast - handling generic classes in edge method should
-        // FIX 2328 take same approach as Nu, see gunge.generics.TypeToken
-//        subject = edger.edge(Class.class, String.class);
-    }
+public class ClassDemoTest extends EdgeDemooooTest {
+    AutoEdger edger;
 
     public void testNewInstance() {
-//        String result = subject.newInstance();
-//        assertEquals("", result);
+        // FIX 2328 unchecked cast - handling generic classes in edge method should
+        // FIX 2328 take same approach as Nu, see gunge.generics.TypeToken
+        Class<String> subject = edger.edge(Class.class, String.class);
+        String result = subject.newInstance();
+        assertEquals("", result);
     }
 }
