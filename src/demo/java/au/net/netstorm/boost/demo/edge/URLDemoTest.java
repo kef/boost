b74b05567976d8ab727183f306de084d602e8c18
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-05-09 13:36:36 +0000

    Story: Implement "auto edge" spike. [id=2328]
    git-svn-id: https://geekscape.org/svn/boost/trunk@3438 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/demo/java/au/net/netstorm/boost/demo/edge/URLDemoTest.java
+++ src/demo/java/au/net/netstorm/boost/demo/edge/URLDemoTest.java
@@ -1,30 +1,32 @@
 package au.net.netstorm.boost.demo.edge;
 
-import au.net.netstorm.boost.sniper.core.LifecycleTestCase;
-import au.net.netstorm.boost.sniper.marker.InjectableTest;
+import java.lang.reflect.InvocationTargetException;
+
+import au.net.netstorm.boost.edge.core.AutoEdger;
+import au.net.netstorm.boost.edge.guts.EdgeException;
 import demo.edge.java.net.URL;
-//FIX 2328 reinstate when default package mapping implemented
-public class URLDemoTest extends LifecycleTestCase implements InjectableTest {
+
+public class URLDemoTest extends EdgeDemooooTest {
     private URL subject;
-//    AutoEdger edger;
+    AutoEdger edger;
 
     public void testURLEdgeConstruction() {
-//        String url = "http://boost";
-//        subject = edger.nu(URL.class, url);
-//        assertEquals(url, subject.toExternalForm());
+        String url = "http://boost";
+        subject = edger.nu(URL.class, url);
+        assertEquals(url, subject.toExternalForm());
     }
 
     public void testURLEdgeConstructionFailure() {
-//        try {
-//            edger.nu(URL.class, "..badone..");
-//            fail();
-//        } catch (EdgeException e) {
-//            // FIX 2328 see FIX in DefaultEdgeMethod do we want to change this?
-//            // FIX 2328 MAG I don't think we want to change this on DEM.
-//            // FIX 2328 MAG But we probably do want to change it elsewhere.
-//            // FIX 2328 MAG Agree this should look like a MURLE.
-//            // assertEquals(true, e.causeIs(MalformedURLException.class));
-//            assertEquals(true, e.causeIs(InvocationTargetException.class));
-//        }
+        try {
+            edger.nu(URL.class, "..badone..");
+            fail();
+        } catch (EdgeException e) {
+            // FIX 2328 see FIX in DefaultEdgeMethod do we want to change this?
+            // FIX 2328 MAG I don't think we want to change this on DEM.
+            // FIX 2328 MAG But we probably do want to change it elsewhere.
+            // FIX 2328 MAG Agree this should look like a MURLE.
+            // assertEquals(true, e.causeIs(MalformedURLException.class));
+            assertEquals(true, e.causeIs(InvocationTargetException.class));
+        }
     }
 }
