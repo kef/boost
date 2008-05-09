9cc762fbcd55c3135222e478517e842cb582c717
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-05-09 09:36:30 +0000

    Story: Implement "auto edge" spike. [id=2328]
    git-svn-id: https://geekscape.org/svn/boost/trunk@3401 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/edge/core/Edge.java
+++ src/core/java/au/net/netstorm/boost/edge/core/Edge.java
@@ -1,4 +1,5 @@
 package au.net.netstorm.boost.edge.core;
 
+//FIX 2328 remove edger dependency on this and delete this
 public interface Edge {
 }
--- src/core/java/au/net/netstorm/boost/edge/core/StaticEdge.java
+++ src/core/java/au/net/netstorm/boost/edge/core/StaticEdge.java
@@ -1,5 +1,6 @@
 package au.net.netstorm.boost.edge.core;
 
+// FIX 2328 remove edger dependency on this and delete this
 public interface StaticEdge {
 }
 
