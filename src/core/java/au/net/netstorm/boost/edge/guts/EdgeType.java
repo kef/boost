12a71f09457a558a347d52999b2b7b881febfb81
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-05-09 01:00:39 +0000

    Story: Implement "auto edge" spike. [id=2328]
    git-svn-id: https://geekscape.org/svn/boost/trunk@3387 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/edge/guts/EdgeType.java
+++ src/core/java/au/net/netstorm/boost/edge/guts/EdgeType.java
@@ -1,7 +1,9 @@
 package au.net.netstorm.boost.edge.guts;
 
+import au.net.netstorm.boost.gunge.type.Data;
+
 // FIX 2328 implement holder that maps edge->real class
-public interface EdgeType {
-    Class<?> edge();
-    Class<?> real();
+public interface EdgeType extends Data {
+    Class<?> getEdge();
+    Class<?> getReal();
 }
