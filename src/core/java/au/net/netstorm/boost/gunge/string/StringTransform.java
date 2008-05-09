7c163a7edfed2967c6e31666a979e0dce75707ec
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-05-09 04:19:18 +0000

    Story: Implement "auto edge" spike. [id=2328]
    git-svn-id: https://geekscape.org/svn/boost/trunk@3397 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/gunge/string/StringTransform.java
+++ src/core/java/au/net/netstorm/boost/gunge/string/StringTransform.java
@@ -2,6 +2,6 @@ package au.net.netstorm.boost.gunge.string;
 
 // FIX 2328 implement me and wire into class warper
 public interface StringTransform {
-    String removePrefix(String prefix);
-    String removeSuffix(String prefix);
+    String stripPrefix(String s, String prefix);
+    String stripSuffix(String s, String prefix);
 }
---
+++ src/test/java/au/net/netstorm/boost/gunge/string/DefaultStringTransformAtomicTest.java
@@ -0,0 +1,31 @@
+package au.net.netstorm.boost.gunge.string;
+
+import au.net.netstorm.boost.sniper.core.LifecycleTestCase;
+import au.net.netstorm.boost.sniper.marker.InjectableTest;
+
+// FIX 2328 reinstate when implemented
+public class DefaultStringTransformAtomicTest extends LifecycleTestCase implements InjectableTest {
+//    StringTransform subject;
+
+    public void testStripPrefix() {
+//        String result = subject.stripPrefix("abc", "a");
+//        assertEquals("bc", result);
+    }
+
+    public void testStripPrefixFailure() {
+//        try {
+//            subject.stripPrefix("abc", "c");
+//        } catch (IllegalArgumentException expected) {}
+    }
+
+    public void testStripSuffix() {
+//        String result = subject.stripPrefix("abc", "c");
+//        assertEquals("ab", result);
+    }
+
+    public void testStripSuffixFailure() {
+//        try {
+//            subject.stripPrefix("abc", "a");
+//        } catch (IllegalArgumentException expected) {}
+    }
+}
