3bb0a7f4626937e4a998624781778d9e42ab27f8
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-05-09 13:27:35 +0000

    Story: Implement "auto edge" spike. [id=2328]
    git-svn-id: https://geekscape.org/svn/boost/trunk@3436 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/demo/java/au/net/netstorm/boost/demo/edge/IntegerDemoTest.java
+++ src/demo/java/au/net/netstorm/boost/demo/edge/IntegerDemoTest.java
@@ -1,20 +1,10 @@
 package au.net.netstorm.boost.demo.edge;
 
 import au.net.netstorm.boost.edge.core.AutoEdger;
-import au.net.netstorm.boost.edge.guts.EdgePackage;
-import au.net.netstorm.boost.sniper.core.LifecycleTestCase;
-import au.net.netstorm.boost.sniper.marker.InjectableTest;
-import au.net.netstorm.boost.sniper.marker.OverlaysWeb;
-import au.net.netstorm.boost.spider.registry.Registry;
 import demo.edge.java.lang.Integer;
-public class IntegerDemoTest extends LifecycleTestCase implements OverlaysWeb, InjectableTest {
-    AutoEdger edger;
-    Registry registry;
 
-    // FIX 2328 not required in normal (non-demo) case, maybe pull out of demo test somehow?
-    public void overlay(Registry registry) {
-        registry.single(EdgePackage.class, DemoEdgePackage.class);
-    }
+public class IntegerDemoTest extends EdgeDemooooTest {
+    AutoEdger edger;
 
     public void testPrimitiveConstruction() {
         int expected = 5;
