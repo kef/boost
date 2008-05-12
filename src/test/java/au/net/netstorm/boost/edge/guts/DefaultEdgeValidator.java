d5dd70930c817502ca738113567576b531c4a618
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-05-12 23:50:23 +0000

    Story: Implement "auto edge" spike. [id=2328]
    git-svn-id: https://geekscape.org/svn/boost/trunk@3476 05db6a09-9903-0410-bedf-ba9bc4063e48

---
+++ src/test/java/au/net/netstorm/boost/edge/guts/DefaultEdgeValidator.java
@@ -0,0 +1,11 @@
+package au.net.netstorm.boost.edge.guts;
+
+public final class DefaultEdgeValidator implements EdgeValidator {
+    public void validateEdge(Class<?> edge, Class<?> real) {
+        // FIX 2328 implement me
+    }
+
+    public void validateStaticEdge(Class<?> edge, Class<?> real) {
+        // FIX 2328 implement me
+    }
+}
--- src/test/java/au/net/netstorm/boost/edge/guts/DefaultEdgeValidatorAtomicTest.java
+++ src/test/java/au/net/netstorm/boost/edge/guts/DefaultEdgeValidatorAtomicTest.java
@@ -1,5 +1,7 @@
 package au.net.netstorm.boost.edge.guts;
 
+import au.net.netstorm.boost.edge.testdata.java.lang.ClassStatic;
+import au.net.netstorm.boost.edge.testdata.java.util.List;
 import au.net.netstorm.boost.sniper.core.LifecycleTestCase;
 import au.net.netstorm.boost.sniper.marker.HasFixtures;
 import au.net.netstorm.boost.sniper.marker.InjectableSubject;
@@ -10,16 +12,17 @@ public final class DefaultEdgeValidatorAtomicTest extends LifecycleTestCase impl
     private EdgeValidator validator;
 
     public void setUpFixtures() {
-//        validator = new DefaultEdgeValidator();
+        validator = new DefaultEdgeValidator();
     }
 
     // FIX 2328 drive up instance edge validation
     public void testValidateEdge() {
+        validator.validateEdge(List.class, java.util.List.class);
 
     }
 
     // FIX 2328 drive up static edge validation
     public void testValidateStaticEdge() {
-
+        validator.validateEdge(ClassStatic.class, Class.class);
     }
 }
