3463169816e91322a1aacbe2ca7fbdc0ef2b7461
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-05-12 23:39:43 +0000

    Story: Implement "auto edge" spike. [id=2328]
    git-svn-id: https://geekscape.org/svn/boost/trunk@3473 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/edge/guts/EdgeValidator.java
+++ src/core/java/au/net/netstorm/boost/edge/guts/EdgeValidator.java
@@ -2,5 +2,6 @@ package au.net.netstorm.boost.edge.guts;
 
 // FIX 2328 adding eager validator
 public interface EdgeValidator {
-
+    void validateEdge(Class<?> edge, Class<?> real);
+    void validateStaticEdge(Class<?> edge, Class<?> real);
 }
