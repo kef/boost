ecb75d4c3ced6eb83c718cc73e7c9114a7258f4c
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-05-12 23:55:39 +0000

    Story: Implement "auto edge" spike. [id=2328]
    git-svn-id: https://geekscape.org/svn/boost/trunk@3479 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/edge/testdata/java/util/BadList.java
+++ src/test/java/au/net/netstorm/boost/edge/testdata/java/util/BadList.java
@@ -1,6 +1,10 @@
 package au.net.netstorm.boost.edge.testdata.java.util;
 
+import java.util.Iterator;
+
 import au.net.netstorm.boost.edge.core.Edge;
 
-public interface BadList<T> extends Edge {}
+public interface BadList<T> extends Edge {
+    Iterator<T> getIterator(); // method should not match
+}
 
--- src/test/java/au/net/netstorm/boost/edge/testdata/java/util/List.java
+++ src/test/java/au/net/netstorm/boost/edge/testdata/java/util/List.java
@@ -1,5 +1,9 @@
 package au.net.netstorm.boost.edge.testdata.java.util;
 
+import java.util.Iterator;
+
 import au.net.netstorm.boost.edge.core.Edge;
 
-public interface List<T> extends Edge {}
+public interface List<T> extends Edge, Iterable<T> {
+    Iterator<T> iterator();
+}
