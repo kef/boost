b02f623c546efaa298cd259975c0ee173a18a267
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-05-14 04:15:12 +0000

    Story: Implement "auto edge" spike. [id=2328]
    git-svn-id: https://geekscape.org/svn/boost/trunk@3501 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/sniper/automock/TestFieldInjector.java
+++ src/test/java/au/net/netstorm/boost/sniper/automock/TestFieldInjector.java
@@ -3,6 +3,7 @@ package au.net.netstorm.boost.sniper.automock;
 public interface TestFieldInjector {
     void injectSubject(Object ref);
 
+    // FIX 2328 add method to inject reals and wire into InjectTest
     void injectTestDoubles(Object ref);
 
     void verify();
