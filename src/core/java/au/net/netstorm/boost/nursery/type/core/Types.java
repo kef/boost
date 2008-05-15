e448414d10024c3a84ace9194eae70178ad90672
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-05-15 03:32:33 +0000

    git-svn-id: https://geekscape.org/svn/boost/trunk@3521 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/nursery/type/core/Types.java
+++ src/core/java/au/net/netstorm/boost/nursery/type/core/Types.java
@@ -1,5 +1,6 @@
 package au.net.netstorm.boost.nursery.type.core;
 
+// FIX 2328 Support "edges" interface.
 // FIX (Nov 21, 2007) 2233 Move out of nursery
 public interface Types {
     <T> T nu(Class<T> iface, Object value);
--- src/core/java/au/net/netstorm/boost/spider/instantiate/Nu.java
+++ src/core/java/au/net/netstorm/boost/spider/instantiate/Nu.java
@@ -1,5 +1,6 @@
 package au.net.netstorm.boost.spider.instantiate;
 
+// FIX 2328 Support "edges".
 public interface Nu {
     <T> T nu(Class<T> impl, Object... params);
 }
