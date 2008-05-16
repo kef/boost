1161660d2b3d4b7db4bc27b805b622334982f5db
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-05-16 03:15:00 +0000

    Reorg,
    git-svn-id: https://geekscape.org/svn/boost/trunk@3523 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/scalpel/engine/DefaultReturnEdger.java
+++ src/core/java/au/net/netstorm/boost/scalpel/engine/DefaultReturnEdger.java
@@ -1,8 +1,9 @@
 package au.net.netstorm.boost.scalpel.engine;
 
-import java.lang.reflect.Method;
 import au.net.netstorm.boost.scalpel.core.AutoEdger;
 import au.net.netstorm.boost.scalpel.core.Edge;
+
+import java.lang.reflect.Method;
 
 final class DefaultReturnEdger implements ReturnEdger {
     AutoEdger edger;
@@ -10,6 +11,12 @@ final class DefaultReturnEdger implements ReturnEdger {
     @SuppressWarnings("unchecked")
     public Object edge(Method edgeMethod, Object realReturn) {
         Class<?> realType = edgeMethod.getReturnType();
+        // FIX 2328 realType may be an "Edge" or NOT.  Both are valid.
+        // FIX 2328 if the developer (retard like me) is attempting to return
+        // FIX 2328 an edge type but has mistakenly not marked the interface
+        // FIX 2328 we early return with the real type.  However we know here
+        // FIX 2328 that a ClassCastException will occur.
+        // FIX 2328 Suggest check and barf with meaningful message.
         if (!Edge.class.isAssignableFrom(realType)) return realReturn;
         Class<Edge> edgeType = (Class<Edge>) realType;
         return edger.edge(edgeType, realReturn);
