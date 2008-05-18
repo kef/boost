e9f051000c9c821e4f6ace13dca4ad46f91e5dc1
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-05-18 22:21:44 +0000

    Story: Implement "auto edge" spike. [id=2328]
    git-svn-id: https://geekscape.org/svn/boost/trunk@3539 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/nursery/type/core/Types.java
+++ src/core/java/au/net/netstorm/boost/nursery/type/core/Types.java
@@ -2,6 +2,7 @@ package au.net.netstorm.boost.nursery.type.core;
 
 // FIX 2328 Support "edges" interface.
 // FIX (Nov 21, 2007) 2233 Move out of nursery
+// FIX 2328 this guy needs to handle the subclass instantiation gracefully so we can kill Nu (see AutoEdger#nee) 
 public interface Types {
     <T> T nu(Class<T> iface, Object value);
 
