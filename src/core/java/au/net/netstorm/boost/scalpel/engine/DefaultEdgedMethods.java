015463f49ed96f170f572400a717e9be2a061b55
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-05-18 11:54:40 +0000

    git-svn-id: https://geekscape.org/svn/boost/trunk@3530 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/scalpel/engine/DefaultEdgedMethods.java
+++ src/core/java/au/net/netstorm/boost/scalpel/engine/DefaultEdgedMethods.java
@@ -1,8 +1,14 @@
 package au.net.netstorm.boost.scalpel.engine;
 
 import java.lang.reflect.Method;
+
+import au.net.netstorm.boost.spider.flavour.StrictMap;
+import au.net.netstorm.boost.spider.flavour.DefaultStrictMap;
+
 // FIX BREADCRUMB 2328 implement me
 final class DefaultEdgedMethods implements EdgedMethods {
+    // FIX 2328 bad 
+    private final StrictMap<Method, Method> methods = new DefaultStrictMap<Method, Method>();
     public DefaultEdgedMethods(Class<?> edge, Class<?> real) {
     }
 
