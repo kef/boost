33480e9d9d59fe5f204d69f12eac4f1aeddec7d7
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-05-18 22:08:23 +0000

    git-svn-id: https://geekscape.org/svn/boost/trunk@3537 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/spider/resolve/ImplementationLookup.java
+++ src/core/java/au/net/netstorm/boost/spider/resolve/ImplementationLookup.java
@@ -1,5 +1,6 @@
 package au.net.netstorm.boost.spider.resolve;
 
+// FIX 2328 this smells bad, needs to be factory abstraction, see FIXes on Factory interface
 public interface ImplementationLookup {
     <T> Class<? extends T> getImplementation(Class<T> iface);
 }
