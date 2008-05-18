81788b2b50552c3a863fab5708d04f6c5a86f7cf
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-05-18 11:34:11 +0000

    Story: Implement "auto edge" spike. [id=2328]
    git-svn-id: https://geekscape.org/svn/boost/trunk@3528 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/sniper/lifecycle/BoostTestLifecycleBlocks.java
+++ src/test/java/au/net/netstorm/boost/sniper/lifecycle/BoostTestLifecycleBlocks.java
@@ -1,6 +1,10 @@
 package au.net.netstorm.boost.sniper.lifecycle;
 
 public class BoostTestLifecycleBlocks implements TestLifecycleBlocks {
+    // FIX 2328 this is duplicated with additions in each test type
+    // FIX 2328 really needs to be addressed now, I pushed in the
+    // FIX 2328 InitializeSubject block and it should not have to be
+    // FIX 2328 be copied everywhere
     private final Class[] pre = {
             ValidateTest.class,
             DataRegisterer.class,
