3b5352a04dfc2d0665e3505e6823628631869746
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-05-18 22:03:12 +0000

    git-svn-id: https://geekscape.org/svn/boost/trunk@3536 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/spider/registry/Factory.java
+++ src/core/java/au/net/netstorm/boost/spider/registry/Factory.java
@@ -3,6 +3,13 @@ package au.net.netstorm.boost.spider.registry;
 import au.net.netstorm.boost.spider.linkage.Linkage;
 
 public interface Factory {
+    // FIX 2328 this smells bad
+
+    // FIX 2328 1. bad name, it is not vending anything -
+    // FIX 2328 really just a lookup and should be implemented as such, i.e. 1 method find with taked next link in chain
+
+    // FIX 2328 2. there does need to be a real factory abstraction here somewhere, this interface (with BluePrint)
+    // FIX 2328 leaks like a sieve. The abstraction can't expose the implementation as there may not be one
     Blueprint get(Linkage linkage);
 
     boolean canHandle(Linkage linkage);
