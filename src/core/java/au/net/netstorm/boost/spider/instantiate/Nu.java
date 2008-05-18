91f54ed8ff637df6bb5bfc2c1ff2896fb7659421
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-05-18 22:15:44 +0000

    Story: Implement "auto edge" spike. [id=2328]
    git-svn-id: https://geekscape.org/svn/boost/trunk@3538 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/spider/instantiate/Nu.java
+++ src/core/java/au/net/netstorm/boost/spider/instantiate/Nu.java
@@ -1,6 +1,9 @@
 package au.net.netstorm.boost.spider.instantiate;
 
 // FIX 2328 Support "edges".
+
+// FIX 2328 MH i think this iface should go all together, once you start abstracting out the creation of
+// FIX 2328 MH objects properly, the logic contained by this fellow should be hidden away never to be head from
 public interface Nu {
     <T> T nu(Class<T> impl, Object... params);
 }
