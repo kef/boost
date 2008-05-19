6eb988095ded143b9a427e778c937f9f0c24dfbf
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-05-19 03:28:57 +0000

    Story: Implement "auto edge" spike. [id=2328]
    git-svn-id: https://geekscape.org/svn/boost/trunk@3548 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/DefaultInjection.java
+++
@@ -1,8 +0,0 @@
-package au.net.netstorm.boost.nursery.eight.legged.spider;
-
-// FIX BREADCRUMB 2328 driving me up
-public final class DefaultInjection implements Injection {
-    public Object apply(InjectionContext ctx, Object... args) {
-        throw new UnsupportedOperationException();
-    }
-}
