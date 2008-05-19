ea2cc4dba9c09bf58eec4d61f91e4d9d0b0e7c11
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-05-19 03:42:16 +0000

    Story: Implement "auto edge" spike. [id=2328]
    git-svn-id: https://geekscape.org/svn/boost/trunk@3552 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/provider/Provider.java
+++ src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/provider/Provider.java
@@ -2,5 +2,4 @@ package au.net.netstorm.boost.nursery.eight.legged.spider.provider;
 
 // FIX 2328 provides an instance for an injection (may itself construct injections)
 public interface Provider {
-    Object nu();
 }
