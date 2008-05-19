2f29957ea2161f96ac643bbc9d58163a2f55bc5a
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-05-19 00:37:51 +0000

    Implement "auto edge" spike. [id=2328]
    git-svn-id: https://geekscape.org/svn/boost/trunk@3543 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/spider/core/ProviderEngine.java
+++ src/core/java/au/net/netstorm/boost/spider/core/ProviderEngine.java
@@ -3,6 +3,7 @@ package au.net.netstorm.boost.spider.core;
 import au.net.netstorm.boost.gunge.type.Implementation;
 import au.net.netstorm.boost.gunge.type.ResolvedInstance;
 
+// FIX 2328 replace me - poor abstraction - needs to avoid touching the implementation
 public interface ProviderEngine {
     ResolvedInstance provide(Implementation implementation, Object[] params);
 }
