e74badebc9aba2cb5eb8e3a91ff2fd906d85b23f
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-05-20 13:33:07 +0000

    Story: Implement "auto edge" spike. [id=2328]
    git-svn-id: https://geekscape.org/svn/boost/trunk@3565 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/guts/InjectionSite.java
+++ src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/guts/InjectionSite.java
@@ -1,5 +1,9 @@
 package au.net.netstorm.boost.nursery.eight.legged.spider.guts;
 
+import au.net.netstorm.boost.spider.linkage.Linkage;
+
 // FIX 2328 encapsulate injection site info, reified type, host class, field name
 public interface InjectionSite {
+    // FIX 2328 temporary step, eventually just use injection sites where applicable
+    Linkage toLinkage();
 }
--- src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/provider/factory/BlueprintedProviderFactory.java
+++ src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/provider/factory/BlueprintedProviderFactory.java
@@ -1,15 +1,42 @@
 package au.net.netstorm.boost.nursery.eight.legged.spider.provider.factory;
 
 import au.net.netstorm.boost.nursery.eight.legged.spider.provider.core.Provider;
+import au.net.netstorm.boost.nursery.eight.legged.spider.provider.core.SingleProvider;
+import au.net.netstorm.boost.nursery.eight.legged.spider.provider.core.MultiProvider;
 import au.net.netstorm.boost.nursery.eight.legged.spider.guts.InjectionSite;
+import au.net.netstorm.boost.spider.registry.BlueprintsRead;
+import au.net.netstorm.boost.spider.registry.Blueprint;
+import au.net.netstorm.boost.spider.registry.Stamp;
+import au.net.netstorm.boost.spider.linkage.Linkage;
+import au.net.netstorm.boost.gunge.type.Implementation;
 
 // FIX 2328 just giving some concepts a name and trying to map them to existing functionality
 public final class BlueprintedProviderFactory implements ProviderFactory {
-    public Provider nu(InjectionSite site) {
-        throw new UnsupportedOperationException();
+    private final BlueprintsRead blueprints;
+
+    public BlueprintedProviderFactory(BlueprintsRead blueprints) {
+        this.blueprints = blueprints;
     }
 
     public boolean canHandle(InjectionSite site) {
-        throw new UnsupportedOperationException();
+        Linkage linkage = site.toLinkage();
+        return canHandle(linkage);
+    }
+
+    public Provider nu(InjectionSite site) {
+        Linkage linkage = site.toLinkage();
+        if (!canHandle(linkage)) throw new IllegalArgumentException("Can't handle this site.");
+        Blueprint blueprint = blueprints.get(linkage);
+        return nuProvider(blueprint);
+    }
+
+    private boolean canHandle(Linkage linkage) {
+        return blueprints.exists(linkage);
+    }
+
+    private Provider nuProvider(Blueprint blueprint) {
+        Implementation impl = blueprint.getImplementation();
+        Stamp stamp = blueprint.getStamp();
+        return stamp == Stamp.SINGLE ? new SingleProvider(impl) : new MultiProvider(impl);
     }
 }
