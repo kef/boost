1b962ce800d57022eebf87dcb6556730ef2a288b
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-05-23 00:12:38 +0000

    Story: Extensible Spider [id=2394]
    git-svn-id: https://geekscape.org/svn/boost/trunk@3583 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/spider/core/Types.java
+++ src/core/java/au/net/netstorm/boost/spider/core/Types.java
@@ -1,7 +1,6 @@
 package au.net.netstorm.boost.spider.core;
 
 // FIX 2328 Support "edges" interface.
-// FIX (Nov 21, 2007) 2233 Move out of nursery
 // FIX BREADCRUMB 2394 renaming in progress...
 public interface Types {
     <T> T nu(Class<T> iface, Object... values);
