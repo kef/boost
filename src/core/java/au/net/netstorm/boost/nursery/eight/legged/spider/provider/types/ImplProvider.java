6a67d0cda4a5bda1c5bf1639e1a7366c263dd0a7
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-05-25 22:33:26 +0000

    Story: Extensible Spider [id=2394]
    git-svn-id: https://geekscape.org/svn/boost/trunk@3591 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/provider/types/ImplProvider.java
+++ src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/provider/types/ImplProvider.java
@@ -3,6 +3,10 @@ package au.net.netstorm.boost.nursery.eight.legged.spider.provider.types;
 import au.net.netstorm.boost.gunge.type.Implementation;
 
 public final class ImplProvider implements Provider {
-    // FIX 2328 Interface?
+    // FIX 2394 Interface?
+    // FIX 2394 can't be, not everything has an interface... maybe that could be changed,
+    // FIX 2394 idea at the moment is that providers are not concerned with that resolution
+    // FIX 2394 as they are more re-usable as really dumb instantiators
+    // FIX 2394 all the smarts (and where new functionality gets plugged in) is in Factories
     public ImplProvider(Implementation impl) {}
 }
