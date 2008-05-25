2d858d7f24485bd40ce1e85290212f00aec6fc68
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-05-25 23:55:40 +0000

    Story: Extensible Spider [id=2394]
    git-svn-id: https://geekscape.org/svn/boost/trunk@3592 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/provider/types/GraphProvider.java
+++ src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/provider/types/GraphProvider.java
@@ -1,4 +1,7 @@
 package au.net.netstorm.boost.nursery.eight.legged.spider.provider.types;
 
 public final class GraphProvider implements Provider {
+    public Object nu(Object... args) {
+        throw new UnsupportedOperationException();
+    }
 }
--- src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/provider/types/ImplProvider.java
+++ src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/provider/types/ImplProvider.java
@@ -8,5 +8,12 @@ public final class ImplProvider implements Provider {
     // FIX 2394 idea at the moment is that providers are not concerned with that resolution
     // FIX 2394 as they are more re-usable as really dumb instantiators
     // FIX 2394 all the smarts (and where new functionality gets plugged in) is in Factories
-    public ImplProvider(Implementation impl) {}
+    private final Implementation impl;
+    public ImplProvider(Implementation impl) {
+        this.impl = impl;
+    }
+
+    public Object nu(Object... args) {
+        throw new UnsupportedOperationException();
+    }
 }
