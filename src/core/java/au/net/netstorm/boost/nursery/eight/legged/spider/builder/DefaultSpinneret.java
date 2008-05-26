534643777d07709b9d8ec26bb8a2a0dc1db656e8
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-05-26 01:13:55 +0000

    Story: Extensible Spider [id=2394]
    git-svn-id: https://geekscape.org/svn/boost/trunk@3597 05db6a09-9903-0410-bedf-ba9bc4063e48

---
+++ src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/builder/DefaultSpinneret.java
@@ -0,0 +1,10 @@
+package au.net.netstorm.boost.nursery.eight.legged.spider.builder;
+
+import au.net.netstorm.boost.nursery.eight.legged.spider.web.Web;
+import au.net.netstorm.boost.nursery.eight.legged.spider.orbs.FactoryOrb;
+
+public final class DefaultSpinneret implements Spinneret {
+    public Web spin(FactoryOrb... factories) {
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ src/test/java/au/net/netstorm/boost/nursery/eight/legged/spider/builder/DefaultSpinneretAtomicTest.java
@@ -0,0 +1,22 @@
+package au.net.netstorm.boost.nursery.eight.legged.spider.builder;
+
+import au.net.netstorm.boost.sniper.core.LifecycleTestCase;
+import au.net.netstorm.boost.sniper.marker.HasFixtures;
+import au.net.netstorm.boost.sniper.marker.InjectableTest;
+import au.net.netstorm.boost.sniper.marker.LazyFields;
+
+public final class DefaultSpinneretAtomicTest extends LifecycleTestCase implements HasFixtures, InjectableTest, LazyFields {
+    private Spinneret subject;
+
+    public void setUpFixtures() {
+        subject = new DefaultSpinneret();
+    }
+
+    // FIX BREADCRUMB 2394 driving up builder
+    public void testSpin() {
+        try {
+            subject.spin();
+            fail();
+        } catch (UnsupportedOperationException expected) {}
+    }
+}
