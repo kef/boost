51ae4f0f29a5b7a576828423ed2f695c6013a692
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-05-26 00:27:10 +0000

    Story: Extensible Spider [id=2394]
    git-svn-id: https://geekscape.org/svn/boost/trunk@3594 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/provider/types/InstanceProvider.java
+++ src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/provider/types/InstanceProvider.java
@@ -1,7 +1,14 @@
 package au.net.netstorm.boost.nursery.eight.legged.spider.provider.types;
 
 public final class InstanceProvider implements Provider {
+    private final Object instance;
+
+    public InstanceProvider(Object instance) {
+        this.instance = instance;
+    }
+
     public Object nu(Object... args) {
-        throw new UnsupportedOperationException();
+        if (args.length != 0) throw new IllegalArgumentException("Instance provider does not support arguments.");
+        return instance;
     }
 }
---
+++ src/test/java/au/net/netstorm/boost/nursery/eight/legged/spider/provider/types/InstanceProviderAtomicTest.java
@@ -0,0 +1,28 @@
+package au.net.netstorm.boost.nursery.eight.legged.spider.provider.types;
+
+import au.net.netstorm.boost.sniper.core.LifecycleTestCase;
+import au.net.netstorm.boost.sniper.marker.HasFixtures;
+import au.net.netstorm.boost.sniper.marker.InjectableTest;
+import au.net.netstorm.boost.sniper.marker.LazyFields;
+
+public final class InstanceProviderAtomicTest extends LifecycleTestCase implements HasFixtures, InjectableTest, LazyFields {
+    private Provider subject;
+    private Object expected;
+
+    public void setUpFixtures() {
+        expected = new Object();
+        subject = new InstanceProvider(expected);
+    }
+
+    public void testNu() {
+        Object result = subject.nu();
+        assertSame(expected, result);
+    }
+
+    public void testNuFailure() {
+        try {
+            subject.nu("any arg");
+            fail();
+        } catch (IllegalArgumentException expected) {}
+    }
+}
