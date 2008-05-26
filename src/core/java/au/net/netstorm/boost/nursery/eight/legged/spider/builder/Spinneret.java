e03a4abd011224b964475bfb351dad83f029c3e0
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-05-26 08:06:12 +0000

    Story: Extensible Spider [id=2394]
    git-svn-id: https://geekscape.org/svn/boost/trunk@3613 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/builder/Spinneret.java
+++ src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/builder/Spinneret.java
@@ -3,7 +3,6 @@ package au.net.netstorm.boost.nursery.eight.legged.spider.builder;
 import au.net.netstorm.boost.nursery.eight.legged.spider.web.Web;
 import au.net.netstorm.boost.nursery.eight.legged.spider.config.WebConfig;
 
-// FIX 2394 spins webs
 public interface Spinneret {
     Web spin(WebConfig... configs);
 }
