28fef11ade72fc3dae3372280c5f56473b6f142f
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-05-26 01:25:06 +0000

    Story: Extensible Spider [id=2394]
    git-svn-id: https://geekscape.org/svn/boost/trunk@3598 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/web/Web.java
+++ src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/web/Web.java
@@ -1,8 +1,11 @@
 package au.net.netstorm.boost.nursery.eight.legged.spider.web;
 
 import au.net.netstorm.boost.nursery.eight.legged.spider.orbs.RuleOrb;
+import au.net.netstorm.boost.nursery.eight.legged.spider.orbs.FactoryOrb;
 
 // FIX 2394 stateful web instance - a central state holder, rather than a buch of disparate maps
 public interface Web {
     void register(RuleOrb... rules);
+    void register(FactoryOrb... rules);
+    Web spawn();
 }
