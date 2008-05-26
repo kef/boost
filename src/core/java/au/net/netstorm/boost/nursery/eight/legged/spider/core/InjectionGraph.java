2ea9b409b7f249c76525be0ea82acb602592dca7
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-05-26 10:14:37 +0000

    Story: Extensible Spider [id=2394]
    git-svn-id: https://geekscape.org/svn/boost/trunk@3622 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/core/InjectionGraph.java
+++ src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/core/InjectionGraph.java
@@ -1,6 +1,5 @@
 package au.net.netstorm.boost.nursery.eight.legged.spider.core;
 
-// FIX 2394 type safe wrapper for an injection graph
 public interface InjectionGraph<T> {
     T apply(Object... args);
 }
