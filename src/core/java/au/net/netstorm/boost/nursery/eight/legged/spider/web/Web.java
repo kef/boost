fe2e72f87257ca117033458bb135cc37f3fd0c74
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-05-27 00:42:10 +0000

    Story: Extensible Spider [id=2394]
    git-svn-id: https://geekscape.org/svn/boost/trunk@3627 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/web/Web.java
+++ src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/web/Web.java
@@ -5,7 +5,8 @@ import au.net.netstorm.boost.nursery.eight.legged.spider.config.RuleConfig;
 
 // FIX 2394 stateful web instance - a central state holder, rather than a bunch of disparate maps
 public interface Web {
+    void register(RuleConfig ruleConfig);
+    // FIX 2394 assuming that these are wildcard factories only, other factories set by rules
     void register(Class<? extends Factory> type);
     void register(Factory factory);
-    void register(RuleConfig ruleConfig);
 }
