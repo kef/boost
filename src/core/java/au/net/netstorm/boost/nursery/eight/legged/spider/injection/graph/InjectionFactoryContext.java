8d7b650ac8f7f2d8791a9a95f01e84c5be2ded27
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-05-28 10:13:20 +0000

    Story: Extensible Spider [id=2394]
    git-svn-id: https://geekscape.org/svn/boost/trunk@3668 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/injection/graph/InjectionFactoryContext.java
+++
@@ -1,8 +0,0 @@
-package au.net.netstorm.boost.nursery.eight.legged.spider.injection.graph;
-
-// FIX 2328 state used to build injection graph
-// FIX 2328   rule: ensure one injection per injection graph
-// FIX 2328         (can still allow multiple instances per object graph, depends on provider implementation)
-public interface InjectionFactoryContext {
-
-}
