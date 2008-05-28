d9f1dba19af2423a4722473f9c4b8a196de3e038
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-05-28 13:29:10 +0000

    Story: Extensible Spider [id=2394]
    git-svn-id: https://geekscape.org/svn/boost/trunk@3674 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/injection/graph/DefaultGraphBuilder.java
+++ src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/injection/graph/DefaultGraphBuilder.java
@@ -2,22 +2,20 @@ package au.net.netstorm.boost.nursery.eight.legged.spider.injection.graph;
 
 import au.net.netstorm.boost.nursery.eight.legged.spider.web.InjectionWeb;
 import au.net.netstorm.boost.nursery.eight.legged.spider.injection.sites.InjectionSite;
-import au.net.netstorm.boost.nursery.eight.legged.spider.injection.sites.DefaultInjectionSite;
+import au.net.netstorm.boost.nursery.eight.legged.spider.injection.sites.InjectionSiteBuilder;
+import au.net.netstorm.boost.nursery.eight.legged.spider.injection.sites.DefaultInjectionSiteBuilder;
 import au.net.netstorm.boost.nursery.eight.legged.spider.injection.types.InjectionType;
 
 public final class DefaultGraphBuilder implements GraphBuilder {
-    private final InjectionWeb context;
+    private final InjectionSiteBuilder builder = new DefaultInjectionSiteBuilder();
+    private final InjectionWeb web;
 
-    public DefaultGraphBuilder(InjectionWeb context) {
-        this.context = context;
+    public DefaultGraphBuilder(InjectionWeb web) {
+        this.web = web;
     }
 
     public Injection build(InjectionType type) {
-        // FIX 2394 arbitraty values at the moment, warrants a special class
-        InjectionSite site = new DefaultInjectionSite(GraphBuilder.class, type, "root");
-
-
-        // FIX 2394 implement me
-        return null;
+        InjectionSite site = builder.build(type);
+        return web.injection(site);
     }
 }
--- src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/web/Web.java
+++ src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/web/Web.java
@@ -6,7 +6,7 @@ import au.net.netstorm.boost.nursery.eight.legged.spider.config.RuleConfig;
 // FIX 2394 stateful web instance - a central state holder, rather than a bunch of disparate maps
 public interface Web {
     void register(RuleConfig ruleConfig);
-    // FIX 2394 conveniance methods only
+    // FIX 2394 conveniance methods only, may not be needed if rules are easy
     void register(Class<? extends Factory> type);
     void register(Factory factory);
 }
