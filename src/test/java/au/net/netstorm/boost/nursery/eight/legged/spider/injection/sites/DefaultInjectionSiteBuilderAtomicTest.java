32596d4e5c3972fff5ef65be301ae1e60a21891a
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-05-28 03:59:37 +0000

    Story: Extensible Spider [id=2394]
    git-svn-id: https://geekscape.org/svn/boost/trunk@3658 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/nursery/eight/legged/spider/injection/sites/DefaultInjectionSiteBuilderAtomicTest.java
+++ src/test/java/au/net/netstorm/boost/nursery/eight/legged/spider/injection/sites/DefaultInjectionSiteBuilderAtomicTest.java
@@ -4,16 +4,24 @@ import au.net.netstorm.boost.sniper.core.LifecycleTestCase;
 import au.net.netstorm.boost.sniper.marker.HasFixtures;
 import au.net.netstorm.boost.sniper.marker.InjectableTest;
 import au.net.netstorm.boost.sniper.marker.LazyFields;
+import au.net.netstorm.boost.sniper.reflect.util.FieldTestUtil;
+import au.net.netstorm.boost.nursery.eight.legged.spider.injection.types.InjectionType;
 
 public final class DefaultInjectionSiteBuilderAtomicTest extends LifecycleTestCase implements HasFixtures, InjectableTest, LazyFields {
     private InjectionSiteBuilder subject;
+    FieldTestUtil fielder;
+    InjectionSiteChecker checker;
+    InjectionType typeMock;
 
     public void setUpFixtures() {
         subject = new DefaultInjectionSiteBuilder();
     }
 
     public void testBuildField() {
-        // FIX 2394 drive me up
+        // FIX BREADCRUMB 2394 aaaaaaaaa building this up
+//        Field field = fielder.get(Dummy.class, "x");
+//        InjectionSite result = subject.build(field);
+//        checker.checkSite(result, Dummy.class, typeMock, "x");
     }
 
     public void testBuildConstructor() {
--- src/test/java/au/net/netstorm/boost/nursery/eight/legged/spider/injection/testdata/Dummy.java
+++ src/test/java/au/net/netstorm/boost/nursery/eight/legged/spider/injection/testdata/Dummy.java
@@ -2,5 +2,6 @@ package au.net.netstorm.boost.nursery.eight.legged.spider.injection.testdata;
 
 public class Dummy {
     public String x;
+    public Dummy() {}
     public Dummy(String x) { this.x = x; }
 }
