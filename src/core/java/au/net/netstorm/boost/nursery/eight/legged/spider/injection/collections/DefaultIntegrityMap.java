d2036a3326eba98abad33635ed521c96d3149d18
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-05-28 13:29:26 +0000

    Story: Extensible Spider [id=2394]
    git-svn-id: https://geekscape.org/svn/boost/trunk@3675 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/injection/collections/DefaultIntegrityMap.java
+++ src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/injection/collections/DefaultIntegrityMap.java
@@ -11,9 +11,6 @@ public final class DefaultIntegrityMap<K, V> implements IntegrityMap<K, V> {
         if (cached != null) return cached;
         V newy = creator.create(key);
         delegate.putIfAbsent(key, newy);
-        // FIX 2394 this is a bit ordinary, could avoid this lookup, but...
-        // FIX 2394 it would be impossible to test, in fact it would not be
-        // FIX 2394 executes on some platforms
         return delegate.get(key);
     }
 }
