02555497ec27019f6c55e56b95ef75b4b06f99b1
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-05-28 22:12:02 +0000

    Story: Extensible Spider [id=2394]
    git-svn-id: https://geekscape.org/svn/boost/trunk@3680 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/injection/graph/GraphBuilder.java
+++ src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/injection/graph/GraphBuilder.java
@@ -2,7 +2,6 @@ package au.net.netstorm.boost.nursery.eight.legged.spider.injection.graph;
 
 import au.net.netstorm.boost.nursery.eight.legged.spider.injection.types.InjectionType;
 
-// FIX 2328 creates an injection for an injection-site
 public interface GraphBuilder {
     // FIX 2394 root arg could be dropped if injection type was parameterized
     <T> InjectionGraph<T> build(Class<T> root, InjectionType type, Object... args);
