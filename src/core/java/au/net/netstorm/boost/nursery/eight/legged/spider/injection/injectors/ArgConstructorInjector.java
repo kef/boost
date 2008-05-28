c54f5a8fb50667d45873bc6bb589e61c7b07881d
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-05-28 12:29:36 +0000

    Story: Extensible Spider [id=2394]
    git-svn-id: https://geekscape.org/svn/boost/trunk@3671 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/injection/injectors/ArgConstructorInjector.java
+++ src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/injection/injectors/ArgConstructorInjector.java
@@ -2,6 +2,7 @@ package au.net.netstorm.boost.nursery.eight.legged.spider.injection.injectors;
 
 import au.net.netstorm.boost.nursery.eight.legged.spider.provider.types.Provider;
 
+// FIX 2394 to be used by parameterized injection aka nuer
 public final class ArgConstructorInjector implements ConstructorInjector {
     private final Provider provider;
     private final Object[] args;
