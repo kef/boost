0fc53b621504a1a6980b50177cd97059475d6a7b
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-05-28 22:49:27 +0000

    Story: Extensible Spider [id=2394]
    git-svn-id: https://geekscape.org/svn/boost/trunk@3683 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/injection/graph/DefaultInjection.java
+++ src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/injection/graph/DefaultInjection.java
@@ -2,14 +2,22 @@ package au.net.netstorm.boost.nursery.eight.legged.spider.injection.graph;
 
 import au.net.netstorm.boost.nursery.eight.legged.spider.injection.sites.InjectionSite;
 import au.net.netstorm.boost.nursery.eight.legged.spider.injection.types.InjectionType;
+import au.net.netstorm.boost.nursery.eight.legged.spider.injection.injectors.ConstructorInjector;
+import au.net.netstorm.boost.nursery.eight.legged.spider.injection.injectors.InjectorFactory;
+import au.net.netstorm.boost.nursery.eight.legged.spider.injection.injectors.ConstuctorInjectorFactory;
 import au.net.netstorm.boost.nursery.eight.legged.spider.web.InjectionWeb;
 
 // FIX 2394 this is a beast, maybe split into pre and post injection phases???
 public final class DefaultInjection implements PhasedInjection {
+    // FIX 2394 these two injector factories should be passed in
+    private final InjectorFactory<ConstructorInjector> ctorFactory = new ConstuctorInjectorFactory();
+//    private final InjectorFactory<MemberInjection> memberFactory = new FieldInjectorFactory();
     private final InjectionSite site;
+    // FIX 2394 dissapear this field;
     private final InjectionType type;
-    private ConstructingInjection constructor;
+    private ConstructorInjector constructor;
     private MemberInjection members;
+
 
     public DefaultInjection(InjectionSite site) {
         this.site = site;
@@ -18,13 +26,13 @@ public final class DefaultInjection implements PhasedInjection {
 
     public void build(InjectionWeb web) {
         // FIX 2394 make these guys stateless factories
-        constructor = new DefaultConstructingInjection(web, site, type);
+        constructor = ctorFactory.nu(web, site);
         members = new DefaultMemberInjection(web, type);
     }
 
     public Object apply() {
         // FIX 2394 would this be simpler if there was a resolution queue rather than a graph
-        Object ref = constructor.apply();
+        Object ref = constructor.inject();
         members.apply(ref);
         return ref;
     }
--- src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/injection/injectors/ConstuctorInjectorFactory.java
+++ src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/injection/injectors/ConstuctorInjectorFactory.java
@@ -1,10 +1,31 @@
 package au.net.netstorm.boost.nursery.eight.legged.spider.injection.injectors;
 
+import java.lang.reflect.Constructor;
+
 import au.net.netstorm.boost.nursery.eight.legged.spider.web.InjectionWeb;
 import au.net.netstorm.boost.nursery.eight.legged.spider.injection.sites.InjectionSite;
+import au.net.netstorm.boost.nursery.eight.legged.spider.injection.sites.InjectionSiteBuilder;
+import au.net.netstorm.boost.nursery.eight.legged.spider.injection.sites.DefaultInjectionSiteBuilder;
+import au.net.netstorm.boost.nursery.eight.legged.spider.injection.graph.Injection;
+import au.net.netstorm.boost.nursery.eight.legged.spider.injection.types.InjectionType;
+import au.net.netstorm.boost.nursery.eight.legged.spider.provider.types.Provider;
 
 public final class ConstuctorInjectorFactory implements InjectorFactory<ConstructorInjector> {
+    private final InjectionSiteBuilder siteBuilder = new DefaultInjectionSiteBuilder();
+
     public ConstructorInjector nu(InjectionWeb web, InjectionSite site) {
-        throw new UnsupportedOperationException();
+        InjectionType type = site.type();
+        Provider provider = web.provider(site);
+        Constructor<?> ctor = type.getConstructor();
+        InjectionSite[] sites = siteBuilder.build(ctor);
+        return nuConstuctor(web, provider, sites);
+    }
+
+    private ConstructorInjector nuConstuctor(InjectionWeb web, Provider provider, InjectionSite[] sites) {
+        Injection[] injections = new Injection[sites.length];
+        for (int i = 0; i < sites.length; ++i) {
+            injections[i] = web.injection(sites[i]);
+        }
+        return new DefaultConstructorInjector(provider, injections);
     }
 }
