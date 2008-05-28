91018562d4732e723f1c98a2f3677b0d4f051c7a
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-05-28 09:53:04 +0000

    Story: Extensible Spider [id=2394]
    git-svn-id: https://geekscape.org/svn/boost/trunk@3666 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/injection/types/DefaultInjectionTypeBuilder.java
+++ src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/injection/types/DefaultInjectionTypeBuilder.java
@@ -1,9 +1,26 @@
 package au.net.netstorm.boost.nursery.eight.legged.spider.injection.types;
 
 import java.lang.reflect.Type;
+import java.lang.reflect.ParameterizedType;
 
 public final class DefaultInjectionTypeBuilder implements InjectionTypeBuilder {
     public InjectionType build(Type type) {
-        throw new UnsupportedOperationException();
+        if (type instanceof Class) return raw(type);
+        if (type instanceof ParameterizedType) return paramertized(type);
+        throw new IllegalArgumentException(
+                "Type not supported. Declarations, Wildcards and Arrays can not be injected," + 
+                "must be one of [Class, ParameterizedType]");
+    }
+
+    private InjectionType raw(Type type) {
+        Class<?> raw = (Class<?>) type;
+        return new DefaultInjectionType(raw);
+    }
+
+    // FIX 2394 add support for paramertized types
+    private InjectionType paramertized(Type type) {
+        ParameterizedType paramertized = (ParameterizedType) type;
+        Type raw = paramertized.getRawType();
+        return raw(raw);
     }
 }
