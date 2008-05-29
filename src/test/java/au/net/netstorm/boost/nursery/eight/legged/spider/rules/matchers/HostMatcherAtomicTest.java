0005a828bc5e52b6d40e8163478aef9f6e7de029
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-05-29 11:18:04 +0000

    Story: Extensible Spider [id=2394]
    git-svn-id: https://geekscape.org/svn/boost/trunk@3706 05db6a09-9903-0410-bedf-ba9bc4063e48

---
+++ src/test/java/au/net/netstorm/boost/nursery/eight/legged/spider/rules/matchers/HostMatcherAtomicTest.java
@@ -0,0 +1,28 @@
+package au.net.netstorm.boost.nursery.eight.legged.spider.rules.matchers;
+
+import au.net.netstorm.boost.sniper.core.LifecycleTestCase;
+import au.net.netstorm.boost.sniper.marker.HasFixtures;
+import au.net.netstorm.boost.sniper.marker.InjectableTest;
+import au.net.netstorm.boost.sniper.marker.LazyFields;
+import au.net.netstorm.boost.nursery.eight.legged.spider.injection.sites.InjectionSite;
+import au.net.netstorm.boost.nursery.eight.legged.spider.rules.declaration.TreeHolder;
+
+public final class HostMatcherAtomicTest extends LifecycleTestCase implements HasFixtures, InjectableTest, LazyFields {
+    private Matcher subject;
+    MatcherChecker checker;
+    InjectionSite siteMock;
+
+    public void setUpFixtures() {
+        subject = new HostMatcher(TreeHolder.class);
+    }
+
+    public void testAccept() {
+        expect.oneCall(siteMock, TreeHolder.class, "host");
+        checker.checkAccept(subject, true, siteMock);
+    }
+
+    public void testDoNotAccept() {
+        expect.oneCall(siteMock, String.class, "host");
+        checker.checkAccept(subject, false, siteMock);
+    }
+}
