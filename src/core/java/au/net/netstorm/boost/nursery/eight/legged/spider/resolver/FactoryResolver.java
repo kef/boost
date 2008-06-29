e285932b6573a70241f2fd128b7ee148aec5fb6b
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-06-29 13:55:52 +0000

    2394
    git-svn-id: https://geekscape.org/svn/boost/trunk@3736 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/resolver/FactoryResolver.java
+++
@@ -1,8 +0,0 @@
-package au.net.netstorm.boost.nursery.eight.legged.spider.resolver;
-
-import au.net.netstorm.boost.nursery.eight.legged.spider.injection.sites.InjectionSite;
-import au.net.netstorm.boost.nursery.eight.legged.spider.factory.core.Factory;
-
-public interface FactoryResolver {
-    Factory resolve(InjectionSite site);
-}
