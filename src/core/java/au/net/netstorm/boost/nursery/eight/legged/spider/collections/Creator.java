25d35e32bab03204d80d960a36fd7ea5fcaef842
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-06-29 13:41:24 +0000

    2394
    git-svn-id: https://geekscape.org/svn/boost/trunk@3729 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/collections/Creator.java
+++
@@ -1,5 +0,0 @@
-package au.net.netstorm.boost.nursery.eight.legged.spider.collections;
-
-public interface Creator<K, V> {
-    V create(K key);
-}
--- src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/collections/IntegrityMap.java
+++
@@ -1,5 +0,0 @@
-package au.net.netstorm.boost.nursery.eight.legged.spider.collections;
-
-public interface IntegrityMap<K, V> {
-    V getOrCreate(K key, Creator<K, V> creator);
-}
