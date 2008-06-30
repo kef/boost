2c0852b3ebcc40e8c1aa3c5ac95a30764a6ce72c
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-06-30 14:28:03 +0000

    2394
    git-svn-id: https://geekscape.org/svn/boost/trunk@3751 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/core/RuleConfig.java
+++
@@ -1,7 +0,0 @@
-package au.net.netstorm.boost.nursery.eight.legged.spider.core;
-
-import au.net.netstorm.boost.nursery.eight.legged.spider.bindings.binder.Binder;
-
-public interface RuleConfig {
-    void apply(Binder binder);
-}
