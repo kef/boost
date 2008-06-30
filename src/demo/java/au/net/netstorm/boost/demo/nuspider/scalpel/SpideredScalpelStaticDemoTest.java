4bb87f3d60a08bb48ebe68eb7f4a2eabda757c8e
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-06-30 13:32:08 +0000

    2394
    git-svn-id: https://geekscape.org/svn/boost/trunk@3745 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/demo/java/au/net/netstorm/boost/demo/nuspider/scalpel/SpideredScalpelStaticDemoTest.java
+++ src/demo/java/au/net/netstorm/boost/demo/nuspider/scalpel/SpideredScalpelStaticDemoTest.java
@@ -7,20 +7,18 @@ import demo.edge.java.lang.Class;
 import demo.edge.java.lang.ClassStatic;
 
 public final class SpideredScalpelStaticDemoTest extends NuSpiderDemooooTest implements InjectableTest {
-   NuImpl nuImpl;
+    NuImpl nu;
 
-    // FIX 2394 this is one ugly demo test
-   public void testInjectStaticEdge() {
-       HasStaticEdge injectable = nuImpl.nu(HasStaticEdge.class);
-       ClassStatic classer = injectable.classer;
-       String name = SomeClass.class.getName();
-       Object expected = new SomeClass();
-       checkClassEdge(classer, name, expected);
-   }
+    public void testInjectStaticEdge() {
+        HasStaticEdge injectable = nu.nu(HasStaticEdge.class);
+        ClassStatic classer = injectable.classer;
+        checkClassStatic(classer);
+    }
 
-    private void checkClassEdge(ClassStatic classer, String name, Object expected) {
-        Class cls = classer.forName(name);
+    private void checkClassStatic(ClassStatic classer) {
+        Class cls = classer.forName("au.net.netstorm.boost.demo.nuspider.scalpel.SomeClass");
         Object actual = cls.newInstance();
+        Object expected = new SomeClass();
         assertEquals(expected, actual);
     }
 }
