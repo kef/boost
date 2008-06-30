2fd195aa72ce297e51846cd41203272825937508
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-06-30 14:36:16 +0000

    2394
    git-svn-id: https://geekscape.org/svn/boost/trunk@3756 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/bindings/binder/SingleMaker.java
+++ src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/bindings/binder/SingleMaker.java
@@ -1,7 +1,6 @@
 package au.net.netstorm.boost.nursery.eight.legged.spider.bindings.binder;
 
-// FIX 2394 not convinced about this guy, could be made part of the target
-// FIX 2394 on the other hand it should be rarely used
+// FIX 2394 kill me
 public interface SingleMaker {
     void asSingle();
 }
--- src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/bindings/binder/Target.java
+++ src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/bindings/binder/Target.java
@@ -3,6 +3,7 @@ package au.net.netstorm.boost.nursery.eight.legged.spider.bindings.binder;
 import au.net.netstorm.boost.nursery.eight.legged.spider.factory.core.Factory;
 import au.net.netstorm.boost.nursery.eight.legged.spider.provider.types.Provider;
 
+// FIX 2394 address multiplicity here instead of SingleMaker
 public interface Target {
     SingleMaker to(Object instance);
     SingleMaker to(Class<?> type);
