73136f14476ecba5898fb67e14ae2cb1a2a42ff5
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-06-30 13:42:50 +0000

    2394
    git-svn-id: https://geekscape.org/svn/boost/trunk@3748 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/nursery/eight/legged/spider/core/DefaultBackReference.java
+++
@@ -1,9 +0,0 @@
-package au.net.netstorm.boost.nursery.eight.legged.spider.core;
-
-public final class DefaultBackReference implements BackReference {
-    Cyclic cyclic;
-
-    public Cyclic get() {
-        return cyclic;
-    }
-}
--- src/test/java/au/net/netstorm/boost/nursery/eight/legged/spider/core/DefaultCyclic.java
+++
@@ -1,9 +0,0 @@
-package au.net.netstorm.boost.nursery.eight.legged.spider.core;
-
-public final class DefaultCyclic implements Cyclic {
-    BackReference link;
-
-    public BackReference get() {
-        return link;
-    }
-}
