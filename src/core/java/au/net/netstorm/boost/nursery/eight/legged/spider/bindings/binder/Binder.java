6f70474117e0676469aeeb7ac27d1812119f0351
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-07-06 07:37:10 +0000

    2394
    git-svn-id: https://geekscape.org/svn/boost/trunk@3766 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/bindings/binder/Binder.java
+++ src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/bindings/binder/Binder.java
@@ -2,6 +2,7 @@ package au.net.netstorm.boost.nursery.eight.legged.spider.bindings.binder;
 
 import au.net.netstorm.boost.nursery.eight.legged.spider.injection.types.InjectionType;
 
+// FIX 2394 Genericise the Target so we get strong typing across into the resolution (target methods)?
 public interface Binder {
     Target bind(Class<?> iface, Class<?> host, String name);
     Target bind(Class<?> iface, Class<?> host);
