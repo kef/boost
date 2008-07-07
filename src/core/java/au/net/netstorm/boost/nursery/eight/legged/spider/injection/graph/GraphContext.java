7da24384dbe9fce272e211a8e8a355d42d1a8500
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-07-07 11:21:28 +0000

    2394
    git-svn-id: https://geekscape.org/svn/boost/trunk@3789 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/injection/graph/GraphContext.java
+++ src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/injection/graph/GraphContext.java
@@ -1,6 +1,14 @@
 package au.net.netstorm.boost.nursery.eight.legged.spider.injection.graph;
 
+import au.net.netstorm.boost.nursery.eight.legged.spider.provider.Provider;
+import au.net.netstorm.boost.nursery.eight.legged.spider.injection.injectors.MemberInjector;
+
 // FIX 2394 use or lose.
 // FIX BREADCRUMB 2394 building up a cleaner graph construction lifecycle: build graph/instantiate/resolve/post-process
+// FIX 2394 acts as a future for each stage to publish to, and the next phase can grap from it
 public interface GraphContext {
+    void instantiate(Injection injection, Provider provider);
+    // FIX 2394 for updated approach MemberInjector is now a bad name
+    void resolve(Injection injection, MemberInjector resolver);
+    void post(Injection injection, PostProcess proccess);
 }
---
+++ src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/injection/graph/PostProcess.java
@@ -0,0 +1,6 @@
+package au.net.netstorm.boost.nursery.eight.legged.spider.injection.graph;
+
+// FIX 2394 use or lose.
+// FIX 2394 holder for all post process tasks, such as lifecycle/construction & aspecterizing
+public interface PostProcess {
+}
