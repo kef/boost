3dd8d1db14c5afb04d7163428ebac96a8930b662
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-07-07 10:57:06 +0000

    iws
    git-svn-id: https://geekscape.org/svn/boost/trunk@3786 05db6a09-9903-0410-bedf-ba9bc4063e48

---
+++ src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/injection/graph/GraphContext.java
@@ -0,0 +1,6 @@
+package au.net.netstorm.boost.nursery.eight.legged.spider.injection.graph;
+
+// FIX 2394 use or lose.
+// FIX BREADCRUMB 2394 building up a cleaner graph construction lifecycle: build graph/instantiate/resolve/post-process
+public interface GraphContext {
+}
