73c55ce64d332afe4299e525214899c44029243e
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-07-07 11:33:39 +0000

    2394
    git-svn-id: https://geekscape.org/svn/boost/trunk@3794 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/injection/injectors/ConstructorInjector.java
+++ src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/injection/injectors/ConstructorInjector.java
@@ -2,7 +2,6 @@ package au.net.netstorm.boost.nursery.eight.legged.spider.injection.injectors;
 
 import au.net.netstorm.boost.nursery.eight.legged.spider.injection.graph.InjectionContext;
 
-// FIX 2394 should actually be behind a provider, this guys leaks everywhere without even trying
 public interface ConstructorInjector {
     Object inject(InjectionContext context);
 }
