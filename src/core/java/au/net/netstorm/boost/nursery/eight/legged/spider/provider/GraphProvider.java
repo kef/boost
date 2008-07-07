50f914f5afb593443bc84027cc435f036d867f21
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-07-07 11:31:10 +0000

    2394
    git-svn-id: https://geekscape.org/svn/boost/trunk@3793 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/provider/GraphProvider.java
+++ src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/provider/GraphProvider.java
@@ -1,5 +1,7 @@
 package au.net.netstorm.boost.nursery.eight.legged.spider.provider;
 
+// FIX 2394 just a name for a concept.
+// FIX 2394 use or lose.
 public final class GraphProvider implements Provider {
     public Object nu(Object... args) {
         throw new UnsupportedOperationException();
