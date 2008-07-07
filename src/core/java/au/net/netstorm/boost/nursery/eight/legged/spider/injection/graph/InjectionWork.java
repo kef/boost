90165001e15fab1c9ad17821a41bc70357879cb1
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-07-07 11:29:16 +0000

    2394
    git-svn-id: https://geekscape.org/svn/boost/trunk@3791 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/injection/graph/InjectionWork.java
+++ src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/injection/graph/InjectionWork.java
@@ -6,7 +6,7 @@ import au.net.netstorm.boost.nursery.eight.legged.spider.injection.injectors.Mem
 // FIX 2394 use or lose.
 // FIX BREADCRUMB 2394 building up a cleaner graph construction lifecycle: build graph/instantiate/resolve/post-process
 // FIX 2394 acts as a future for each stage to publish to, and the next phase can grap from it
-public interface GraphContext {
+public interface InjectionWork {
     void instantiate(Injection injection, Provider provider);
     // FIX 2394 for updated approach MemberInjector is now a bad name
     void resolve(Injection injection, MemberInjector resolver);
