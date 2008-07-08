033fec48e5d5af8a08430be5858c5231f2f58184
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-07-08 01:52:36 +0000

    2394
    git-svn-id: https://geekscape.org/svn/boost/trunk@3811 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/gunge/collection/IntegrityMap.java
+++ src/core/java/au/net/netstorm/boost/gunge/collection/IntegrityMap.java
@@ -2,12 +2,14 @@ package au.net.netstorm.boost.gunge.collection;
 
 import java.util.Set;
 
-// FIX 2394 this guy might serve as a replacement for strict map
+// FIX 2394 this guy might serve as a replacement for strict map - elliminates dirty exists() method
+// FIX 2394 for general purpose use, might wan't a concurrent and non-concurrent implementation
 public interface IntegrityMap<K, V> {
     V get(K key, Creator<K, V> creator);
 
-    // FIX 2394 name. could be just get.
     V get(K key);
 
+    // FIX 2394 can this class just be made iterable, only ever allow iteration over keys
+    // FIX 2394 force use of different datastructure if you are trying to iterate over values
     Set<K> keySet();
 }
