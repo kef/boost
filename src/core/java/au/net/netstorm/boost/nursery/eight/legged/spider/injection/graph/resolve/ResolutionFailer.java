148f89c75f9bfe5c4f75d0533f4f492d05f38f49
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-07-21 14:06:40 +0000

    2394
    git-svn-id: https://geekscape.org/svn/boost/trunk@3874 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/injection/graph/resolve/ResolutionFailer.java
+++ src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/injection/graph/resolve/ResolutionFailer.java
@@ -2,7 +2,7 @@ package au.net.netstorm.boost.nursery.eight.legged.spider.injection.graph.resolv
 
 import au.net.netstorm.boost.gunge.collection.Failer;
 import au.net.netstorm.boost.nursery.eight.legged.spider.injection.sites.InjectionSite;
-import au.net.netstorm.boost.nursery.eight.legged.spider.injection.graph.core.CannotProvideException;
+import au.net.netstorm.boost.nursery.eight.legged.spider.injection.graph.resolve.CannotProvideException;
 
 public final class ResolutionFailer implements Failer<InjectionSite> {
     public void fail(InjectionSite site) {
