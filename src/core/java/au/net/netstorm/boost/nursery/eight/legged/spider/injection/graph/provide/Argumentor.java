2de760e46010d74f635a84675b99c12c7d04beff
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-07-22 08:07:06 +0000

    2394
    git-svn-id: https://geekscape.org/svn/boost/trunk@3897 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/injection/graph/provide/Argumentor.java
+++
@@ -1,9 +0,0 @@
-package au.net.netstorm.boost.nursery.eight.legged.spider.injection.graph.provide;
-
-import au.net.netstorm.boost.nursery.eight.legged.spider.injection.sites.InjectionSite;
-import au.net.netstorm.boost.nursery.eight.legged.spider.provider.Provider;
-
-// FIX 2394 delete.
-public interface Argumentor {
-    void providers(Providers providers, Provider root, InjectionSite site, Object[] args);
-}
