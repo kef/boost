9f2f30f652fdf761856510d50389892f96fd5fbc
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-07-22 12:24:38 +0000

    2394
    git-svn-id: https://geekscape.org/svn/boost/trunk@3919 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/aspects/resolver/AspectResolver.java
+++ src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/aspects/resolver/AspectResolver.java
@@ -2,7 +2,6 @@ package au.net.netstorm.boost.nursery.eight.legged.spider.aspects.resolver;
 
 import au.net.netstorm.boost.nursery.eight.legged.spider.aspects.core.AspectSpec;
 
-// FIX 2394 Use or lose. Wire into PostProcessor.
 public interface AspectResolver {
     AspectSpec resolve(Object o);
 }
