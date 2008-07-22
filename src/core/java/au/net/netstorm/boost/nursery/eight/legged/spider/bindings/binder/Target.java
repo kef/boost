28d9ce85808a2d65d34ceb83b362eb003b5219b9
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-07-22 11:58:07 +0000

    2394
    git-svn-id: https://geekscape.org/svn/boost/trunk@3906 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/bindings/binder/Target.java
+++ src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/bindings/binder/Target.java
@@ -3,10 +3,6 @@ package au.net.netstorm.boost.nursery.eight.legged.spider.bindings.binder;
 import au.net.netstorm.boost.nursery.eight.legged.spider.factory.core.Factory;
 import au.net.netstorm.boost.nursery.eight.legged.spider.provider.Provider;
 
-// FIX 2394 address multiplicity here instead of SingleMaker
-// FIX 2394 or another idea is to do some name based injection, Binder single vs Binder multi and make it implicit
-
-// FIX 2394 thinking about it, idea 2 is definately the way to go
 public interface Target<T> {
     void to(T instance);
 
