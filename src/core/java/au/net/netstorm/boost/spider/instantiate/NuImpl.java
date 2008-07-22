03d98df1289c9ba0a422c760c2a82c9610931b3e
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-07-22 12:04:21 +0000

    2394
    git-svn-id: https://geekscape.org/svn/boost/trunk@3910 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/spider/instantiate/NuImpl.java
+++ src/core/java/au/net/netstorm/boost/spider/instantiate/NuImpl.java
@@ -1,8 +1,5 @@
 package au.net.netstorm.boost.spider.instantiate;
 
-// FIX 2328 Support "edges".
 public interface NuImpl {
-    // FIX 2394 slowly minimising calls to this fellow, will be replaced by Types#nu
-    // FIX 2394 one idea for getting rid of this all together is to make direct impls be registered
     <T> T nu(Class<T> impl, Object... params);
 }
