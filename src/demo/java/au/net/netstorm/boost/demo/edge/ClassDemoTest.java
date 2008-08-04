68d3c571d1e95269e43df389eff896c466b91fa4
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-08-04 08:34:40 +0000

    2395
    git-svn-id: https://geekscape.org/svn/boost/trunk@3927 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/demo/java/au/net/netstorm/boost/demo/edge/ClassDemoTest.java
+++ src/demo/java/au/net/netstorm/boost/demo/edge/ClassDemoTest.java
@@ -3,6 +3,9 @@ package au.net.netstorm.boost.demo.edge;
 import au.net.netstorm.boost.scalpel.core.AutoEdger;
 import demo.edge.java.lang.Class;
 
+// FIX 2328 Ensure satisfactory message when an "instance" interface is used where "static" should be
+// FIX 2328 and vice-versa.
+
 public class ClassDemoTest extends EdgeDemooooTest {
     AutoEdger edger;
 
