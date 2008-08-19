770c351583f8b63b701ac3ba49826042bc58ce3f
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-08-19 11:13:13 +0000

    2130 The data proxy fairy.
    git-svn-id: https://geekscape.org/svn/boost/trunk@3948 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/nursery/immutable/DataInvocationHandlerValidator.java
+++
@@ -1,8 +0,0 @@
-package au.net.netstorm.boost.nursery.immutable;
-
-import au.net.netstorm.boost.gunge.introspect.FieldValueSpec;
-import au.net.netstorm.boost.gunge.type.Interface;
-
-public interface DataInvocationHandlerValidator {
-    void check(FieldValueSpec[] fields, Interface iFace);
-}
