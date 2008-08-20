2a2b47207fe67f2934391ce82065cf33fc9eb454
mag <mag@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-08-20 05:48:05 +0000

    2130 Sort out data proxies
    git-svn-id: https://geekscape.org/svn/boost/trunk@3959 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/test/java/au/net/netstorm/boost/nursery/immutable/Centipede.java
+++
@@ -1,5 +0,0 @@
-package au.net.netstorm.boost.nursery.immutable;
-
-interface Centipede extends HasLegs {
-    String name();
-}
--- src/test/java/au/net/netstorm/boost/nursery/immutable/HasLegs.java
+++
@@ -1,5 +0,0 @@
-package au.net.netstorm.boost.nursery.immutable;
-
-interface HasLegs {
-    Number numLegs();
-}
