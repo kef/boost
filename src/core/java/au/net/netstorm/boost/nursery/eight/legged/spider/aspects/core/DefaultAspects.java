6707a45530d46a8274dfeda7c1a8649990b3b8e0
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-08-21 13:32:49 +0000

    2394
    git-svn-id: https://geekscape.org/svn/boost/trunk@3992 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/aspects/core/DefaultAspects.java
+++ src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/aspects/core/DefaultAspects.java
@@ -1,25 +1,25 @@
 package au.net.netstorm.boost.nursery.eight.legged.spider.aspects.core;
 
-import java.util.List;
+import java.util.Set;
 
-import au.net.netstorm.boost.gunge.collection.ListCreator;
 import au.net.netstorm.boost.gunge.collection.Creator;
 import au.net.netstorm.boost.gunge.collection.DefaultIntegrityMap;
+import au.net.netstorm.boost.gunge.collection.SetCreator;
 import au.net.netstorm.boost.gunge.collection.IntegrityMap;
 import au.net.netstorm.boost.spider.onion.core.Layer;
 
 public final class DefaultAspects implements Aspects {
     // FIX 2394 create some nice interfaces for these somehow.
-    private final IntegrityMap<Class, List<Class<? extends Layer>>> aspects = new DefaultIntegrityMap();
-    private final Creator<Class, List<Class<? extends Layer>>> creator = new ListCreator();
+    private final IntegrityMap<Class, Set<Class<? extends Layer>>> aspects = new DefaultIntegrityMap();
+    private final Creator<Class, Set<Class<? extends Layer>>> creator = new SetCreator();
 
     public void add(Class iface, Class<? extends Layer> aspect) {
-        List<Class<? extends Layer>> classes = aspects.get(iface, creator);
+        Set<Class<? extends Layer>> classes = aspects.get(iface, creator);
         classes.add(aspect);
     }
 
     public Class<? extends Layer>[] get(Class iface) {
-        List<Class<? extends Layer>> classes = aspects.get(iface, creator);
+        Set<Class<? extends Layer>> classes = aspects.get(iface, creator);
         return classes.toArray(new Class[classes.size()]);
     }
 }
