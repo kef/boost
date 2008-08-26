ef9e5e301abfce620b0b15b3918eed479ed77495
mark.hibberd <mark.hibberd@05db6a09-9903-0410-bedf-ba9bc4063e48>
2008-08-26 11:50:58 +0000

    2394
    git-svn-id: https://geekscape.org/svn/boost/trunk@4016 05db6a09-9903-0410-bedf-ba9bc4063e48

--- src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/injection/graph/core/ProtocolEnforcer.java
+++
@@ -1,5 +0,0 @@
-package au.net.netstorm.boost.nursery.eight.legged.spider.injection.graph.core;
-
-public interface ProtocolEnforcer {
-    Object apply(ProtocolInstance protocol);
-}
--- src/core/java/au/net/netstorm/boost/nursery/eight/legged/spider/injection/graph/core/ProtocolInstanceWirer.java
+++
@@ -1,9 +0,0 @@
-package au.net.netstorm.boost.nursery.eight.legged.spider.injection.graph.core;
-
-import au.net.netstorm.boost.nursery.eight.legged.spider.injection.sites.InjectionSite;
-import au.net.netstorm.boost.nursery.eight.legged.spider.provider.Provider;
-import au.net.netstorm.boost.gunge.optional.Optional;
-
-public interface ProtocolInstanceWirer {
-    ProtocolInstance nu(InjectionSite root, Optional<Provider> provider, Object[] args);
-}
